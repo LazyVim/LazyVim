7261a792a9d0381891938fa869b6a6b27c2e8f58
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-30 18:56:48 +0100

    fix: lazxy bootstrap code checked lazypath twice

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,9 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
-    vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
-  end
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
 end
 vim.opt.rtp:prepend(lazypath)
 
