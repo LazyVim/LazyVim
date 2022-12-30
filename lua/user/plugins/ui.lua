fbd502221d66c924cb81d7e8babc7141c29b33cf
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-30 23:37:29 +0100

    style(alpha): better colors

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -72,7 +72,7 @@ return {
         button.opts.hl = "AlphaButtons"
         button.opts.hl_shortcut = "AlphaShortcut"
       end
-      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.section.footer.opts.hl = "Type"
       dashboard.section.header.opts.hl = "AlphaHeader"
       dashboard.section.buttons.opts.hl = "AlphaButtons"
       dashboard.opts.layout[1].val = 8
