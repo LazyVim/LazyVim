1467c900ac001e14a705275e2b4743f14b7ff8c4
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-31 13:13:30 +0100

    feat(alpha): added lazy shortcut

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -86,6 +86,7 @@ return {
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
         dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
         dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+        dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
       for _, button in ipairs(dashboard.section.buttons.val) do
