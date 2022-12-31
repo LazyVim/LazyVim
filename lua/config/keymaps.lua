8d43ddec9b208a217f84783915714c8f5bd0a10e
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-31 18:05:04 +0100

    feat(git): added lazygit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -64,3 +64,16 @@ vim.cmd([[
   xnoremap * :<C-u>call g:VSetSearch('/')<CR>/<C-R>=@/<CR><CR>
   xnoremap # :<C-u>call g:VSetSearch('?')<CR>?<C-R>=@/<CR><CR>
 ]])
+
+vim.keymap.set("n", "<leader>gg", function()
+  require("lazy.util").open_cmd({ "lazygit" }, {
+    terminal = true,
+    close_on_exit = true,
+    enter = true,
+    float = {
+      size = { width = 0.9, height = 0.9 },
+      margin = { top = 0, right = 0, bottom = 0, left = 0 },
+    },
+  })
+end, { desc = "Lazygit" })
+
