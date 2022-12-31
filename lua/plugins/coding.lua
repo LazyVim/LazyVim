c3eda447dd0a663292f34f13c645a93360aa0854
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-31 17:07:30 +0100

    refactor: moved gitsigns and illuminate from coding to editor

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,21 +1,5 @@
 return {
 
-  -- git signs
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "BufReadPre",
-    config = {
-      signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "契" },
-        topdelete = { text = "契" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
-      },
-    },
-  },
-
   -- snippets
   {
     "L3MON4D3/LuaSnip",
@@ -95,30 +79,5 @@ return {
         },
       })
     end,
-  },
-
-  -- references
-  {
-    "RRethy/vim-illuminate",
-    event = "BufReadPost",
-    config = function()
-      require("illuminate").configure({ delay = 200 })
-    end,
-    keys = {
-      {
-        "]]",
-        function()
-          require("illuminate").goto_next_reference(false)
-        end,
-        desc = "Next Reference",
-      },
-      {
-        "[[",
-        function()
-          require("illuminate").goto_prev_reference(false)
-        end,
-        desc = "Prev Reference",
-      },
-    },
   },
 }
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -32,4 +32,45 @@ return {
       key_labels = { ["<leader>"] = "SPC" },
     },
   },
+
+  -- git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    config = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "契" },
+        topdelete = { text = "契" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+    },
+  },
+
+  -- references
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    config = function()
+      require("illuminate").configure({ delay = 200 })
+    end,
+    keys = {
+      {
+        "]]",
+        function()
+          require("illuminate").goto_next_reference(false)
+        end,
+        desc = "Next Reference",
+      },
+      {
+        "[[",
+        function()
+          require("illuminate").goto_prev_reference(false)
+        end,
+        desc = "Prev Reference",
+      },
+    },
+  },
 }
