17c0eb4d56f1edc4643576e41086f15853d06cf1
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-31 11:18:01 +0100

    feat(ui): added noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -45,6 +45,25 @@ return {
     },
   },
 
+  -- noicer ui
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+  },
+
   -- dashboard
   {
     "goolord/alpha-nvim",
