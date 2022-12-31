6dd32b53e534772e8b572082a4a9c2e7f1e6e1bd
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-31 18:05:59 +0100

    perf: load keymaps and autocmds on VeryLazy

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 require("config.options")
 require("config.lazy")
-require("config.autocmds")
-require("config.keymaps")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.autocmds")
+    require("config.keymaps")
+  end,
+})
