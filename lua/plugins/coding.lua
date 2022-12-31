b267e33810941d4be4fdd6d531ead2032f2fb64f
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-12-31 17:03:56 +0100

    feat(illuminate): added illuminate

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -96,4 +96,29 @@ return {
       })
     end,
   },
+
+  -- references
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost",
+    config = function()
+      require("illuminate").configure({ delay = 200 })
+    end,
+    keys = {
+      {
+        "]]",
+        function()
+          require("illuminate").goto_next_reference(false)
+        end,
+        desc = "Next Reference",
+      },
+      {
+        "[[",
+        function()
+          require("illuminate").goto_prev_reference(false)
+        end,
+        desc = "Prev Reference",
+      },
+    },
+  },
 }
