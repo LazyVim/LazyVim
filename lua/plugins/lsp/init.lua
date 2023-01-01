0e6b710627c2c297b91437fdb2b3b07a3cad30ae
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-01 12:58:29 +0100

    feat(mason): added keymap

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -13,7 +13,12 @@ return {
     dependencies = {
       { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
       { "folke/neodev.nvim", config = true },
-      { "williamboman/mason.nvim", config = true, cmd = "Mason" },
+      {
+        "williamboman/mason.nvim",
+        config = true,
+        cmd = "Mason",
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+      },
       { "williamboman/mason-lspconfig.nvim", config = { automatic_installation = true } },
       "hrsh7th/cmp-nvim-lsp",
     },
