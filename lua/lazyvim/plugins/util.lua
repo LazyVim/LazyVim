d4a48226dab3632f88bedc34a92f26637cc21574
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 09:49:49 +0100

    feat(startuptime): better defaults

--- lua/lazyvim/plugins/util.lua
+++ lua/lazyvim/plugins/util.lua
@@ -1,5 +1,11 @@
 return {
 
   -- measure startuptime
-  { "dstein64/vim-startuptime", cmd = "StartupTime" },
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 }
