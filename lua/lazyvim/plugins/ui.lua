7b9d409c2f16f424ebb314e7120078b0606a1f59
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 09:47:42 +0100

    feat(bufferline): added siagnostic icons

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -35,7 +35,26 @@ return {
   {
     "akinsho/nvim-bufferline.lua",
     event = "BufAdd",
-    config = true,
+    config = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config.icons").diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
   },
 
   -- statusline
