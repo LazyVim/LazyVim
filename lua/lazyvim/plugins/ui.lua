4d2327dd1ab285d8a3fef2fc3d0a9cbe7d31f433
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 09:47:23 +0100

    perf(dressing): lazy-load on vim.ui function call

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -17,7 +17,18 @@ return {
   {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
-    config = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
   },
 
   -- bufferline
