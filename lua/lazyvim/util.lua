e6dfed58f7638e07eed9619a619fabf8fbeb4cd0
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 17:35:59 +0100

    feat(util): util.on_attach

--- lua/lazyvim/util.lua
+++ lua/lazyvim/util.lua
@@ -1,6 +1,18 @@
 local M = {}
 
 M.root_patterns = { ".git", "/lua" }
+
+---@param on_attach fun(client, buffer)
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
 -- returns the root directory based on:
 -- * lsp workspace folders
 -- * lsp root_dir
