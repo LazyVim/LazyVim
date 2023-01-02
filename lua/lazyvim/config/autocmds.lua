63bea54be2391facf20921b6c52c41f2b625794e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 17:36:49 +0100

    fix(autocmds): better way of opening file at last location

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -11,7 +11,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    vim.cmd([[silent! normal! g`"]])
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
   end,
 })
 
