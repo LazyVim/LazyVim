9b5dd50837d64d2ca71cb9e0868a3aece3c8e4a0
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-02 17:38:29 +0100

    feat(telescope): added a gazillian mappings for Telescope

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -1,4 +1,5 @@
 vim.g.neo_tree_remove_legacy_commands = 1
+local util = require("lazyvim.util")
 
 return {
 
@@ -43,11 +44,46 @@ return {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
     keys = {
-      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      { "<leader>/", util.telescope("live_grep"), desc = "Find in Files (Grep)" },
+      { "<leader><space>", util.telescope("find_files"), desc = "Find Files" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find in Files (Grep)" },
+      { "<leader>ff", util.telescope("find_files"), desc = "Find Files" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      { "<leader>gc", "<Cmd>Telescope git_commits<CR>", desc = "commits" },
+      { "<leader>gs", "<Cmd>Telescope git_status<CR>", desc = "status" },
+      { "<leader>ha", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+      { "<leader>hc", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      { "<leader>hf", "<cmd>Telescope filetypes<cr>", desc = "File Types" },
+      { "<leader>hh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+      { "<leader>hk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+      { "<leader>hm", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+      { "<leader>ho", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+      { "<leader>hs", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+      { "<leader>ht", "<cmd>Telescope builtin<cr>", desc = "Telescope" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>sg", util.telescope("live_grep"), desc = "Grep" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+      { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      {
+        "<leader>ss",
+        util.telescope("lsp_document_symbols", {
+          symbols = {
+            "Class",
+            "Function",
+            "Method",
+            "Constructor",
+            "Interface",
+            "Module",
+            "Struct",
+            "Trait",
+            "Field",
+            "Property",
+          },
+        }),
+        desc = "Goto Symbol",
+      },
     },
     config = true,
   },
@@ -84,6 +120,7 @@ return {
         ["<leader>c"] = { name = "+code" },
         ["<leader>f"] = { name = "+file" },
         ["<leader>g"] = { name = "+git" },
+        ["<leader>h"] = { name = "+help" },
         ["<leader>x"] = { name = "+diagnostics" },
       })
     end,
