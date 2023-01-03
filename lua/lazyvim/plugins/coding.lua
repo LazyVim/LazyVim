2fe505328422c4c4c8a6cc55968751291fe104b5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-04 00:53:42 +0100

    feat: added mini.surround

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -84,6 +84,26 @@ return {
     end,
   },
 
+  -- surround
+  {
+    "echasnovski/mini.surround",
+    keys = { "gz" },
+    config = function()
+      -- use gz mappings instead of s to prevent conflict with leap
+      require("mini.surround").setup({
+        mappings = {
+          add = "gza", -- Add surrounding in Normal and Visual modes
+          delete = "gzd", -- Delete surrounding
+          find = "gzf", -- Find surrounding (to the right)
+          find_left = "gzF", -- Find surrounding (to the left)
+          highlight = "gzh", -- Highlight surrounding
+          replace = "gzr", -- Replace surrounding
+          update_n_lines = "gzn", -- Update `n_lines`
+        },
+      })
+    end,
+  },
+
   -- comments
   { "JoosepAlviste/nvim-ts-context-commentstring" },
   {
