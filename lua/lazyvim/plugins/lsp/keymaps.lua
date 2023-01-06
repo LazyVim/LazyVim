92c22d793283e44b0cec66470d656ee2b6dfc519
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-07 00:13:38 +0100

    feat(lps)!: move signature help from `<-k>` to `gK`

--- lua/lazyvim/plugins/lsp/keymaps.lua
+++ lua/lazyvim/plugins/lsp/keymaps.lua
@@ -12,14 +12,13 @@ function M.on_attach(client, buffer)
   self:map("gI", "Telescope lsp_implementations", { desc = "Goto Implementation" })
   self:map("gt", "Telescope lsp_type_definitions", { desc = "Goto Type Definition" })
   self:map("K", vim.lsp.buf.hover, { desc = "Hover" })
+  self:map("gK", vim.lsp.buf.signature_help, { desc = "Signature Help", has = "signatureHelp" })
   self:map("[d", M.diagnostic_goto(true), { desc = "Next Diagnostic" })
   self:map("]d", M.diagnostic_goto(false), { desc = "Prev Diagnostic" })
   self:map("]e", M.diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
   self:map("[e", M.diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
   self:map("]w", M.diagnostic_goto(true, "WARNING"), { desc = "Next Warning" })
   self:map("[w", M.diagnostic_goto(false, "WARNING"), { desc = "Prev Warning" })
-
-  self:map("<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help", mode = { "i", "n" }, has = "signatureHelp" })
   self:map("<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", mode = { "n", "v" }, has = "codeAction" })
 
   local format = require("lazyvim.plugins.lsp.format").format
