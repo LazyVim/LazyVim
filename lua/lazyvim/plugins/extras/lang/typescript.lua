6c73e775cfc8e23cbe2bb43873933458383804c5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-08 15:52:00 +0100

    feat(typescript): added extras for typescript

---
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -0,0 +1,29 @@
+vim.notify("adding typescript")
+return {
+
+  -- add typescript to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "typescript")
+    end,
+  },
+
+  -- correctly setup lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "jose-elias-alvarez/typescript.nvim" },
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        tsserver = {},
+      },
+      setup = {
+        tsserver = function(_, opts)
+          require("typescript").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+}
