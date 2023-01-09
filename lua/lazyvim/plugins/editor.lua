a0fadd6a11addb1f51bc2142fbd2606ec59dbc79
Myles Mo <54089360+emxxjnm@users.noreply.github.com>
2023-01-09 15:03:53 +0800

    fix(editor): gitsigns prev/next hunk keymap (#26)
    Co-authored-by: Myles Mo <mylesmo.ash@gmail.com>

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -180,8 +180,8 @@ return {
         end
 
         -- stylua: ignore start
-        map("n", "]h", gs.prev_hunk, "Next Hunk")
-        map("n", "[h", gs.next_hunk, "Prev Hunk")
+        map("n", "]h", gs.next_hunk, "Next Hunk")
+        map("n", "[h", gs.prev_hunk, "Prev Hunk")
         map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
         map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
         map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
