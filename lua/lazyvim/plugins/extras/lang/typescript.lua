2128bf3e20cceaf6c77275151087dd7606a9c15c
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-10 17:54:45 +0100

    fix(typescript): only set ts keymaps for tsserver

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -14,10 +14,12 @@ return {
     dependencies = {
       "jose-elias-alvarez/typescript.nvim",
       init = function()
-        require("lazyvim.util").on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+        require("lazyvim.util").on_attach(function(server, buffer)
+          if server == "tsserver" then
+            -- stylua: ignore
+            vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+            vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+          end
         end)
       end,
     },
