cce3129e995fa349002c518a7655bac3fb23a395
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-10 19:32:02 +0100

    fix(typescipt): properly check client.name in on_attach

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -11,18 +11,7 @@ return {
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").on_attach(function(server, buffer)
-          if server == "tsserver" then
-            -- stylua: ignore
-            vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-            vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-          end
-        end)
-      end,
-    },
+    dependencies = { "jose-elias-alvarez/typescript.nvim" },
     opts = {
       -- make sure mason installs the server
       servers = {
@@ -30,6 +19,13 @@ return {
       },
       setup = {
         tsserver = function(_, opts)
+          require("lazyvim.util").on_attach(function(client, buffer)
+            if client.name == "tsserver" then
+              -- stylua: ignore
+              vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+              vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+            end
+          end)
           require("typescript").setup({ server = opts })
           return true
         end,
