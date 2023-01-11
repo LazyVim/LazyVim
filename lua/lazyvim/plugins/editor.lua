24923ed31ad4c019b68583cce2acdc65dd839897
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-11 17:28:55 +0100

    refactor(neotree)!: keymaps are now under `<leader>fe` and `<leader>fE`

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -8,15 +8,15 @@ return {
     cmd = "Neotree",
     keys = {
       {
-        "<leader>ft",
+        "<leader>fe",
         function()
           require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
         end,
-        desc = "NeoTree (root dir)",
+        desc = "Explorer NeoTree (root dir)",
       },
-      { "<leader>fT", "<cmd>Neotree toggle<CR>", desc = "NeoTree (cwd)" },
-      { "<leader>e", "<leader>ft", desc = "NeoTree (root dir)", remap = true },
-      { "<leader>E", "<leader>fT", desc = "NeoTree (cwd)", remap = true },
+      { "<leader>fE", "<cmd>Neotree toggle<CR>", desc = "Explorer NeoTree (cwd)" },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
     },
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
