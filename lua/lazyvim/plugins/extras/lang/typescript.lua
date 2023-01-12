a33d90652428ef4224b43e8f4cbec447c85fb117
Andreas Gerlach <appelgriebsch@users.noreply.github.com>
2023-01-12 13:25:42 +0100

    fix: add tsx treesitter to typescript lang module (#48)

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -4,7 +4,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      table.insert(opts.ensure_installed, "typescript")
+      vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
     end,
   },
 
