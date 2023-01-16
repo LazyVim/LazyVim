3d498ee7136be179aada077d39c14adff6056187
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-16 20:39:12 +0100

    fix(version): don't use version for schemastore and nvim-cmp. Last version is too old

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -30,6 +30,7 @@ return {
   -- auto completion
   {
     "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
     event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
--- lua/lazyvim/plugins/extras/lang/json.lua
+++ lua/lazyvim/plugins/extras/lang/json.lua
@@ -11,7 +11,10 @@ return {
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "b0o/SchemaStore.nvim" },
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+      version = false, -- last release is way too old
+    },
     opts = {
       -- make sure mason installs the server
       servers = {
