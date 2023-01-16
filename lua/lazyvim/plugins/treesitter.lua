876d52fee3e41d2c6d166bc05b06072fe443fb5e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-16 23:36:01 +0100

    fix(windows): set version=false for treesitter. Last release is too old and broken on Windows. Fixes #2

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
     event = "BufReadPost",
     keys = {
