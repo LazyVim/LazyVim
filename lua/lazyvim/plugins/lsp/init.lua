cc0171e770505c779374bac078d2e6f62a0f16d8
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-01-22 10:31:48 +0100

    feat(lsp): setup lsp servers not supported by mason-lspconfig and added mason=false to skip server setup by mason. Fixes #95

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -33,6 +33,7 @@ return {
       servers = {
         jsonls = {},
         sumneko_lua = {
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
           settings = {
             Lua = {
               workspace = {
@@ -69,7 +70,6 @@ return {
 
       -- setup autoformat
       require("lazyvim.plugins.lsp.format").autoformat = opts.autoformat
-
       -- setup formatting and keymaps
       require("lazyvim.util").on_attach(function(client, buffer)
         require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
@@ -86,26 +86,39 @@ return {
       local servers = opts.servers
       local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-      require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(servers) })
-      require("mason-lspconfig").setup_handlers({
-        function(server)
-          if servers[server] == false then
+      local function setup(server)
+        local server_opts = servers[server] or {}
+        server_opts.capabilities = capabilities
+        if opts.setup[server] then
+          if opts.setup[server](server, server_opts) then
             return
           end
-          local server_opts = servers[server] or {}
-          server_opts.capabilities = capabilities
-          if opts.setup[server] then
-            if opts.setup[server](server, server_opts) then
-              return
-            end
-          elseif opts.setup["*"] then
-            if opts.setup["*"](server, server_opts) then
-              return
-            end
+        elseif opts.setup["*"] then
+          if opts.setup["*"](server, server_opts) then
+            return
           end
-          require("lspconfig")[server].setup(server_opts)
-        end,
-      })
+        end
+        require("lspconfig")[server].setup(server_opts)
+      end
+
+      local mlsp = require("mason-lspconfig")
+      local available = mlsp.get_available_servers()
+
+      local ensure_installed = {} ---@type string[]
+      for server, server_opts in pairs(servers) do
+        if server_opts then
+          server_opts = server_opts == true and {} or server_opts
+          -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+          if server_opts.mason == false or not vim.tbl_contains(available, server) then
+            setup(server)
+          else
+            ensure_installed[#ensure_installed + 1] = server
+          end
+        end
+      end
+
+      require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+      require("mason-lspconfig").setup_handlers({ setup })
     end,
   },
 
