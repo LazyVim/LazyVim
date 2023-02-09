3af91550be67d9699567727fffcae8aabafc0898
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-09 14:37:37 +0100

    feat(bufferline): added keymaps to pin and delete non-pinned buffers. See #218

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -44,6 +44,10 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    },
     opts = {
       options = {
         diagnostics = "nvim_lsp",
