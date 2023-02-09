42d2411bfa1d6fcc5edc20ee6dbc0dff0f0fba6e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-09 22:49:07 +0100

    fix(leap): don't use the x and X mappings in visual mode

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -150,6 +150,8 @@ return {
         leap.opts[k] = v
       end
       leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
     end,
   },
 
