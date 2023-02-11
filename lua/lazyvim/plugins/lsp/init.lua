b03a391f70467c9ac99d353df98c20041b0554e3
Zhizhen He <hezhizhen.yi@gmail.com>
2023-02-11 23:35:04 +0800

    style: fix comment typo

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -28,7 +28,7 @@ return {
       autoformat = true,
       -- options for vim.lsp.buf.format
       -- `bufnr` and `filter` is handled by the LazyVim formatter,
-      -- but can be also overriden when specified
+      -- but can be also overridden when specified
       format = {
         formatting_options = nil,
         timeout_ms = nil,
--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -72,7 +72,7 @@ function M.get_root()
 end
 
 -- this will return a function that calls telescope.
--- cwd will defautlt to lazyvim.util.get_root
+-- cwd will default to lazyvim.util.get_root
 -- for `files`, git_files or find_files will be chosen depending on .git
 function M.telescope(builtin, opts)
   local params = { builtin = builtin, opts = opts }
