543126818e678509ee9e17836f0e7906146a1002
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-12 10:59:58 +0100

    fix(lsp): temp mason fix for sumenko_lua -> lua_ls rename. See #248

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -37,7 +37,7 @@ return {
       ---@type lspconfig.options
       servers = {
         jsonls = {},
-        sumneko_lua = {
+        lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
           settings = {
             Lua = {
@@ -99,6 +99,14 @@ return {
           end
         end
         require("lspconfig")[server].setup(server_opts)
+      end
+
+      -- temp fix for lspconfig rename
+      -- https://github.com/neovim/nvim-lspconfig/pull/2439
+      local mappings = require("mason-lspconfig.mappings.server")
+      if not mappings.lspconfig_to_package.lua_ls then
+        mappings.lspconfig_to_package.lua_ls = "lua-language-server"
+        mappings.package_to_lspconfig["lua-language-server"] = "lua_ls"
       end
 
       local mlsp = require("mason-lspconfig")
