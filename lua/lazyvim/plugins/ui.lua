5d6f0d58d5daf2f87ea9f0a49170103925d1b528
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-14 21:56:09 +0100

    fix(notify): install notify when noice is not enabled

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -20,6 +20,15 @@ return {
         return math.floor(vim.o.columns * 0.75)
       end,
     },
+    init = function()
+      -- when noice is not enabled, install notify on VeryLazy
+      local Util = require("lazyvim.util")
+      if not Util.has("noice.nvim") then
+        Util.on_very_lazy(function()
+          vim.notify = require("notify")
+        end)
+      end
+    end,
   },
 
   -- better vim.ui
