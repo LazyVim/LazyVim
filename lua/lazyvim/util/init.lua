cc3070d30e829cb60f916fb562d00f7a30f98991
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-14 21:55:52 +0100

    faet(util): added `Util.on_very_lazy`

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -20,6 +20,16 @@ function M.has(plugin)
   return require("lazy.core.config").plugins[plugin] ~= nil
 end
 
+---@param fn fun()
+function M.on_very_lazy(fn)
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+      fn()
+    end,
+  })
+end
+
 ---@param name string
 function M.opts(name)
   local plugin = require("lazy.core.config").plugins[name]
