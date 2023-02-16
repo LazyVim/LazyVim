eccd122e6dafae48bbf3c63e9fb3ae1ad4fc47a4
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-16 14:12:04 +0100

    feat(leap): lazy-load leap/flit and added keymaps

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -152,9 +152,24 @@ return {
 
   -- easily jump to any location and enhanced f/t motions for Leap
   {
+    "ggandor/flit.nvim",
+    keys = function()
+      ---@type LazyKeys[]
+      local ret = {}
+      for _, key in ipairs({ "f", "F", "t", "T" }) do
+        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = "nx" },
+  },
+  {
     "ggandor/leap.nvim",
-    event = "VeryLazy",
-    dependencies = { { "ggandor/flit.nvim", opts = { labeled_modes = "nv" } } },
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
     config = function(_, opts)
       local leap = require("leap")
       for k, v in pairs(opts) do
