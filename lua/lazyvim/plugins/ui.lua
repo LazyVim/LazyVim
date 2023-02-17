78cb3bb66d45762ba85190b2901360f683a8aa95
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2023-02-17 01:18:59 -0500

    fix(lualine): Add a space separator between progress & location in lualine (#285)

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -145,7 +145,7 @@ return {
             },
           },
           lualine_y = {
-            { "progress", separator = "", padding = { left = 1, right = 0 } },
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
             { "location", padding = { left = 0, right = 1 } },
           },
           lualine_z = {
