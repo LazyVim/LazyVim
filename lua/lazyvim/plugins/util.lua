6b9f0c0c3c16b39a0764632402707822a084af50
d2kay <ditsuke@protonmail.com>
2023-02-19 14:26:08 +0530

    feat: persist pinned tabs (#304)
    Adds `globals` to session opts to persist pinned tabs

--- lua/lazyvim/plugins/util.lua
+++ lua/lazyvim/plugins/util.lua
@@ -13,7 +13,7 @@ return {
   {
     "folke/persistence.nvim",
     event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
     -- stylua: ignore
     keys = {
       { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
