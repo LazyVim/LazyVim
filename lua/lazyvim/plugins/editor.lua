4b37723558e45f14c3e8d05fc84ad3a701b9faf0
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-28 10:01:53 +0100

    fix(git-signs): larger icons for delete

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -243,8 +243,8 @@ return {
       signs = {
         add = { text = "▎" },
         change = { text = "▎" },
-        delete = { text = "󰐊" },
-        topdelete = { text = "󰐊" },
+        delete = { text = "" },
+        topdelete = { text = "" },
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       },
