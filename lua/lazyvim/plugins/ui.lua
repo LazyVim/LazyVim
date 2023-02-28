d97bf66eed76c91eac11c81dad69aff9f632b214
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-28 09:51:29 +0100

    fix(mini-indentscope): disable indentscope for filetypes during init. Fixes #318

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -182,13 +182,15 @@ return {
       symbol = "│",
       options = { try_as_border = true },
     },
-    config = function(_, opts)
+    init = function()
       vim.api.nvim_create_autocmd("FileType", {
         pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
       })
+    end,
+    config = function(_, opts)
       require("mini.indentscope").setup(opts)
     end,
   },
