70966bbc19cb6e216e65cb887a01b9be9057e3b9
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-02-28 09:59:01 +0100

    style: desc for gw

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -64,7 +64,7 @@ map(
   { desc = "Redraw / clear hlsearch / diff update" }
 )
 
-map({ "n", "x" }, "gw", "*N", { desc = "Highlight under cursor" })
+map({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
