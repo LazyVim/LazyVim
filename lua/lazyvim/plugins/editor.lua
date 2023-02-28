588dc5dd65aa3881122aa4e59710de6722423251
Sakari <sk308269317@outlook.com>
2023-02-28 16:59:50 +0800

    fix(icons): replace obsolete Nerd icons (#331)

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -243,8 +243,8 @@ return {
       signs = {
         add = { text = "▎" },
         change = { text = "▎" },
-        delete = { text = "契" },
-        topdelete = { text = "契" },
+        delete = { text = "󰐊" },
+        topdelete = { text = "󰐊" },
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       },
