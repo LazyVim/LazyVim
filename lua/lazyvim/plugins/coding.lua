f6f3ce4a9b7a797653dc290b9485d2ce970af871
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-02 14:15:51 +0100

    fix(luasnip): dont build jsregexp on Windows

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -3,7 +3,7 @@ return {
   -- snippets
   {
     "L3MON4D3/LuaSnip",
-    build = "make install_jsregexp",
+    build = (not jit.os:find("Windows")) and "make install_jsregexp" or nil,
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
