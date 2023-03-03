04a60e251b1c2437ad6d0a237fbacc1848d4aac1
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-03 08:28:27 +0100

    fix(luasnip): added note that jsregexp is optional if it fails to build

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -3,7 +3,9 @@ return {
   -- snippets
   {
     "L3MON4D3/LuaSnip",
-    build = (not jit.os:find("Windows")) and "make install_jsregexp" or nil,
+    build = (not jit.os:find("Windows"))
+        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+      or nil,
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
