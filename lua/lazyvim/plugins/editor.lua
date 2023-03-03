57c2dcb3a35704a849ea8ba210f963c22fb6d1ce
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-03 20:10:40 +0100

    feat(neo-tree): added expanders

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -46,6 +46,14 @@ return {
           ["<space>"] = "none",
         },
       },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
     },
   },
 
