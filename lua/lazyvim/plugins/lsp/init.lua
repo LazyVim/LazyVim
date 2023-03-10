5aad5749a745f928b0a1954917467f04c84e8a07
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-10 08:44:17 +0100

    feat(null-ls): added .neoconf.json to null-ls root_dir

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -138,6 +138,7 @@ return {
     opts = function()
       local nls = require("null-ls")
       return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {
           nls.builtins.formatting.fish_indent,
           nls.builtins.diagnostics.fish,
