75299da24c4982f0d3ec6c9c3fbad7f94aa9eb72
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-10 08:44:47 +0100

    feat(eslint): added an extra for eslint lsp that runs EslintFixAll before saving a buffer

---
+++ lua/lazyvim/plugins/extras/linting/eslint.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    -- other settings removed for brevity
+    opts = {
+      servers = {
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectory = { mode = "auto" },
+          },
+        },
+      },
+      setup = {
+        eslint = function()
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            command = "EslintFixAll",
+          })
+        end,
+      },
+    },
+  },
+}
