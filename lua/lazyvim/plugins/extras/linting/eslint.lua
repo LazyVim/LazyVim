f1dbd097e4461c322d6982c925d605a6ed57a4b2
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-10 08:54:56 +0100

    fix(eslint): only run EslintFixAll on buffers where eslint is attached

--- lua/lazyvim/plugins/extras/linting/eslint.lua
+++ lua/lazyvim/plugins/extras/linting/eslint.lua
@@ -14,7 +14,11 @@ return {
       setup = {
         eslint = function()
           vim.api.nvim_create_autocmd("BufWritePre", {
-            command = "EslintFixAll",
+            callback = function(event)
+              if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
+                vim.cmd("EslintFixAll")
+              end
+            end,
           })
         end,
       },
