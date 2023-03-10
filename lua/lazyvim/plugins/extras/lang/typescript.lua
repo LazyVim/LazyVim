5bb23147877b3fcbe4cc28c0a7d820bcb52bb141
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-03-10 08:43:53 +0100

    feat(typescript): added null-ls typescript code actions

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -42,4 +42,10 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
+    end,
+  },
 }
