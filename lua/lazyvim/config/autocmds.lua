ed48b85dbea30a15518b4caa58cbf5d2b54f97a0
Võ Quang Chiến <87252943+yeuxacucodon@users.noreply.github.com>
2023-03-31 14:40:57 +0700

    feat(autocmds): add auto create dir (#493)
    * feat(autocmds): add auto create dir
    
    * refactor: auto-create dir
    
    ---------
    
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -67,3 +67,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
