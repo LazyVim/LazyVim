432e9bfe72d6bfe6ec3a2eec2da4aacff339ed58
loichyan <loichyan@foxmail.com>
2023-04-08 18:02:46 +0800

    feat(catppuccin): enable integration for used plugins

--- lua/lazyvim/plugins/colorscheme.lua
+++ lua/lazyvim/plugins/colorscheme.lua
@@ -12,5 +12,53 @@ return {
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        dashboard = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        markdown = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true },
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+      custom_highlights = function(c)
+        return {
+          -- Use tokyonight style leap highlights.
+          LeapMatch = { fg = c.pink, bold = true },
+          LeapLabelPrimary = { fg = c.green, bold = true },
+          LeapLabelSecondary = { fg = c.blue, bold = true },
+          LeapBackdrop = { fg = c.overlay0 },
+          NavicText = { fg = c.text, bg = c.none },
+        }
+      end,
+    },
   },
 }
