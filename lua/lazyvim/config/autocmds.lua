7476da78117b032bc34d0130e6b1f9c7eea77073
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-04-16 07:53:42 +0200

    fix(autocmds): don't auto create dirs for urls. Fixes #583

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -72,6 +72,9 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = augroup("auto_create_dir"),
   callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
     local file = vim.loop.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
