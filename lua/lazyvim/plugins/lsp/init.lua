06c38c52a91bb1dcc2dec2d79d6e13606324be77
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-04-18 19:59:19 +0200

    feat(diagnostics): show diagnostics source when more than one source is reporting diagnostics

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -21,7 +21,11 @@ return {
       diagnostics = {
         underline = true,
         update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "●" },
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
         severity_sort = true,
       },
       -- Automatically format on save
