1d2c97cbda099c76ee132ee5019a3f80fd88f8ed
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-04-19 16:44:57 +0200

    fix(treesitter): de-duplicate langs in endure_installed. Fixes #628

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -67,6 +67,17 @@ return {
     },
     ---@param opts TSConfig
     config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
