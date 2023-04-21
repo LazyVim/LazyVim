d63c4710083cd2bcab104972bd4a6b3d1b80c3c6
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-04-21 08:46:45 +0200

    feat(copilot): added lualine component for copilot. Simple icon colored by status. Will add status message when available (warnings)

--- lua/lazyvim/plugins/extras/coding/copilot.lua
+++ lua/lazyvim/plugins/extras/coding/copilot.lua
@@ -10,6 +10,34 @@ return {
       panel = { enabled = false },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local Util = require("lazyvim.util")
+      local colors = {
+        [""] = Util.fg("Special"),
+        ["Normal"] = Util.fg("Special"),
+        ["Warning"] = Util.fg("DiagnosticWarn"),
+        ["InProgress"] = Util.fg("DiagnosticInfo"),
+      }
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local icon = require("lazyvim.config").icons.kinds.Copilot
+          local status = require("copilot.api").status.data
+          return icon .. (status.message or "")
+        end,
+        cond = function()
+          local clients = vim.lsp.get_active_clients({ name = "copilot", bufnr = 0 })
+          return #clients > 0
+        end,
+        color = function()
+          local status = require("copilot.api").status.data
+          return colors[status.status] or colors[""]
+        end,
+      })
+    end,
+  },
 
   -- copilot cmp source
   {
