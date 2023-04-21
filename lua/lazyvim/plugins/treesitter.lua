0bf9b2d9f2e609515d8995b31c3845a2e94385fb
loqusion <loqusion@gmail.com>
2023-04-21 02:07:13 -0500

    fix: remove unintentional `<lt>nop>` mapping

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -60,7 +60,7 @@ return {
         keymaps = {
           init_selection = "<C-space>",
           node_incremental = "<C-space>",
-          scope_incremental = "<nop>",
+          scope_incremental = false,
           node_decremental = "<bs>",
         },
       },
