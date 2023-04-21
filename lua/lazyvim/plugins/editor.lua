ad214af54d47e67aa8be46ae12cbe7c71da0c0bf
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-04-21 23:42:16 +0200

    feat(neo-tree): refresh neotree git status when closing a lazygit terminal

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -40,6 +40,7 @@ return {
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = true,
+        use_libuv_file_watcher = true,
       },
       window = {
         mappings = {
@@ -55,6 +56,17 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
+        end,
+      })
+    end,
   },
 
   -- search/replace in multiple files
