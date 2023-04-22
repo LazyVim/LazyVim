4b500110198322e70467cefb79415f1bcc5a201e
Albert Gu <albertfgu@gmail.com>
2023-04-22 21:00:44 +0000

    Change DAP REPL binding from open to toggle

--- lua/lazyvim/plugins/extras/dap/core.lua
+++ lua/lazyvim/plugins/extras/dap/core.lua
@@ -82,7 +82,7 @@ return {
     { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
     { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
     { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
-    { "<leader>dr", function() require("dap").repl.open() end, desc = "Repl" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
     { "<leader>ds", function() require("dap").session() end, desc = "Session" },
     { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
     { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
