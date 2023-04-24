728d524d2173dc3bf0f412169ea54d6406a6a849
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-04-24 08:40:23 +0200

    fix(bufferline): don't force remove buffers

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -60,9 +60,9 @@ return {
     opts = {
       options = {
         -- stylua: ignore
-        close_command = function(n) require("mini.bufremove").delete(n, true) end,
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
         -- stylua: ignore
-        right_mouse_command = function(n) require("mini.bufremove").delete(n, true) end,
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         diagnostics = "nvim_lsp",
         always_show_bufferline = false,
         diagnostics_indicator = function(_, _, diag)
