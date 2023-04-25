dc1d48f436a90c2ed3efd5fb562c2dd1afb0649f
Albert Gu <albertfgu@gmail.com>
2023-04-25 00:12:41 -0700

    style: Change description of notify.dismiss() binding (#675)

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
         function()
           require("notify").dismiss({ silent = true, pending = true })
         end,
-        desc = "Delete all Notifications",
+        desc = "Dismiss all Notifications",
       },
     },
     opts = {
