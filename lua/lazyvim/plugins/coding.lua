44df7434d4320d27672cca937e594efbf1e021ba
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-12 17:03:16 +0200

    perf(comment): removed nvim-ts-context-commentstring, since mini.comment now handles that internally

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -137,20 +137,11 @@ return {
   },
 
   -- comments
-  { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
   {
     "echasnovski/mini.comment",
     event = "VeryLazy",
-    opts = {
-      hooks = {
-        pre = function()
-          require("ts_context_commentstring.internal").update_commentstring({})
-        end,
-      },
-    },
-    config = function(_, opts)
-      require("mini.comment").setup(opts)
-    end,
+    opts = {},
+    main = "mini.comment",
   },
 
   -- better text-objects
