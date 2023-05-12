d41ef05cc00bf3502fb55512c265a02116127ca8
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-12 20:33:51 +0200

    feat(mini.comment)!: removed ts-context-commentstring since mini.comment provides this functionality out of the box
    If you use this plugin, you can add it again with the following code:
    {
      "nvim-treesitter/nvim-treesitter",
      opts = {
        context_commentstring = {
          enable = true,
          enable_autocmd = false,
        },
      },
      dependencies = {
        "JoosepAlviste/nvim-ts-context-commentstring",
      },
    }

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -34,7 +34,6 @@ return {
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
-      context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = {
         "bash",
         "c",
