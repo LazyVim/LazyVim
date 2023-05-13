a213da4430c1012bf7278080cbdc03a236e8230f
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-13 15:45:02 +0200

    feat(noice): use noice's improved entry doc view for cmp

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -218,6 +218,7 @@ return {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
         },
       },
       presets = {
