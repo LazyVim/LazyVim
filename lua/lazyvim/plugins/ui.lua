a43d1b79f29deecc694bd409ab497f8a62091b11
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-18 08:22:08 +0200

    feat(noice): enable the inc_rename preset when inc_rename is availble

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -231,6 +231,7 @@ return {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
+        inc_rename = true,
       },
     },
     -- stylua: ignore
