29554455aa3e7d76377080869f0556389a05e321
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-21 10:52:46 +0200

    feat(tailwind): don't enable tailwind in markdown files

--- lua/lazyvim/plugins/extras/lang/tailwind.lua
+++ lua/lazyvim/plugins/extras/lang/tailwind.lua
@@ -3,7 +3,18 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        tailwindcss = {},
+        tailwindcss = {
+          filetypes_exclude = { "markdown" },
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude, ft)
+          end, tw.default_config.filetypes)
+        end,
       },
     },
   },
