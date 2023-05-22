99785ce36b523cca46c1f2fd6edeaf1189bcd934
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-22 18:56:21 +0200

    fix(persistence): dont add rtp to session

--- lua/lazyvim/plugins/util.lua
+++ lua/lazyvim/plugins/util.lua
@@ -13,7 +13,7 @@ return {
   {
     "folke/persistence.nvim",
     event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
     -- stylua: ignore
     keys = {
       { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
