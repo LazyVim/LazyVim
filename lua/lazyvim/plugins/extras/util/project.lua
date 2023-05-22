f8982332be70499b74623f6ed44c1c58217eb546
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2023-05-22 20:30:18 +0200

    feat(project): add to mini.starter (#640)
    * feat(project): add to mini.starter
    
    * fix(project): make alpha and mini.starter optional

--- lua/lazyvim/plugins/extras/util/project.lua
+++ lua/lazyvim/plugins/extras/util/project.lua
@@ -20,11 +20,26 @@ return {
 
   {
     "goolord/alpha-nvim",
+    optional = true,
     opts = function(_, dashboard)
       local button = dashboard.button("p", " " .. " Projects", ":Telescope projects <CR>")
       button.opts.hl = "AlphaButtons"
       button.opts.hl_shortcut = "AlphaShortcut"
       table.insert(dashboard.section.buttons.val, 4, button)
+    end,
+  },
+  {
+    "echasnovski/mini.starter",
+    optional = true,
+    opts = function(_, opts)
+      local items = {
+        {
+          name = "Projects",
+          action = "Telescope projects",
+          section = string.rep(" ", 22) .. "Telescope",
+        },
+      }
+      vim.list_extend(opts.items, items)
     end,
   },
 }
