163a4f927514ad6ea1aa06db2ad754fb954d87f5
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2023-05-24 08:55:03 +0200

    feat(go): add go lang extras (#795)
    * feat(go): add go lang extras
    
    * fix(go): update plugin
    
    * fix(go): add neotest adapter to adapters
    
    * fix(go): pr changes

---
+++ lua/lazyvim/plugins/extras/lang/go.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          semanticTokens = true,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "mason.nvim",
+        opts = {
+          ensure_installed = { "delve" },
+        },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-go",
+    },
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          -- Here we can set options for neotest-go, e.g.
+          -- args = { "-tags=integration" }
+        },
+      },
+    },
+  },
+}
