b172f47f278df105bbbf60b5c0e411fda01c1819
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-24 10:35:05 +0200

    fix(go): extend mason ensure_installed

--- lua/lazyvim/plugins/extras/lang/go.lua
+++ lua/lazyvim/plugins/extras/lang/go.lua
@@ -26,9 +26,10 @@ return {
     dependencies = {
       {
         "mason.nvim",
-        opts = {
-          ensure_installed = { "delve" },
-        },
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          table.insert(opts.ensure_installed, "delve")
+        end,
       },
     },
   },
