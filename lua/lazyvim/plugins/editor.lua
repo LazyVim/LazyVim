877e63ca8ee5efe1420744f7a843dd4fd0215764
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-25 18:33:54 +0200

    fix(telescope): use last-known telescope commit to work on Neovim 0.8.0 when running in Neovim 0.8.0

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -81,6 +81,7 @@ return {
   -- fuzzy finder
   {
     "nvim-telescope/telescope.nvim",
+    commit = vim.fn.has("nvim-0.9.0") == 0 and "057ee0f8783" or nil,
     cmd = "Telescope",
     version = false, -- telescope did only one release, so use HEAD for now
     keys = {
