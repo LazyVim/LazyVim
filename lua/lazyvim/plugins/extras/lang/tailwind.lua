9c7821e681e264905d7f764aeddda78162a9d867
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-25 21:55:05 +0200

    fix!: remove nvim-colorizer.lua It actually wasn't even enabled and I'll add hipatterns with optional tailwind support soon

--- lua/lazyvim/plugins/extras/lang/tailwind.lua
+++ lua/lazyvim/plugins/extras/lang/tailwind.lua
@@ -19,14 +19,6 @@ return {
     },
   },
   {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
