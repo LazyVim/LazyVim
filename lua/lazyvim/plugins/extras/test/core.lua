efc72b06496f7540085928094808e9a0f45222b9
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-27 09:37:55 +0200

    feat(neotest): added `<leader>tt` and `<leader>tT` to run tests in file or all test files

--- lua/lazyvim/plugins/extras/test/core.lua
+++ lua/lazyvim/plugins/extras/test/core.lua
@@ -22,6 +22,8 @@ return {
       --     args = { "-tags=integration" },
       --   },
       -- },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
       quickfix = {
         open = function()
           if require("lazyvim.util").has("trouble.nvim") then
@@ -73,6 +75,8 @@ return {
     end,
     -- stylua: ignore
     keys = {
+      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+      { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
       { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
       { "<leader>tR", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
