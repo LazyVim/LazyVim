a94ef5953a6f14731d9aea447e769c280db140ab
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-05-29 22:28:04 +0200

    fix(indent-blankline): don't load indent-blankline for nvim-notify

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -187,7 +187,7 @@ return {
     },
     init = function()
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason", "notify" },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
