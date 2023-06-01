08422bf559cb74a2e673e60d551269cbcb2e18df
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-02 00:03:52 +0200

    perf(illuminate): on large files >2000 lines, enable lsp references only

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -303,7 +303,13 @@ return {
   {
     "RRethy/vim-illuminate",
     event = { "BufReadPost", "BufNewFile" },
-    opts = { delay = 200 },
+    opts = {
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { "lsp" },
+      },
+    },
     config = function(_, opts)
       require("illuminate").configure(opts)
 
