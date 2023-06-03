bb4a7fe410d8aaf9507cd2a8ed7c7680852cf297
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-03 10:47:35 +0200

    fix(spectre): make spectre behave with regards to swap files

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -73,6 +73,8 @@ return {
   -- search/replace in multiple files
   {
     "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
     -- stylua: ignore
     keys = {
       { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
