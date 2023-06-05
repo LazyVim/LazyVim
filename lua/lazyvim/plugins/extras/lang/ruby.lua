ac2a93feb12b705051063d32d2d953d582c66d3f
Kevin Robayna <me@kevinrobayna.com>
2023-06-05 08:56:07 +0100

    install solargraph through mason

--- lua/lazyvim/plugins/extras/lang/ruby.lua
+++ lua/lazyvim/plugins/extras/lang/ruby.lua
@@ -4,6 +4,13 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "ruby",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
         "solargraph",
       })
     end,
