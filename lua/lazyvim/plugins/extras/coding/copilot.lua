b929413ddd9afd64e12f0a6ec2969e3692284795
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-05 22:51:55 +0200

    feat(copilot): enable copilot in markdown and help files

--- lua/lazyvim/plugins/extras/coding/copilot.lua
+++ lua/lazyvim/plugins/extras/coding/copilot.lua
@@ -8,6 +8,10 @@ return {
     opts = {
       suggestion = { enabled = false },
       panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
     },
   },
   {
