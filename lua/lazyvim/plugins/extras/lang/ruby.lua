80a43181acacf254c193b7fb597cb7c4cbaae3b8
Kevin Robayna <me@kevinrobayna.com>
2023-06-05 08:46:07 +0100

    remove personal things that other people might not use

--- lua/lazyvim/plugins/extras/lang/ruby.lua
+++ lua/lazyvim/plugins/extras/lang/ruby.lua
@@ -4,13 +4,6 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "ruby",
-      })
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
         "solargraph",
       })
     end,
@@ -42,31 +35,16 @@ return {
     opts = {
       adapters = {
         ["neotest-rspec"] = {
-          rspec_cmd = function()
-            return vim.tbl_flatten({
-              "bundle",
-              "exec",
-              "rspec",
-            })
-          end,
+          -- NOTE: By default neotest-rspec uses the system wide rspec gem isntead of the one through bundler
+          -- rspec_cmd = function()
+          --   return vim.tbl_flatten({
+          --     "bundle",
+          --     "exec",
+          --     "rspec",
+          --   })
+          -- end,
         },
       },
     },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, {
-        nls.builtins.formatting.rubocop.with({
-          command = "bundle",
-          args = vim.list_extend({ "exec", "rubocop" }, nls.builtins.formatting.rubocop._opts.args),
-        }),
-        nls.builtins.diagnostics.rubocop.with({
-          command = "bundle",
-          args = vim.list_extend({ "exec", "rubocop" }, nls.builtins.diagnostics.rubocop._opts.args),
-        }),
-      })
-    end,
   },
 }
