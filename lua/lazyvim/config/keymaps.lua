1897617c98807ce9dd8eaa1f495a2d49a6166316
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-06 07:46:27 +0200

    fix(keymaps): terminal show/hide

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -127,19 +127,19 @@ end
 
 -- floating terminal
 local lazyterm = function() Util.float_term(nil, { cwd = Util.get_root() }) end
-map("n", "<leader>ft",  lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
 map("n", "<leader>fT", function() Util.float_term() end, { desc = "Terminal (cwd)" })
 map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<c-_>", "<c-/>", { remap = true, desc = "which_key_ignore" })
+map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
 
 -- Terminal Mappings
 map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window"})
-map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window"})
-map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window"})
-map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window"})
-map("t", '<C-/>', "<cmd>close<cr>", {desc = "Hide Terminal"})
-map("t", "<c-_>", "<c-/>", { remap = true, desc = "which_key_ignore" })
+map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
 -- windows
 map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
