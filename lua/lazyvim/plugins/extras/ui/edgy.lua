f7b0c853cc9b6bd035a637a85d3979f040e46c6f
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-08 07:53:56 +0200

    fix(edgy): dont layout toggleterm floats

--- lua/lazyvim/plugins/extras/ui/edgy.lua
+++ lua/lazyvim/plugins/extras/ui/edgy.lua
@@ -9,7 +9,13 @@ return {
     },
     opts = {
       bottom = {
-        { ft = "toggleterm", size = { height = 0.4 } },
+        {
+          ft = "toggleterm",
+          size = { height = 0.4 },
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
+          end,
+        },
         {
           ft = "lazyterm",
           title = "LazyTerm",
