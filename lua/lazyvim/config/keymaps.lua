d6bb907cf042463073540f7334e4271a4d2fb108
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-09 16:27:45 +0200

    fix(keymaps): dont map jk in visual. Use x instead of v. Fixes #912

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -16,8 +16,8 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -- better up/down
-map({ "n", "v" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "v" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Move to window using the <ctrl> hjkl keys
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
