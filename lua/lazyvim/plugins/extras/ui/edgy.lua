6ffc89512e998f01b3dc1856a689de6ffdec0b9e
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-09 12:38:33 +0200

    feat(edgy): neo-tree filessystem is now also a pinned view

--- lua/lazyvim/plugins/extras/ui/edgy.lua
+++ lua/lazyvim/plugins/extras/ui/edgy.lua
@@ -50,6 +50,10 @@ return {
           filter = function(buf)
             return vim.b[buf].neo_tree_source == "filesystem"
           end,
+          pinned = true,
+          open = function()
+            vim.api.nvim_input("<esc><space>e")
+          end,
           size = { height = 0.5 },
         },
         {
