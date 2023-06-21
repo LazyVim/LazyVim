318a4c8304091422dfdfcec44dae7a676d999ee1
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-21 10:51:01 +0200

    feat: added extra for flash.nvim

---
+++ lua/lazyvim/plugins/extras/editor/flash.lua
@@ -0,0 +1,26 @@
+return {
+  { "ggandor/leap.nvim", enabled = false },
+  { "ggandor/flit.nvim", enabled = false },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+      },
+      {
+        "S",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+      },
+    },
+  },
+}
