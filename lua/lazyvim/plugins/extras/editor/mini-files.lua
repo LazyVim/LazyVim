e624511fa249bc3af58e61b0ee5fdd4426bc2977
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-22 16:17:52 +0200

    feat(extras): added extra for mini.files

---
+++ lua/lazyvim/plugins/extras/editor/mini-files.lua
@@ -0,0 +1,21 @@
+return {
+  "echasnovski/mini.files",
+  opts = {
+    windows = {
+      preview = true,
+    },
+    options = {
+      -- Whether to use for editing directories
+      -- Disabled by default in LazyVim because neo-tree is used for that
+      use_as_default_explorer = false,
+    },
+  },
+  keys = {
+    {
+      "<leader>fm",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+    },
+  },
+}
