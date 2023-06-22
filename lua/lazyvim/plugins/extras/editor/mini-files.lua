52995227808371a537f6d42ad2f0656571b92299
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-22 16:29:25 +0200

    feat(mini.files): added extra keymap to open mini.files in the cwd

--- lua/lazyvim/plugins/extras/editor/mini-files.lua
+++ lua/lazyvim/plugins/extras/editor/mini-files.lua
@@ -16,6 +16,14 @@ return {
       function()
         require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
       end,
+      desc = "Open mini.files (directory of current file)",
+    },
+    {
+      "<leader>fM",
+      function()
+        require("mini.files").open(vim.loop.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
     },
   },
 }
