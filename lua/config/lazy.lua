c9eb48abfab0778746c61f7fe690b108ab9cac81
Anjo Tadena <tadena.anjo@gmail.com>
2023-06-24 21:49:57 +0800

    Add custom config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,8 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
