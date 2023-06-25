e9358d977a379e54d73dd29691d60f7a51ed6923
jyuan0 <jyuan0@users.noreply.github.com>
2023-06-25 04:05:07 -0400

    fix(keymaps): remove redundant ':' on lazy keymap (#977)

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -89,7 +89,7 @@ map("v", "<", "<gv")
 map("v", ">", ">gv")
 
 -- lazy
-map("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- new file
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
