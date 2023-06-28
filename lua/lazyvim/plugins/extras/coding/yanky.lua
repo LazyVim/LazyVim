e83d2c814f336470fa212cef2f03ceb8ac173012
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-28 12:59:06 +0200

    fix(yanky): use leader-p for telescope integration

--- lua/lazyvim/plugins/extras/coding/yanky.lua
+++ lua/lazyvim/plugins/extras/coding/yanky.lua
@@ -9,7 +9,7 @@ return {
     },
     keys = {
         -- stylua: ignore
-      { "<leader>P", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
+      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
       { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
       { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
       { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
