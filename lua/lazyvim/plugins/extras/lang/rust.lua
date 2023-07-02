272f6af842eb66b6e6cbdea211b17a1196acdff5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-07-02 12:57:47 +0200

    feat(rust): make mason optional

--- lua/lazyvim/plugins/extras/lang/rust.lua
+++ lua/lazyvim/plugins/extras/lang/rust.lua
@@ -32,6 +32,7 @@ return {
   -- Ensure Rust debugger is installed
   {
     "williamboman/mason.nvim",
+    optional = true,
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "codelldb" })
