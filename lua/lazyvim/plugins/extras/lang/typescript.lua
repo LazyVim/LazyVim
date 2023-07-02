4009620651c7422b3ce11484916865073e95574d
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-07-02 13:04:49 +0200

    fix(typescript): use new lsp keymaps config for typescript

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -19,6 +19,10 @@ return {
       servers = {
         ---@type lspconfig.options.tsserver
         tsserver = {
+          keys = {
+            { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+            { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+          },
           settings = {
             typescript = {
               format = {
@@ -42,14 +46,6 @@ return {
       },
       setup = {
         tsserver = function(_, opts)
-          require("lazyvim.util").on_attach(function(client, buffer)
-            if client.name == "tsserver" then
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-            end
-          end)
           require("typescript").setup({ server = opts })
           return true
         end,
