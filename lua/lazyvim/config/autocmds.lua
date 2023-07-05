967ab1965d10477230ca96531ca66bacdec240d6
Sam Amis <s_amis@hotmail.co.uk>
2023-07-05 18:30:49 +0100

    fix(autocmds): jump to last loc in buffer
    vim.api.nvim_win_set_cursor is expecting a window handle but was being
    passed the current buffer's handle instead

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -38,7 +38,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, buf, mark)
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
   end,
 })
