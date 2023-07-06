22c9477b9642b1a8abc303ffcb44c6989c7a5ca2
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-07-06 15:51:29 +0200

    feat(flash): use c-s in the cmdline to toggle flash

--- lua/lazyvim/plugins/extras/editor/flash.lua
+++ lua/lazyvim/plugins/extras/editor/flash.lua
@@ -40,6 +40,14 @@ return {
         end,
         desc = "Treesitter Search",
       },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
     },
   },
   {
