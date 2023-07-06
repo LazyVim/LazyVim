bcff5a19379e32a85049500de420aa24b271fb72
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-07-06 15:35:34 +0200

    fix(util): use spec to check if a plugin is enabled

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -17,7 +17,7 @@ end
 
 ---@param plugin string
 function M.has(plugin)
-  return require("lazy.core.config").plugins[plugin] ~= nil
+  return require("lazy.core.config").spec.plugins[plugin] ~= nil
 end
 
 function M.fg(name)
