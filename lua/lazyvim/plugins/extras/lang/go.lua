40d0023ab6cd5c5bd13a67d2605d02079158aa7c
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-07-14 22:21:57 +0200

    feat(lang): add null-ls tools to mason's ensure_installed

--- lua/lazyvim/plugins/extras/lang/go.lua
+++ lua/lazyvim/plugins/extras/lang/go.lua
@@ -100,7 +100,7 @@ return {
         "mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          table.insert(opts.ensure_installed, "delve")
+          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "gofumpt", "goimports-reviser", "delve" })
         end,
       },
     },
