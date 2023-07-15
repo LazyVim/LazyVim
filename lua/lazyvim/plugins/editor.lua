dbce606fa303580a64483caa02c6e4ed93801f90
Iordanis Petkakis <dpetka2001@gmail.com>
2023-07-15 21:27:57 +0300

    feat(neo-tree): update to v3.x and change `follow_current_file` to a table and enable it

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -5,6 +5,7 @@ return {
   -- file explorer
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     cmd = "Neotree",
     keys = {
       {
@@ -41,7 +42,7 @@ return {
       open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
       filesystem = {
         bind_to_cwd = false,
-        follow_current_file = true,
+        follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
       },
       window = {
