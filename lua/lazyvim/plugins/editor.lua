53cd0b1cff2420cb84c9138770c094592244cad0
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-07-16 08:39:25 +0200

    fix(neo-tree): remove legaccy commands

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -29,7 +29,6 @@ return {
       vim.cmd([[Neotree close]])
     end,
     init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
       if vim.fn.argc() == 1 then
         local stat = vim.loop.fs_stat(vim.fn.argv(0))
         if stat and stat.type == "directory" then
