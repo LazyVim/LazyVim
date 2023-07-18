64bc723df314db3cdb2ece6993d21bd7ab53ab32
Mayrixon <bygsxizhen@gmail.com>
2023-07-18 14:27:01 +0100

    Remove personalise configs.

--- lua/lazyvim/plugins/extras/lang/tex.lua
+++ lua/lazyvim/plugins/extras/lang/tex.lua
@@ -35,8 +35,6 @@ return {
       vim.g.vimtex_complete_enabled = 0 -- use texlab for completion
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-
-      vim.g.vimtex_view_method = vim.fn.has("mac") == 1 and "skim" or "zathura"
     end,
   },
 
