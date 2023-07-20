fa84969916e8d8edf1217092600bd8c15e87da70
Tom MacWright <tom@macwright.com>
2023-07-20 16:12:34 -0400

    Add more documentation

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 local load_textobjects = false
 return {
+  -- Treesitter is a new parser generator tool that we can
+  -- use in Neovim to power faster and more accurate
+  -- syntax highlighting.
   {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
