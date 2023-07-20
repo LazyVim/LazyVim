ff5fca27b9c26301ea7ea03c68f8becec9cce137
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-07-20 12:20:19 -0300

    fix(terraform): configure commentstring

--- lua/lazyvim/plugins/extras/lang/terraform.lua
+++ lua/lazyvim/plugins/extras/lang/terraform.lua
@@ -1,3 +1,9 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "hcl", "terraform" },
+  desc = "terraform/hcl commentstring configuration",
+  command = "setlocal commentstring=#\\ %s",
+})
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
