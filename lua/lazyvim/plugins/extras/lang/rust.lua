5c35604ebf7d969121bc31f21160b1d9c071c51c
Andreas Gerlach <andi@appelgriebsch.org>
2023-07-23 10:27:28 +0200

    fix: rust dap adapter installation

--- lua/lazyvim/plugins/extras/lang/rust.lua
+++ lua/lazyvim/plugins/extras/lang/rust.lua
@@ -31,13 +31,17 @@ return {
 
   -- Ensure Rust debugger is installed
   {
-    "williamboman/mason.nvim",
+    "mfussenegger/nvim-dap",
     optional = true,
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "codelldb" })
-      end
-    end,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "codelldb" })
+        end,
+      },
+    },
   },
 
   {
