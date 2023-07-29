4daf86680c17559a6faf0e0223dc1ca99fe6c96d
Jonas Holst Damtoft <Jomik@users.noreply.github.com>
2023-07-29 15:54:17 +0200

    feat(yaml): support line folding

--- lua/lazyvim/plugins/extras/lang/yaml.lua
+++ lua/lazyvim/plugins/extras/lang/yaml.lua
@@ -21,6 +21,15 @@ return {
       -- make sure mason installs the server
       servers = {
         yamlls = {
+          -- Have to add this for yamlls to understand that we support line folding
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
           -- lazy-load schemastore when needed
           on_new_config = function(new_config)
             new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
