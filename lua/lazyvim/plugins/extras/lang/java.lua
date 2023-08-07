afc680e900460889e94be907e33c3d9206c00a90
aqav <66734646+aqav@users.noreply.github.com>
2023-08-07 22:12:23 +0800

    feat(java): java lang support

--- lua/lazyvim/plugins/extras/lang/java.lua
+++ lua/lazyvim/plugins/extras/lang/java.lua
@@ -50,7 +50,7 @@ return {
     ft = { "java" },
     config = function()
       local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-      local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
+      local workspace_dir = vim.fn.stdpath("data") .. "/lsp/java/" .. project_name
       local config = {
         cmd = {
           "java",
