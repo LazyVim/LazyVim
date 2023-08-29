377c0e397c3585cf7bae6afb8bd279a47954be0a
Jakub Kozłowicz <ja.kozlowicz@gmail.com>
2023-08-29 17:58:42 +0200

    feat(python): add more venv names (#1381)

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -61,7 +61,14 @@ return {
   {
     "linux-cultist/venv-selector.nvim",
     cmd = "VenvSelect",
-    opts = {},
+    opts = {
+      name = {
+        "venv",
+        ".venv",
+        "env",
+        ".env",
+      },
+    },
     keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
   },
 }
