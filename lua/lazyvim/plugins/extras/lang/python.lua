451bde5b41ec410afbb2b5e75e339a3fc44c5587
Xie Zejian <xiezej@gmail.com>
2023-08-29 23:59:18 +0800

    fix(python): disable ruff hover correctly (#1365)
    * fix: disable ruff hover correctly
    
    * better fix

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -14,16 +14,16 @@ return {
         pyright = {},
         ruff_lsp = {},
       },
-    },
-    setup = {
-      ruff_lsp = function()
-        require("lazyvim.util").on_attach(function(client, _)
-          if client.name == "ruff_lsp" then
-            -- Disable hover in favor of Pyright
-            client.server_capabilities.hoverProvider = false
-          end
-        end)
-      end,
+      setup = {
+        ruff_lsp = function()
+          require("lazyvim.util").on_attach(function(client, _)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
     },
   },
   {
