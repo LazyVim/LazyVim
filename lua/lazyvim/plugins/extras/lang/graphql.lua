24e2c24dc2ad4c5339a9de0a7b1aeb281252e3da
Josh Medeski <josh.medeski@gmail.com>
2023-09-01 10:58:36 -0500

    fix: mason ensure installed name

--- lua/lazyvim/plugins/extras/lang/graphql.lua
+++ lua/lazyvim/plugins/extras/lang/graphql.lua
@@ -14,7 +14,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "graphql",
+        "graphql-language-service-cli",
       })
     end,
   },
