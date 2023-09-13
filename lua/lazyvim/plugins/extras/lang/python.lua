d073ece621c698fa86f91051b362bd60c5b5b368
roc <roc@imroc.cc>
2023-09-13 22:04:07 +0800

    fix(python): ensure debugpy installed if using nvim-dap

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -46,16 +46,25 @@ return {
     "mfussenegger/nvim-dap",
     optional = true,
     dependencies = {
-      "mfussenegger/nvim-dap-python",
-      -- stylua: ignore
-      keys = {
-        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
-        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
+      {
+        "mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "debugpy" })
+        end,
       },
-      config = function()
-        local path = require("mason-registry").get_package("debugpy"):get_install_path()
-        require("dap-python").setup(path .. "/venv/bin/python")
-      end,
+      {
+        "mfussenegger/nvim-dap-python",
+        -- stylua: ignore
+        keys = {
+          { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+          { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
+        },
+        config = function()
+          local path = require("mason-registry").get_package("debugpy"):get_install_path()
+          require("dap-python").setup(path .. "/venv/bin/python")
+        end,
+      },
     },
   },
   {
