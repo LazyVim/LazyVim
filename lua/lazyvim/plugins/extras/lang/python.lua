b6c504da6939c19e5399b276a3b290033b68e9bf
Hai Ninh Hoang <34930708+haininhhoang94@users.noreply.github.com>
2023-09-15 16:24:18 +0700

    Update python.lua
     dap_enabled = true, -- Ensure that the venv selector affect PythonPath in nvim-dap as well! (in venv-selector.nvim)

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -68,6 +68,7 @@ return {
         "env",
         ".env",
       },
+      dap_enabled = true, -- Ensure that the venv selector affect PythonPath in nvim-dap as well!
     },
     keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
   },
