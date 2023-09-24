97b26727b9bc902c32d211a2b20b873dc861e4b3
Joey Yu <joey@jyu.dev>
2023-09-24 03:18:23 +0000

    feat(clojure): add clojure support

---
+++ lua/lazyvim/plugins/extras/lang/clojure.lua
@@ -0,0 +1,32 @@
+return {
+    "Olical/conjure",
+    ft = { "clojure", "fennel" }, -- etc
+    -- [Optional] cmp-conjure for cmp
+    dependencies = {
+      {
+        "PaterJason/cmp-conjure",
+        config = function()
+          local cmp = require("cmp")
+          local config = cmp.get_config()
+          table.insert(config.sources, {
+            name = "buffer",
+            option = {
+              sources = {
+                { name = "conjure" },
+              },
+            },
+          })
+          cmp.setup(config)
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
+    end,
+    init = function()
+      -- Set configuration options here
+      vim.g["conjure#debug"] = true
+    end,
+  }
+  
