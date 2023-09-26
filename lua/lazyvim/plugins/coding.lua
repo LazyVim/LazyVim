7c79cfe07314245decd73ec67e7247bba22abaa9
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-09-26 14:16:22 +0200

    fix(mini.comment): better lua pattern for tags. Fixes #1413

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -191,6 +191,7 @@ return {
           }, {}),
           f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
           c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
         },
       }
     end,
