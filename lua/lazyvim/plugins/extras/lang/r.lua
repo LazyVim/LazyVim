7cbc788b4676b17d2161d502c13b81f681d5c779
Maximilian Muecke <muecke.maximilian@gmail.com>
2023-09-27 22:40:42 +0200

    feat: R specific root_dir setup

--- lua/lazyvim/plugins/extras/lang/r.lua
+++ lua/lazyvim/plugins/extras/lang/r.lua
@@ -11,7 +11,13 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        r_language_server = {},
+        r_language_server = {
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern("DESCRIPTION", "NAMESPACE", ".Rbuildignore")(fname)
+              or require("lspconfig.util").find_git_ancestor(fname)
+              or vim.loop.os_homedir()
+          end,
+        },
       },
     },
   },
