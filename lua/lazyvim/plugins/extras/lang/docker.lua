718c5c6036f2aff6534501e151dd3e5ff13a42fa
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-09-29 16:09:33 +0200

    fix(docker): put hadolint in its own spec

--- lua/lazyvim/plugins/extras/lang/docker.lua
+++ lua/lazyvim/plugins/extras/lang/docker.lua
@@ -8,6 +8,13 @@ return {
     end,
   },
   {
+    "mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "hadolint" })
+    end,
+  },
+  {
     "nvimtools/none-ls.nvim",
     optional = true,
     opts = function(_, opts)
@@ -17,13 +24,6 @@ return {
         nls.builtins.diagnostics.hadolint,
       })
     end,
-    dependencies = {
-      "mason.nvim",
-      opts = function(_, opts)
-        opts.ensure_installed = opts.ensure_installed or {}
-        vim.list_extend(opts.ensure_installed, { "hadolint" })
-      end,
-    },
   },
   {
     "mfussenegger/nvim-lint",
