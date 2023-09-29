6b05ed7dcd729322e2e5b6752655a9491cd17f86
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-09-29 12:00:45 +0200

    fix: remove deprecated leap/flit code checks

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -212,30 +212,6 @@ return {
     end,
   },
 
-  -- disable old installations of leap and flit. Optional so it doesn't appear under disabled plugins
-  {
-    "ggandor/leap.nvim",
-    enabled = function()
-      vim.schedule(function()
-        local Config = require("lazy.core.config")
-        if Config.spec.disabled["leap.nvim"] or Config.spec.disabled["flit.nvim"] then
-          require("lazy.core.util").warn(
-            [[`flash.nvim` is now the default **LazyVim** jump plugin.
-**leap.nvim** and **flit.nvim** have been disabled.
-Please remove the plugins from your config.
-If you rather use leap/flit instead, you can add the leap extra:
-`lazyvim.plugins.extras.editor.leap`
-]],
-            { title = "LazyVim" }
-          )
-        end
-      end)
-      return false
-    end,
-    optional = true,
-  },
-  { "ggandor/flit.nvim", enabled = false, optional = true },
-
   -- Flash enhances the built-in search functionality by showing labels
   -- at the end of each match, letting you quickly jump to a specific
   -- location.
