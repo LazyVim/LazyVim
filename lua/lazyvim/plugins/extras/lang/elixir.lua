2ffd629cc7a68e6e4a721ac8babe6b7cf612868e
George Guimarães <george.guimaraes@gmail.com>
2023-10-01 14:48:09 -0300

    feat(elixir): Add credo if none-ls is used (#1546)

--- lua/lazyvim/plugins/extras/lang/elixir.lua
+++ lua/lazyvim/plugins/extras/lang/elixir.lua
@@ -29,4 +29,24 @@ return {
       },
     },
   },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.credo,
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        elixir = { "credo" },
+      },
+    },
+  },
 }
