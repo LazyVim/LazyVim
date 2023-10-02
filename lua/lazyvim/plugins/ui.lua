6e68cfe5e50b53ef77ea05079a01a0a90367bc03
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-10-02 04:45:15 -0500

    fix(indent-blankline): set `config.indent.tab_char`
    See lukas-reineke/indent-blankline.nvim#665.

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -168,7 +168,10 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      indent = { char = "│" },
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
       scope = { enabled = false },
       exclude = {
         filetypes = {
