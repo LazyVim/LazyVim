86d36946727bade57918c6a358c6b6bae52d441b
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-02 20:59:00 +0200

    feat(conform): added new keymap (leader-cF) to format injected languages

--- lua/lazyvim/plugins/extras/formatting/conform.lua
+++ lua/lazyvim/plugins/extras/formatting/conform.lua
@@ -4,6 +4,16 @@ return {
     dependencies = { "mason.nvim" },
     lazy = true,
     cmd = "ConformInfo",
+    keys = {
+      {
+        "<leader>cF",
+        function()
+          require("conform").format({ formatters = { "injected" } })
+        end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
     init = function()
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
       -- Install the conform formatter on VeryLazy
