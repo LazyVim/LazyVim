4ff51cd678343ea32668eaf0c1103caacb4334a2
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-02 11:02:53 +0200

    feat(mini.files): added lsp rename support to mini.files

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -299,4 +299,25 @@ function M.changelog()
   vim.diagnostic.disable(float.buf)
 end
 
+---@param from string
+---@param to string
+function M.on_rename(from, to)
+  local clients = vim.lsp.get_active_clients()
+  for _, client in ipairs(clients) do
+    if client:supports_method("workspace/willRenameFiles") then
+      local resp = client.request_sync("workspace/willRenameFiles", {
+        files = {
+          {
+            oldUri = vim.uri_from_fname(from),
+            newUri = vim.uri_from_fname(to),
+          },
+        },
+      }, 1000)
+      if resp and resp.result ~= nil then
+        vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding)
+      end
+    end
+  end
+end
+
 return M
