d989ecc943b3240db0be3aa0369b96089cee4b40
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-04 18:07:21 +0200

    fix(options): only enable treesitter `foldexpr` on nightly. Fixes #1581

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -65,9 +65,13 @@ vim.opt.foldlevel = 99
 vim.opt.foldtext = "v:lua.require'lazyvim.util.ui'.foldtext()"
 
 if vim.fn.has("nvim-0.9.0") == 1 then
+  vim.opt.statuscolumn = [[%!v:lua.require'lazyvim.util.ui'.statuscolumn()]]
+end
+
+-- HACK: causes freezes on <= 0.9, so only enable on >= 0.10 for now
+if vim.fn.has("nvim-0.10") == 1 then
   vim.opt.foldmethod = "expr"
   vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-  vim.opt.statuscolumn = [[%!v:lua.require'lazyvim.util.ui'.statuscolumn()]]
 else
   vim.opt.foldmethod = "indent"
 end
