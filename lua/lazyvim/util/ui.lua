35053290bf957a230c8758d31ba3997c0699f5f9
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-04 12:47:24 +0200

    fix(ui): use custom fold when treesitter foldtext returns a string

--- lua/lazyvim/util/ui.lua
+++ lua/lazyvim/util/ui.lua
@@ -24,7 +24,7 @@ end
 function M.foldtext()
   local ok = pcall(vim.treesitter.get_parser, vim.api.nvim_get_current_buf())
   local ret = ok and vim.treesitter.foldtext and vim.treesitter.foldtext()
-  if not ret then
+  if not ret or type(ret) == "string" then
     ret = { { vim.api.nvim_buf_get_lines(0, vim.v.lnum - 1, vim.v.lnum, false)[1], {} } }
   end
   table.insert(ret, { " " .. require("lazyvim.config").icons.misc.dots })
