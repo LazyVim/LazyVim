a1c5886947e20059ad7802e71e0a82b413af6657
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-06 09:26:14 +0200

    feat(conform): ignore formatting errors for injected languages and fix condition example

--- lua/lazyvim/plugins/extras/formatting/conform.lua
+++ lua/lazyvim/plugins/extras/formatting/conform.lua
@@ -33,9 +33,10 @@ return {
       -- You can also define any custom formatters here.
       ---@type table<string,table>
       formatters = {
+        injected = { options = { ignore_errors = true } },
         -- -- Example of using dprint only when a dprint.json file is present
         -- dprint = {
-        --   condition = function(ctx)
+        --   condition = function(self, ctx)
         --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
         --   end,
         -- },
