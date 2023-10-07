2c310d6ca51128a8b82d75d9cab5e0c25c38dfd9
Radvil <radvil@@access-mobile.com>
2023-10-08 02:18:43 +0800

    perf: enable lazy_update_context option to update context on "CursorHold" instead of of using default "CursorMove"

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -362,6 +362,7 @@ return {
         highlight = true,
         depth_limit = 5,
         icons = require("lazyvim.config").icons.kinds,
+        lazy_update_context = true,
       }
     end,
   },
