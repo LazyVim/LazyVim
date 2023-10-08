838ce85ad6ae2dc2fa1d7518b0c5eb1eb8fe29b2
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-08 22:59:50 +0200

    fix(bufferline): fix bufferline when restoring a session

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -82,6 +82,17 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd("BufAdd", {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
   },
 
   -- statusline
