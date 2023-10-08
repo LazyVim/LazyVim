a9eaf7bb9a1ee8196293ceb417d7b43481b5fad6
Markis Taylor <m@rkis.net>
2023-10-08 15:34:30 -0400

    fix: update to none-ls/conform

--- lua/lazyvim/plugins/extras/formatting/black.lua
+++ lua/lazyvim/plugins/extras/formatting/black.lua
@@ -6,10 +6,20 @@ return {
     end,
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
+    optional = true,
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.black)
     end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { { "blackd", "black" } },
+      },
+    },
   },
 }
