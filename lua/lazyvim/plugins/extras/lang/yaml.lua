33c677a55e97ee115ad7050856856df7cd96b3e1
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-08 19:29:00 +0200

    fix(yaml): yaml schemas are a dict, not a list, so merge properly. Fixes #1636

--- lua/lazyvim/plugins/extras/lang/yaml.lua
+++ lua/lazyvim/plugins/extras/lang/yaml.lua
@@ -35,8 +35,11 @@ return {
           },
           -- lazy-load schemastore when needed
           on_new_config = function(new_config)
-            new_config.settings.yaml.schemas = new_config.settings.yaml.schemas or {}
-            vim.list_extend(new_config.settings.yaml.schemas, require("schemastore").yaml.schemas())
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
           end,
           settings = {
             redhat = { telemetry = { enabled = false } },
