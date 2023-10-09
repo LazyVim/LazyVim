2c658a374dc9f25ff779605e62572b09130f4801
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-09 11:34:58 +0200

    style(cmp): dont include cmp-nvim-lsp as a dep of lspconfig

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -8,12 +8,6 @@ return {
       { "folke/neodev.nvim", opts = {} },
       "mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-      {
-        "hrsh7th/cmp-nvim-lsp",
-        cond = function()
-          return require("lazyvim.util").has("nvim-cmp")
-        end,
-      },
     },
     ---@class PluginLspOpts
     opts = {
