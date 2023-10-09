6b857f1de648292f2410f9beaa95318bd30454d5
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-09 17:49:00 +0200

    feat(options): set `virtualedit=block` by default

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -43,6 +43,7 @@ opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
