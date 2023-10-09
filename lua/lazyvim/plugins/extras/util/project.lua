0955129796749651e1413006e36e9a1c6d3027f3
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-09 22:11:17 +0200

    fix(project): make sure to load telescope extension after telescope ran setup

--- lua/lazyvim/plugins/extras/util/project.lua
+++ lua/lazyvim/plugins/extras/util/project.lua
@@ -9,7 +9,9 @@ return {
         event = "VeryLazy",
         config = function(_, opts)
           require("project_nvim").setup(opts)
-          require("telescope").load_extension("projects")
+          require("lazyvim.util").on_load("telescope.nvim", function()
+            require("telescope").load_extension("projects")
+          end)
         end,
         keys = {
           { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
