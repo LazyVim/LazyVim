b72a5c2dfa3e6e096b85cf64990aade56e50435c
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-11 14:26:16 +0200

    fix(lualine): when opening nvim with a file, show an empty statusline till lualine loads

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -101,7 +101,13 @@ return {
     event = "VeryLazy",
     init = function()
       vim.g.lualine_laststatus = vim.o.laststatus
-      vim.o.laststatus = 0
+      if vim.fn.argc(-1) > 0 then
+        -- set an empty statusline till lualine loads
+        vim.o.statusline = " "
+      else
+        -- hide the statusline on the starter page
+        vim.o.laststatus = 0
+      end
     end,
     opts = function()
       local icons = require("lazyvim.config").icons
