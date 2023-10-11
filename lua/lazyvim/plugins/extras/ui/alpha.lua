d2d5d9f53c859478ae02533402e74e57386fecc7
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-11 18:10:04 +0200

    feat(dashboard): added LazyExtras shortcut to dashboard/alpha

--- lua/lazyvim/plugins/extras/ui/alpha.lua
+++ lua/lazyvim/plugins/extras/ui/alpha.lua
@@ -28,6 +28,7 @@ return {
         dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
         dashboard.button("c", " " .. " Config", "<cmd> e $MYVIMRC <cr>"),
         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+        dashboard.button("e", " " .. " LazyExtras", "<cmd> LazyExtras <cr>"),
         dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
         dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
       }
--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -383,6 +383,7 @@ return {
             { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
             { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
             { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+            { action = "LazyExtras", desc = " LazyExtras", icon = " ", key = "e" },
             { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
             { action = "qa", desc = " Quit", icon = " ", key = "q" },
           },
