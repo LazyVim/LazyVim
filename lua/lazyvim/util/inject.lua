14f3f036e9223315f310ba9d35182690638e7bd7
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-12 09:42:48 +0200

    feat(inject): util method to get upvalue

--- lua/lazyvim/util/inject.lua
+++ lua/lazyvim/util/inject.lua
@@ -17,4 +17,18 @@ function M.args(fn, wrapper)
   end
 end
 
+function M.get_upvalue(func, name)
+  local i = 1
+  while true do
+    local n, v = debug.getupvalue(func, i)
+    if not n then
+      break
+    end
+    if n == name then
+      return v
+    end
+    i = i + 1
+  end
+end
+
 return M
