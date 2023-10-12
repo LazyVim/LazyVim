8e71968c2bd9e59e535cdac0a99e667f8f120322
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-12 20:43:11 +0200

    fix(nvim-lint): make sure to set custom linters. Fixes #1687

--- lua/lazyvim/plugins/linting.lua
+++ lua/lazyvim/plugins/linting.lua
@@ -29,6 +29,8 @@ return {
       for name, linter in pairs(opts.linters) do
         if type(linter) == "table" and type(lint.linters[name]) == "table" then
           lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
+        else
+          lint.linters[name] = linter
         end
       end
       lint.linters_by_ft = opts.linters_by_ft
