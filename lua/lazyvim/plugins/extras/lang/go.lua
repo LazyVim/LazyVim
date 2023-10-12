8f67d18fb81766510d8c5c7422bda28f5b0bdff1
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-10-12 15:22:29 -0300

    fix: add mason.nvim as dependency of none-ls

--- lua/lazyvim/plugins/extras/lang/go.lua
+++ lua/lazyvim/plugins/extras/lang/go.lua
@@ -92,12 +92,14 @@ return {
   {
     "nvimtools/none-ls.nvim",
     optional = true,
-    {
-      "williamboman/mason.nvim",
-      opts = function(_, opts)
-        opts.ensure_installed = opts.ensure_installed or {}
-        vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
-      end,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+        end,
+      },
     },
     opts = function(_, opts)
       local nls = require("null-ls")
@@ -123,7 +125,7 @@ return {
     optional = true,
     dependencies = {
       {
-        "mason.nvim",
+        "williamboman/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
           vim.list_extend(opts.ensure_installed, { "delve" })
