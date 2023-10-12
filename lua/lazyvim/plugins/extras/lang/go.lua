b875bfc5b54b5fa096e64ebcc9c7e37c9435b4bb
Juan Cruz De La Torre <juancruz.delatorre@paddle.com>
2023-10-12 14:45:29 -0300

    feat(go): only install required packages

--- lua/lazyvim/plugins/extras/lang/go.lua
+++ lua/lazyvim/plugins/extras/lang/go.lua
@@ -83,8 +83,22 @@ return {
   },
   -- Ensure Go tools are installed
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "goimports", "gofumpt" })
+    end,
+  },
+  {
     "nvimtools/none-ls.nvim",
     optional = true,
+    {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
+      end,
+    },
     opts = function(_, opts)
       local nls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources or {}, {
@@ -111,7 +125,7 @@ return {
         "mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "goimports", "delve" })
+          vim.list_extend(opts.ensure_installed, { "delve" })
         end,
       },
       {
