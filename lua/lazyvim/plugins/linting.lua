cdae38ddd44edbf5e124129fc94e9d1038592760
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-12 20:28:56 +0200

    fix(nvim-lint): check on linter name instead of linter. Fixes #1685

--- lua/lazyvim/plugins/linting.lua
+++ lua/lazyvim/plugins/linting.lua
@@ -27,7 +27,7 @@ return {
 
       local lint = require("lint")
       for name, linter in pairs(opts.linters) do
-        if type(linter) == "table" and type(lint.linters) == "table" then
+        if type(linter) == "table" and type(lint.linters[name]) == "table" then
           lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
         end
       end
