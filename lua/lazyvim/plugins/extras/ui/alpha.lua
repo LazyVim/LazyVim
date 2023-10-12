ce74e28464daa7d694f9ab4e41fae3174e30c8b3
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-12 11:20:46 +0200

    feat(starter): added Lazy Extras to `mini.starter`

--- lua/lazyvim/plugins/extras/ui/alpha.lua
+++ lua/lazyvim/plugins/extras/ui/alpha.lua
@@ -21,16 +21,17 @@ return {
       ]]
 
       dashboard.section.header.val = vim.split(logo, "\n")
+      -- stylua: ignore
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
-        dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
-        dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
-        dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
-        dashboard.button("c", " " .. " Config", "<cmd> e $MYVIMRC <cr>"),
+        dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
+        dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
+        dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
+        dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
+        dashboard.button("c", " " .. " Config",          "<cmd> e $MYVIMRC <cr>"),
         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-        dashboard.button("e", " " .. " LazyExtras", "<cmd> LazyExtras <cr>"),
-        dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
-        dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+        dashboard.button("e", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
+        dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
       }
       for _, button in ipairs(dashboard.section.buttons.val) do
         button.opts.hl = "AlphaButtons"
