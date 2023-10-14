5e1a86d3b257db37c8b443deea6ca1fefcb0b1aa
Kevin Traver <kevintraver@kevintraver.com>
2023-10-14 06:40:25 -0600

    feat(toggle): add keymap to toggle treesitter context (#1711)
    * feat: add keymap to toggle treesitter context
    
    * fix: get correct enabled state
    
    ---------
    
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -110,6 +110,22 @@ return {
     event = "LazyFile",
     enabled = true,
     opts = { mode = "cursor" },
+    keys = {
+      {
+        "<leader>ut",
+        function()
+          local Util = require("lazyvim.util")
+          local tsc = require("treesitter-context")
+          tsc.toggle()
+          if Util.inject.get_upvalue(tsc.toggle, "enabled") then
+            Util.info("Enabled Treesitter Context", { title = "Option" })
+          else
+            Util.warn("Disabled Treesitter Context", { title = "Option" })
+          end
+        end,
+        desc = "Toggle Treesitter Context",
+      },
+    },
   },
 
   -- Automatically add closing tags for HTML and JSX
