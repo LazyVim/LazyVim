93f7a72f9191d11e50c2f675137f1e3fe67ea883
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-14 17:36:06 +0200

    feat: add markdown-preview back

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -42,35 +42,23 @@ return {
     },
   },
 
-  -- Markdown preview (requires deno)
+  -- Markdown preview
   {
-    "toppair/peek.nvim",
-    enabled = function()
-      if vim.fn.executable("deno") == 1 then
-        return true
-      end
-      require("lazyvim.util").warn({
-        "`peek.nvim` requires `deno` to be installed.\n",
-        "To hide this message, install `deno` or disable the `toppair/peek.nvim` plugin.",
-      }, { title = "LazyVim Extras `lang.markdown`" })
-      return false
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
     end,
-    build = "deno task --quiet build:fast",
     keys = {
       {
         "<leader>cp",
         ft = "markdown",
-        function()
-          local peek = require("peek")
-          if peek.is_open() then
-            peek.close()
-          else
-            peek.open()
-          end
-        end,
+        "<cmd>MarkdownPreviewToggle<cr>",
         desc = "Peek (Markdown Preview)",
       },
     },
-    opts = { theme = "light" },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
   },
 }
