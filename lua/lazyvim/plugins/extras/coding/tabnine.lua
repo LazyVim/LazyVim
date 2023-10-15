c8e5501ee5ecd1a7b27ff3aa5f41e54c2e98ff0b
Edouard Shamis <47914518+edshamis@users.noreply.github.com>
2023-10-15 23:42:38 +0300

    feat(tabnine): add build cmd for Windows (#1737)
    * feat(tabnine): add build cmd for Windows
    
    * fix(prettier): use prettier instead of prettierd. Too many people get truncated files. Fixes #712. See #1735
    
    * feat: disable kind_filter for markdown and help
    
    * feat(lualine): pretty_path now highlights file basename when modified
    
    * fix(root): dont use single-file lsps for root detection. use workspaces only
    
    * feat(lualine): new root dir component that only shows when cwd != root_dir
    
    * refactor
    
    ---------
    
    Co-authored-by: edshamis <pc>
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>

--- lua/lazyvim/plugins/extras/coding/tabnine.lua
+++ lua/lazyvim/plugins/extras/coding/tabnine.lua
@@ -8,7 +8,7 @@ return {
       -- Add TabNine support, make sure you run :CmpTabnineHub after installation.
       {
         "tzachar/cmp-tabnine",
-        build = "./install.sh",
+        build = Util.is_win() and "pwsh -noni .\\install.ps1" or "./install.sh",
         dependencies = "hrsh7th/nvim-cmp",
         opts = {
           max_lines = 1000,
@@ -16,8 +16,7 @@ return {
           sort = true,
         },
         config = function(_, opts)
-          local tabnine = require("cmp_tabnine.config")
-          tabnine:setup(opts)
+          require("cmp_tabnine.config"):setup(opts)
         end,
       },
     },
