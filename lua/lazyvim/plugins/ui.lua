8c726cd16638f51308f479f26143d94a6c3013ea
Edouard Shamis <47914518+edshamis@users.noreply.github.com>
2023-10-16 08:44:10 +0300

    feat(lualine): use gitsigns for diff source (#1744)
    Co-authored-by: edshamis <pc>

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -184,6 +184,16 @@ return {
                 modified = icons.git.modified,
                 removed = icons.git.removed,
               },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
             },
           },
           lualine_y = {
