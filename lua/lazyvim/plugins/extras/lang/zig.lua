30d08b8178a9e51f2843b87f0acddce5cea706b8
thoxy <thi.hur67@gmail.com>
2023-10-16 20:25:25 +0200

    add dap debug with codelldb

--- lua/lazyvim/plugins/extras/lang/zig.lua
+++ lua/lazyvim/plugins/extras/lang/zig.lua
@@ -54,4 +54,38 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      dap.configurations.zig = {
+        {
+          name = "Zig Run",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            os.execute("zig build")
+            local command = "find ! -type d -path './zig-out/bin/*' | grep -v 'Test' | sed 's#.*/##'"
+            local bin_location = io.popen(command, "r")
+            if bin_location ~= nil then
+              return "zig-out/bin/" .. bin_location:read("*a"):gsub("[\n\r]", "")
+            else
+              return ""
+            end
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = function()
+            local argv = {}
+            arg = vim.fn.input(string.format("Arguments: "))
+            for a in string.gmatch(arg, "%S+") do
+              table.insert(argv, a)
+            end
+            return argv
+          end,
+        },
+      }
+    end,
+  },
 }
