6392284de86858fc2456e5e872820e0c4b0588ae
Phúc Lê Khắc <phuc.lkh@gmail.com>
2023-10-17 18:35:11 +0200

    feat(fold): expand fold to previous line when reopen buffer

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -42,6 +42,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      vim.cmd("normal! zv")
     end
   end,
 })
