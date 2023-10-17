5c1656729aeb39fbb8dc29d4f3d6d86d0836f8b4
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-17 18:28:06 +0200

    fix(dashboard): config shortcut when opening dashboard again. Fixes #1768

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -371,15 +371,15 @@ return {
           header = vim.split(logo, "\n"),
           -- stylua: ignore
           center = {
-            { action = "Telescope find_files",              desc = " Find file",       icon = " ", key = "f" },
-            { action = "ene | startinsert",                 desc = " New file",        icon = " ", key = "n" },
-            { action = "Telescope oldfiles",                desc = " Recent files",    icon = " ", key = "r" },
-            { action = "Telescope live_grep",               desc = " Find text",       icon = " ", key = "g" },
-            { action = Util.telescope.config_files(),       desc = " Config",          icon = " ", key = "c" },
-            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-            { action = "LazyExtras",                        desc = " Lazy Extras",     icon = " ", key = "x" },
-            { action = "Lazy",                              desc = " Lazy",            icon = "󰒲 ", key = "l" },
-            { action = "qa",                                desc = " Quit",            icon = " ", key = "q" },
+            { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+            { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+            { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+            { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+            { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
           },
           footer = function()
             local stats = require("lazy").stats()
