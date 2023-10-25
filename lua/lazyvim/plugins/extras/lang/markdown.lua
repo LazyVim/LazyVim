b651560ad0ee750c96bca6866cfdc008e75d397c
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-25 15:40:57 +0200

    perf(markdown): prevent headlines.nvim slowing down initial rendering with `nvim README.md`

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -79,5 +79,12 @@ return {
       return opts
     end,
     ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      -- PERF: schedule to prevent headlines slowing down opening a file
+      vim.schedule(function()
+        require("headlines").setup(opts)
+        require("headlines").refresh()
+      end)
+    end,
   },
 }
