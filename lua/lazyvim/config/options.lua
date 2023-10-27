e01ad513aa4f50bdb385a7454c9e1ec3a0d5dc94
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-10-27 18:34:33 +0200

    fix(sessions): added folds to sessions

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -34,7 +34,7 @@ opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.scrolloff = 4 -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" }
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
