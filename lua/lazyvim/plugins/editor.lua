39861698235c03d30096b3fb315ce38eeaef95e2
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-11-04 10:14:31 +0100

    fix(spectre): don't build nvim-spectre

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -98,6 +98,7 @@ return {
   -- search/replace in multiple files
   {
     "nvim-pack/nvim-spectre",
+    build = false,
     cmd = "Spectre",
     opts = { open_cmd = "noswapfile vnew" },
     -- stylua: ignore
