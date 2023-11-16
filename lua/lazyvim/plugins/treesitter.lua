02d8ad971546c6cf431bccbcf6a8182a7109830c
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2023-11-16 19:52:11 +0100

    fix typo

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -11,7 +11,7 @@ return {
       -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
       -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
       -- during startup.
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
