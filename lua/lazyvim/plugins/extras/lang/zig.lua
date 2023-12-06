71bb27a0b8e416b7185cba7e95b9717905a1f735
Thoxy67 <the.thi67@gmail.com>
2023-12-06 08:57:30 +0100

    add zigfmt none-ls formating

--- lua/lazyvim/plugins/extras/lang/zig.lua
+++ lua/lazyvim/plugins/extras/lang/zig.lua
@@ -88,4 +88,14 @@ return {
       }
     end,
   },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.zigfmt,
+      })
+    end,
+  },
 }
