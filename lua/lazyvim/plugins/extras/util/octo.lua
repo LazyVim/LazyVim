6a6a65fe3ca4b517dc084b892e237a3d4e7b5b89
Andreas Gerlach <andi@appelgriebsch.org>
2023-12-31 14:55:10 +0100

    feat(util): octo.nvim extension fixes #2276

---
+++ lua/lazyvim/plugins/extras/util/octo.lua
@@ -0,0 +1,25 @@
+return {
+
+  -- Ensure GH tool is installed
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "gh" })
+    end,
+  },
+
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      picker = "telescope",
+    }
+  },
+
+}
