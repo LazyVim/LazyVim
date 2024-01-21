823d39646c019d2eb7e6a68c29e2cb37f9333a44
Pablo <52180403+pablos123@users.noreply.github.com>
2024-01-21 16:10:18 -0300

    docs: typo in init.lua (#2365)

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -94,7 +94,7 @@ return {
       -- setup autoformat
       Util.format.register(Util.lsp.formatter())
 
-      -- deprectaed options
+      -- deprecated options
       if opts.autoformat ~= nil then
         vim.g.autoformat = opts.autoformat
         Util.deprecate("nvim-lspconfig.opts.autoformat", "vim.g.autoformat")
