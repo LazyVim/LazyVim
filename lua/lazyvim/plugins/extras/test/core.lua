e29e891faaec25ae90b46998bb9b91e642e73142
Samuel Abreu <sdepaula@gmail.com>
2024-01-21 15:37:38 -0300

    feat(test): Add <leader>tl to neotest.run_last() (#1968)

--- lua/lazyvim/plugins/extras/test/core.lua
+++ lua/lazyvim/plugins/extras/test/core.lua
@@ -110,6 +110,7 @@ return {
       { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
       { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
       { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
+      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
       { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
       { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
