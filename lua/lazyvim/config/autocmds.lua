8e2046f3d02a80558802b6cbcd4c8f7d44f7b6b4
Frederick Zhang <frederick888@tsundere.moe>
2024-01-22 06:20:54 +1100

    fix(autocmds): avoid checktime when buftype=nofile (#2270)
    It triggered an 'E11: Invalid in command-line window; <CR> executes,
    CTRL-C quits' error when Neovim was in Command Line window and
    FocusGained.

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -7,7 +7,11 @@ end
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
   group = augroup("checktime"),
-  command = "checktime",
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd('checktime')
+    end
+  end
 })
 
 -- Highlight on yank
