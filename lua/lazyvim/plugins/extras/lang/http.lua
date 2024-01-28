e4e7b02fa2011fd83967711bf998305fdda88205
Peter Benjamin <petermbenjamin@gmail.com>
2024-01-28 14:15:21 -0800

    feat(http): add http extra lang

---
+++ lua/lazyvim/plugins/extras/lang/http.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "http",
+        "json",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "jq" })
+    end,
+  },
+}
