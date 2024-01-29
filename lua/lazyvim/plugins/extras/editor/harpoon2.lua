18abcba900ef8a075098ef8d62e39cd7ae4d4840
Nybkox <nybkox@gmail.com>
2024-01-29 16:46:51 +0100

    chore: formatting

--- lua/lazyvim/plugins/extras/editor/harpoon2.lua
+++ lua/lazyvim/plugins/extras/editor/harpoon2.lua
@@ -1,11 +1,11 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-opts = {
-  menu = {
-    width = vim.api.nvim_win_get_width(0) - 4,
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
   },
-},
   keys = {
     {
       "<leader>H",
