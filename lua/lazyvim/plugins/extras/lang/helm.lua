6a9e097bd2fd984af2ecd8700deb677e3d0cdd25
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-02 21:55:51 -0800

    fix(helm): remove docker compose lsp overrides
    Since docker compose LSP does not seem to be triggered or started by
    opening *.yaml helm files, the config overrides were removed to simplify
    Helm config.

--- lua/lazyvim/plugins/extras/lang/helm.lua
+++ lua/lazyvim/plugins/extras/lang/helm.lua
@@ -1,11 +1,4 @@
 require("lazyvim.util").lsp.on_attach(function(client, buffer)
-  if client.name == "docker_compose_language_service" then
-    if vim.api.nvim_get_option_value("filetype", { buf = buffer }) == "helm" then
-      vim.schedule(function()
-        vim.cmd("LspStop ++force docker_compose_language_service")
-      end)
-    end
-  end
   if client.name == "yamlls" then
     if vim.api.nvim_get_option_value("filetype", { buf = buffer }) == "helm" then
       vim.schedule(function()
@@ -16,13 +9,12 @@ require("lazyvim.util").lsp.on_attach(function(client, buffer)
 end)
 
 return {
-  "towolf/vim-helm",
+  { "towolf/vim-helm", ft = "helm" },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         yamlls = {},
-        docker_compose_language_service = {},
         helm_ls = {},
       },
     },
