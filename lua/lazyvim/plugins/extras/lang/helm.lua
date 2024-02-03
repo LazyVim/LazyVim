b68a00ca98aee47e340df89e773a9a63bda8a54b
Peter Benjamin <petermbenjamin@gmail.com>
2024-02-02 22:22:46 -0800

    fix(helm): ensure mason installs helm lsp

--- lua/lazyvim/plugins/extras/lang/helm.lua
+++ lua/lazyvim/plugins/extras/lang/helm.lua
@@ -19,4 +19,12 @@ return {
       },
     },
   },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "helm-ls",
+      })
+    end,
+  },
 }
