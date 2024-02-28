8c4e16cf64479de557b4533be60a35f8c30f4c15
Luckas <luckasranarison@gmail.com>
2024-02-28 15:21:02 +0300

    fix(dot): remove hyprlang plugin

--- lua/lazyvim/plugins/extras/util/dot.lua
+++ lua/lazyvim/plugins/extras/util/dot.lua
@@ -7,32 +7,6 @@ local function have(path)
 end
 
 return {
-
-  -- Add Hyprland Parser
-  {
-    "luckasRanarison/tree-sitter-hyprlang",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        require("nvim-treesitter.parsers").get_parser_configs().hyprlang = {
-          install_info = {
-            url = "https://github.com/luckasRanarison/tree-sitter-hyprlang",
-            files = { "src/parser.c" },
-            branch = "master",
-          },
-          filetype = "hyprlang",
-        }
-
-        opts.ensure_installed = opts.ensure_installed or {}
-        vim.list_extend(opts.ensure_installed, { "hyprlang" })
-      end,
-    },
-    enabled = function()
-      return have("hypr")
-    end,
-    event = "BufRead */hypr/*.conf",
-  },
-
   -- add some stuff to treesitter
   {
     "nvim-treesitter/nvim-treesitter",
@@ -49,10 +23,15 @@ return {
           [".*/waybar/config"] = "jsonc",
           [".*/mako/config"] = "dosini",
           [".*/kitty/*.conf"] = "bash",
+          [".*/hypr/.*%.conf"] = "hyprlang",
         },
       })
 
       add("git_config")
+
+      if have("hypr") then
+        add("hyprlang")
+      end
 
       if have("fish") then
         add("fish")
