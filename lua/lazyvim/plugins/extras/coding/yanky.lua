d9f5e6db165167c80fe80ede68b0e1815a81cc43
MoetaYuko <loli@yuko.moe>
2024-03-07 18:40:51 +0800

    fix(yanky): properly disable sqlite.lua on Windows (#2543)
    When directly modifying its `enabled` property, the plugin will remain
    disabled even if required by another plugin.

--- lua/lazyvim/plugins/extras/coding/yanky.lua
+++ lua/lazyvim/plugins/extras/coding/yanky.lua
@@ -2,7 +2,7 @@ return {
   -- better yank/paste
   {
     "gbprod/yanky.nvim",
-    dependencies = { { "kkharji/sqlite.lua", enabled = not jit.os:find("Windows") } },
+    dependencies = not jit.os:find("Windows") and { "kkharji/sqlite.lua" } or {},
     opts = {
       highlight = { timer = 250 },
       ring = { storage = jit.os:find("Windows") and "shada" or "sqlite" },
