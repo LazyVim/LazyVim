39fbfd79eb85ff49da89395dcf927cbd4c87e40c
Iordanis Petkakis <dpetka2001@gmail.com>
2024-03-07 17:34:49 +0200

    fix(lang): Change `rustacean` keymaps to `vim.keymap.set` instead of `which-key` (#2660)
    Based on this [comment](https://github.com/LazyVim/LazyVim/pull/2198#pullrequestreview-1873658780).

--- lua/lazyvim/plugins/extras/lang/rust.lua
+++ lua/lazyvim/plugins/extras/lang/rust.lua
@@ -46,13 +46,11 @@ return {
     ft = { "rust" },
     opts = {
       server = {
-        on_attach = function(client, bufnr)
-          -- register which-key mappings
-          local wk = require("which-key")
-          wk.register({
-            ["<leader>cR"] = { function() vim.cmd.RustLsp("codeAction") end, "Code Action" },
-            ["<leader>dr"] = { function() vim.cmd.RustLsp("debuggables") end, "Rust debuggables" },
-          }, { mode = "n", buffer = bufnr })
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function() vim.cmd.RustLsp("codeAction") end,
+            { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function() vim.cmd.RustLsp("debuggables") end,
+            { desc = "Rust debuggables", buffer = bufnr })
         end,
         default_settings = {
           -- rust-analyzer language server configuration
