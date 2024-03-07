40df8ce1ea2b7d0c575a4a526197cca6adaaef27
Wellington Lopes Souza <wellington.lopess@mercadolivre.com>
2024-03-07 13:49:06 -0300

    fix(go): adding opts recursive_run (#2520)
    Adding opts recursive_run to fix the error 'no Go files in /path/project.' This issue is caused by a recent change in the 'nvim-neotest/neotest-go' project, which now [defaults to non-recursive behavior](https://github.com/nvim-neotest/neotest-go/pull/72)

--- lua/lazyvim/plugins/extras/lang/go.lua
+++ lua/lazyvim/plugins/extras/lang/go.lua
@@ -148,6 +148,7 @@ return {
         ["neotest-go"] = {
           -- Here we can set options for neotest-go, e.g.
           -- args = { "-tags=integration" }
+          recursive_run = true,
         },
       },
     },
