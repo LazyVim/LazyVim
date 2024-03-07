be1c1349a21ffcb0161608e550e2b1e282b00432
Nybkox <nybkox@gmail.com>
2024-03-07 12:07:38 +0100

    feat(extras): add harpoon2 (#2455)
    * feat(extras): add harpoon2
    
    * Update lua/lazyvim/plugins/extras/editor/harpoon2.lua
    
    Co-authored-by: Iordanis Petkakis <dpetka2001@gmail.com>
    
    * chore: formatting
    
    ---------
    
    Co-authored-by: Iordanis Petkakis <dpetka2001@gmail.com>

---
+++ lua/lazyvim/plugins/extras/editor/harpoon2.lua
@@ -0,0 +1,61 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+  },
+  keys = {
+    {
+      "<leader>H",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Harpoon file",
+    },
+    {
+      "<leader>h",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Harpoon quick menu",
+    },
+    {
+      "<leader>1",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Harpoon to file 1",
+    },
+    {
+      "<leader>2",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Harpoon to file 2",
+    },
+    {
+      "<leader>3",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Harpoon to file 3",
+    },
+    {
+      "<leader>4",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Harpoon to file 4",
+    },
+    {
+      "<leader>5",
+      function()
+        require("harpoon"):list():select(5)
+      end,
+      desc = "Harpoon to file 5",
+    },
+  },
+}
