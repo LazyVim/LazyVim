51885595765944a1b20f574ababbe41e27102cbf
Adam Stracener <adam@adamstracener.com>
2024-03-08 16:41:14 -0600

    Fixed terraform.lua issues. I'm bad.

--- lua/lazyvim/plugins/extras/lang/terraform.lua
+++ lua/lazyvim/plugins/extras/lang/terraform.lua
@@ -52,4 +52,25 @@ return {
       },
     },
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "ANGkeith/telescope-terraform-doc.nvim",
+        config = function()
+          Util.on_load("telescope.nvim", function()
+            require("telescope").load_extension("terraform_doc")
+          end)
+        end,
+      },
+      {
+        "cappyzawa/telescope-terraform.nvim",
+        config = function()
+          Util.on_load("telescope.nvim", function()
+            require("telescope").load_extension("terraform")
+          end)
+        end,
+      },
+    },
+  },
 }
