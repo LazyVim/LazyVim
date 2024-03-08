935bc2283f9968a8fb1fc2466a14618a838cb904
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-08 11:51:18 +0100

    fix(extras): prevent duplicate extra imports

--- lua/lazyvim/plugins/xtras.lua
+++ lua/lazyvim/plugins/xtras.lua
@@ -8,6 +8,17 @@ local prios = {
   ["lazyvim.plugins.extras.dap.core"] = 1,
 }
 
+local used = {} ---@type table<string, boolean>
+
+---@type string[]
+Config.json.data.extras = vim.tbl_filter(function(extra)
+  if used[extra] then
+    return false
+  end
+  used[extra] = true
+  return true
+end, Config.json.data.extras)
+
 table.sort(Config.json.data.extras, function(a, b)
   local pa = prios[a] or 10
   local pb = prios[b] or 10
