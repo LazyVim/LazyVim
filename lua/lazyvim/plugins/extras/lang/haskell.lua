ebdb8bdae1ae850a7bf69cf8c81cc1211f583784
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2024-03-08 02:14:14 -0500

    fix(haskell): add filetype to haskell-snippets for lazy load

--- lua/lazyvim/plugins/extras/lang/haskell.lua
+++ lua/lazyvim/plugins/extras/lang/haskell.lua
@@ -63,6 +63,7 @@ return {
   {
     "mrcjkb/haskell-snippets.nvim",
     dependencies = { "L3MON4D3/LuaSnip", },
+    ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
     config = function()
       local haskell_snippets = require('haskell-snippets').all
       require('luasnip').add_snippets('haskell', haskell_snippets, { key = 'haskell' })
