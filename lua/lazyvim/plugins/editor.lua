cd359d892c83c8678fc1afdd7d9777841a54617d
PostCyberPunk <lyhguest2@outlook.com>
2024-03-09 09:56:45 +0800

    fix(neo-tree): setreg option should use table instead of string

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -65,7 +65,7 @@ return {
             function(state)
               local node = state.tree:get_node()
               local path = node:get_id()
-              vim.fn.setreg("+", path, "c")
+              vim.fn.setreg("+", path, { "c" })
             end,
             desc = "copy path to clipboard",
           },
