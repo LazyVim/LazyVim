1c9f4160a22dbdca2db36169460fcf28641607e7
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-11 23:13:48 +0100

    fix(options): dont set clipboard in an SSH session, so that OSC52 can be enabled (when supported)

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -15,7 +15,13 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
+
+if not vim.env.SSH_TTY then
+  -- only set clipboard if not in ssh, to make sure the OSC 52
+  -- integration works automatically. Requires Neovim >= 0.10.0
+  opt.clipboard = "unnamedplus" -- Sync with system clipboard
+end
+
 opt.completeopt = "menu,menuone,noselect"
 opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
