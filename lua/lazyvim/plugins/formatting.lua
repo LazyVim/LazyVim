afca0b7876abf9022ee0754270f603e981ea42a5
Iordanis Petkakis <dpetka2001@gmail.com>
2024-03-14 15:14:41 +0200

    fix(conform): make `lsp_fallback` option user configurable (#2616)

--- lua/lazyvim/plugins/formatting.lua
+++ lua/lazyvim/plugins/formatting.lua
@@ -84,6 +84,7 @@ return {
           timeout_ms = 3000,
           async = false, -- not recommended to change
           quiet = false, -- not recommended to change
+          lsp_fallback = true, -- not recommended to change
         },
         ---@type table<string, conform.FormatterUnit[]>
         formatters_by_ft = {
