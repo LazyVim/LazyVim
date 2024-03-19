393a9c7c7ab2c37a69a9b5da609c27fa2fd3816c
Stefan Krüger <stefan@standa.de>
2024-03-19 22:02:22 +0100

    fix(neotest): add required nvim-nio to test and dap extra (neotest and nvim-dap-ui respectively) (#2763)
    * Adding required nvim-nio to neotest in test-core
    
    neotest now requires nvim-neotest/nvim-nio to be
    installed as per BREAKING CHANGE: https://github.com/nvim-neotest/neotest/pull/337
    
    * Adding nvim-nio as dependency
    
    nvim-dap-ui now requires  nvim-neotest/nvim-nio as a dependency as of recent changes

--- lua/lazyvim/plugins/extras/dap/core.lua
+++ lua/lazyvim/plugins/extras/dap/core.lua
@@ -18,6 +18,7 @@ return {
     -- fancy UI for the debugger
     {
       "rcarriga/nvim-dap-ui",
+      dependencies = { "nvim-neotest/nvim-nio" },
       -- stylua: ignore
       keys = {
         { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
