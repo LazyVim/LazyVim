4187417158a22ff80a059a73f2eddb113f59c113
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-20 22:25:59 +0100

    fix(python): basedpyright is now supported by mason-lspconfig

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -13,18 +13,6 @@ return {
       end
     end,
   },
-  -- basedpyright support.
-  -- Remove when merged: https://github.com/williamboman/mason-lspconfig.nvim/pull/379
-  {
-    "williamboman/mason.nvim",
-    optional = true,
-    opts = function(_, opts)
-      if vim.g.lazyvim_python_lsp == "basedpyright" then
-        opts.ensure_installed = opts.ensure_installed or {}
-        table.insert(opts.ensure_installed, "basedpyright")
-      end
-    end,
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
