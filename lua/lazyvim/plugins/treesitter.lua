dacd3e0708e511354b3afe8c3bea4d5be870648b
Phúc Lê Khắc <phuc.lkh@gmail.com>
2024-03-21 14:56:26 +0700

    feat(treesitter): `v` for incremental node select

--- lua/lazyvim/plugins/treesitter.lua
+++ lua/lazyvim/plugins/treesitter.lua
@@ -81,8 +81,7 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
+          node_incremental = "v",
           scope_incremental = false,
           node_decremental = "<bs>",
         },
