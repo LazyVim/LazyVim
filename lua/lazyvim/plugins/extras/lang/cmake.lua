69aedc265724c71711753333e3b7506fbeab78ef
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-22 10:23:09 +0100

    fix(cmake): lazy load cmake-tools. Fixes #2767

--- lua/lazyvim/plugins/extras/lang/cmake.lua
+++ lua/lazyvim/plugins/extras/lang/cmake.lua
@@ -43,7 +43,25 @@ return {
   },
   {
     "Civitasv/cmake-tools.nvim",
+    lazy = true,
+    init = function()
+      local loaded = false
+      local function check()
+        local cwd = vim.uv.cwd()
+        if vim.fn.filereadable(cwd .. "/CMakeLists.txt") == 1 then
+          require("lazy").load({ plugins = { "cmake-tools.nvim" } })
+          loaded = true
+        end
+      end
+      check()
+      vim.api.nvim_create_autocmd("DirChanged", {
+        callback = function()
+          if not loaded then
+            check()
+          end
+        end,
+      })
+    end,
     opts = {},
-    event = "LazyFile",
   },
 }
