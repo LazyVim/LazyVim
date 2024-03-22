7ab628501150b6dbaa6daae4a8ac7b864318677f
Sergey Kochetkov <skoch13@pm.me>
2024-03-22 10:38:10 +0100

    feat(options): Disable foldtext for nightly (#2447)
    * chores(format.lua): Fix typo
    
    * Change gitsigns.nvim hunk preview shortcut
    
    Change `preview_hunk` to recently added `preview_hunk_inline` command
    
    * feat(options): remove redundant commented line
    
    * feat(options): use native folding
    
    see https://github.com/neovim/neovim/pull/20750

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -74,7 +74,6 @@ opt.wrap = false -- Disable line wrap
 opt.fillchars = {
   foldopen = "",
   foldclose = "",
-  -- fold = "⸱",
   fold = " ",
   foldsep = " ",
   diff = "╱",
@@ -87,16 +86,18 @@ end
 
 -- Folding
 vim.opt.foldlevel = 99
-vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
 
 if vim.fn.has("nvim-0.9.0") == 1 then
   vim.opt.statuscolumn = [[%!v:lua.require'lazyvim.util'.ui.statuscolumn()]]
+  vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
 end
 
 -- HACK: causes freezes on <= 0.9, so only enable on >= 0.10 for now
 if vim.fn.has("nvim-0.10") == 1 then
   vim.opt.foldmethod = "expr"
   vim.opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+  vim.opt.foldtext = ""
+  vim.opt.fillchars = "fold: "
 else
   vim.opt.foldmethod = "indent"
 end
