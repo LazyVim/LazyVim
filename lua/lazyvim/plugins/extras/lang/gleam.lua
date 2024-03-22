7fb31270e330f03b19e031d34a010f091c672bf0
Jiri Luzny <jiri.luzny@gmail.com>
2024-03-22 07:26:19 +0100

    Addd Gleam lang support

---
+++ lua/lazyvim/plugins/extras/lang/gleam.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "gleam" })
+      end
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      require("lspconfig").gleam.setup({})
+    end,
+  },
+
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      languages = {
+        gleam = "// %s",
+      },
+    },
+  },
+}
