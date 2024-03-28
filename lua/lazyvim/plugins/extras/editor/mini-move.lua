3166236ff88a13e7e5b7579ae780b682f2356d62
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-29 00:21:52 +0100

    fix(mini.move): default mini.move setup already maps the correct keys

--- lua/lazyvim/plugins/extras/editor/mini-move.lua
+++ lua/lazyvim/plugins/extras/editor/mini-move.lua
@@ -3,30 +3,5 @@ return {
     "echasnovski/mini.move",
     event = "VeryLazy",
     opts = {},
-    keys = function()
-      local ret = {}
-      local directions = { "left", "down", "up", "right" }
-      local keys = { "h", "j", "k", "l" }
-      local move = require("mini.move")
-      for i, dir in ipairs(directions) do
-        ret[#ret + 1] = {
-          "<A-" .. keys[i] .. ">",
-          mode = { "i", "n" },
-          function()
-            move.move_line(dir)
-          end,
-        }
-      end
-      for i, dir in ipairs(directions) do
-        ret[#ret + 1] = {
-          "<A-" .. keys[i] .. ">",
-          mode = { "v" },
-          function()
-            move.move_selection(dir)
-          end,
-        }
-      end
-      return ret
-    end,
   },
 }
