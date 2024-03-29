754467525b420c85150f13b2cd9e280570c7556c
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-03-29 07:27:43 +0100

    feat(trouble): added `<leader>cS` to open a trouble list of all references / definitions / ... of the item under the cursor

--- lua/lazyvim/plugins/extras/editor/trouble-v3.lua
+++ lua/lazyvim/plugins/extras/editor/trouble-v3.lua
@@ -20,6 +20,11 @@ return {
       { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
       { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
       { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+      {
+        "<leader>cS",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP references/definitions/... (Trouble)",
+      },
       { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
       { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
     },
