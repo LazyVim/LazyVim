22e271deaa48a75a0924cca050abaad7720249de
cbosvik <132846580+cbosvik@users.noreply.github.com>
2024-04-01 23:34:07 +0200

    Update jsonnet.lua

--- lua/lazyvim/plugins/extras/lang/jsonnet.lua
+++ lua/lazyvim/plugins/extras/lang/jsonnet.lua
@@ -4,7 +4,6 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- Ensure mason installs the server
         jsonnet_ls = {},
       },
     },
@@ -15,14 +14,6 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "jsonnet" })
       end
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "jsonnet-language-server",
-      })
     end,
   },
 }
