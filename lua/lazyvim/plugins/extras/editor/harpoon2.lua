927ff0f409ff54b8205e954cb8aebf4063c967cb
Rabin Adhikari <rabin.adk1@gmail.com>
2024-04-04 16:31:25 +0545

    Harpoon2: Use `add` to add the current buffer to the list instead of `append`
    Use `add` to add the current buffer to the list instead of `append.` The plugin warns about the deprecation of the latter in favor of the previous one.

--- lua/lazyvim/plugins/extras/editor/harpoon2.lua
+++ lua/lazyvim/plugins/extras/editor/harpoon2.lua
@@ -11,7 +11,7 @@ return {
       {
         "<leader>H",
         function()
-          require("harpoon"):list():append()
+          require("harpoon"):list():add()
         end,
         desc = "Harpoon File",
       },
