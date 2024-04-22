7e3207775341ced13a41a3de6a07c10ab5f6e7d2
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-04-22 09:45:00 +0200

    fix: no need for plenary deps here

--- lua/lazyvim/plugins/extras/editor/harpoon2.lua
+++ lua/lazyvim/plugins/extras/editor/harpoon2.lua
@@ -1,7 +1,6 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     menu = {
       width = vim.api.nvim_win_get_width(0) - 4,
