0fa63603afcb9b478c49a54d94314d20af00ae95
Iordanis Petkakis <dpetka2001@gmail.com>
2024-04-22 10:56:00 +0300

    fix(trouble_v3): keymap `previous` is deprecated, use `prev` instead (#2968)

--- lua/lazyvim/plugins/extras/editor/trouble-v3.lua
+++ lua/lazyvim/plugins/extras/editor/trouble-v3.lua
@@ -29,6 +29,20 @@ return {
       },
       { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
       { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+      {
+        "[q",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").prev({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cprev)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = "Previous Trouble/Quickfix Item",
+      },
     },
   },
 
