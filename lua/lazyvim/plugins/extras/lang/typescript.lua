41eae677c53ade18b44e53b2e6668e673e701c59
Avinash Thakur <iamavinashthakur.at@gmail.com>
2024-05-01 20:51:13 +0530

    feat(typescript): add "node" debug adapter
    add "node" debug adapter for compatibility with .vscode/launch.json

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -1,3 +1,7 @@
+local debugtypePreferred = {
+  ['node'] = 'pwa-node',
+}
+
 return {
 
   -- add typescript to treesitter
@@ -87,6 +91,18 @@ return {
           },
         }
       end
+      if not dap.adapters['node'] then
+        dap.adapters['node'] = function (cb, config)
+          config.type = debugtypePreferred[config.type] or config.type
+          local nativeAdapter = dap.adapters['pwa-node']
+          if type(nativeAdapter) == 'function' then
+            nativeAdapter(cb, config )
+          else
+            cb(nativeAdapter)
+          end
+        end
+      end
+
       for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
         if not dap.configurations[language] then
           dap.configurations[language] = {
