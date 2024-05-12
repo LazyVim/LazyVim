30ce84f7a7fb4e88b47ddf2f569b2ea9b7d56717
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2024-05-12 11:08:55 +0300

    fix(neo-tree): correctly set up `cwd` (#3097)
    * fix(neo-tree): correctly set up `cwd`
    
    * refactor: cleanup
    
    ---------
    
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>

--- lua/lazyvim/util/root.lua
+++ lua/lazyvim/util/root.lua
@@ -142,7 +142,10 @@ function M.setup()
     LazyVim.root.info()
   end, { desc = "LazyVim roots for the current buffer" })
 
-  vim.api.nvim_create_autocmd({ "LspAttach", "BufWritePost", "DirChanged" }, {
+  -- FIX: doesn't properly clear cache in neo-tree `set_root` (which should happen presumably on `DirChanged`),
+  -- probably because the event is triggered in the neo-tree buffer, therefore add `BufEnter`
+  -- Maybe this is too frequent on `BufEnter` and something else should be done instead??
+  vim.api.nvim_create_autocmd({ "LspAttach", "BufWritePost", "DirChanged", "BufEnter" }, {
     group = vim.api.nvim_create_augroup("lazyvim_root_cache", { clear = true }),
     callback = function(event)
       M.cache[event.buf] = nil
