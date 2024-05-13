356f95df378821708b54ecceb8d3edce3391cc53
akira <asa.shakira340@gmail.com>
2024-05-12 17:29:09 -0700

    update creates.nvim src option to completion

--- lua/lazyvim/plugins/extras/lang/rust.lua
+++ lua/lazyvim/plugins/extras/lang/rust.lua
@@ -8,7 +8,7 @@ return {
         "Saecki/crates.nvim",
         event = { "BufRead Cargo.toml" },
         opts = {
-          src = {
+          completion = {
             cmp = { enabled = true },
           },
         },
