32a9ee054977fdef75a3236927ea96268fc8ae38
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-15 15:22:11 +0545

    Update gitui.lua

--- lua/lazyvim/plugins/extras/util/gitui.lua
+++ lua/lazyvim/plugins/extras/util/gitui.lua
@@ -18,6 +18,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "gitui" })
+      -- delete lazygit keymap for file history
       vim.keymap.del("n", "<leader>gf")
     end,
   },
