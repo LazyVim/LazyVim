91ead221167a722a8dd8651cb5c53bf5db4d1b4d
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-15 15:48:46 +0200

    feat(lua_ls): configure default lua_ls inlay hint settings

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -75,6 +75,17 @@ return {
               completion = {
                 callSnippet = "Replace",
               },
+              doc = {
+                privateName = { "^_" },
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+              },
             },
           },
         },
