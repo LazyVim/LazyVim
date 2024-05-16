d999be7401783e0f3a610319fa1b327d38fa3e52
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-16 17:45:12 +0200

    feat(coding)!: use native comments on 0.10, with support for ts_context_commentstring

---
+++ lua/lazyvim/plugins/extras/coding/mini-comment.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
+}
