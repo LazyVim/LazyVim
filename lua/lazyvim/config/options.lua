66aac4e6d41730c913c208261f444f670476255c
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-16 11:41:00 +0545

    refactor(options): prefer opt in place of vim.opt

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -92,21 +92,21 @@ if vim.fn.has("nvim-0.10") == 1 then
 end
 
 -- Folding
-vim.opt.foldlevel = 99
+opt.foldlevel = 99
 
 if vim.fn.has("nvim-0.9.0") == 1 then
-  vim.opt.statuscolumn = [[%!v:lua.require'lazyvim.util'.ui.statuscolumn()]]
-  vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
+  opt.statuscolumn = [[%!v:lua.require'lazyvim.util'.ui.statuscolumn()]]
+  opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
 end
 
 -- HACK: causes freezes on <= 0.9, so only enable on >= 0.10 for now
 if vim.fn.has("nvim-0.10") == 1 then
-  vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
-  vim.opt.foldtext = ""
-  vim.opt.fillchars = "fold: "
+  opt.foldmethod = "expr"
+  opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+  opt.foldtext = ""
+  opt.fillchars = "fold: "
 else
-  vim.opt.foldmethod = "indent"
+  opt.foldmethod = "indent"
 end
 
 vim.o.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
