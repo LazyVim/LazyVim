abefa2d2c995467225f3d962fa418862afa1eb52
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-16 20:52:53 +0200

    feat(extras): added extra for the `nvim-treesitter` rewrite. Some plugins are not compatible and will be disabled.

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -170,4 +170,19 @@ function M.safe_keymap_set(mode, lhs, rhs, opts)
   end
 end
 
+---@generic T
+---@param list T[]
+---@return T[]
+function M.dedup(list)
+  local ret = {}
+  local seen = {}
+  for _, v in ipairs(list) do
+    if not seen[v] then
+      table.insert(ret, v)
+      seen[v] = true
+    end
+  end
+  return ret
+end
+
 return M
