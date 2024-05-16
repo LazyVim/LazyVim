f02507b1598379250baab293c423f79c393e28e5
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-16 17:44:32 +0200

    feat(util): set_upvalue

--- lua/lazyvim/util/inject.lua
+++ lua/lazyvim/util/inject.lua
@@ -31,4 +31,20 @@ function M.get_upvalue(func, name)
   end
 end
 
+function M.set_upvalue(func, name, value)
+  local i = 1
+  while true do
+    local n = debug.getupvalue(func, i)
+    if not n then
+      break
+    end
+    if n == name then
+      debug.setupvalue(func, i, value)
+      return
+    end
+    i = i + 1
+  end
+  LazyVim.error("upvalue not found: " .. name)
+end
+
 return M
