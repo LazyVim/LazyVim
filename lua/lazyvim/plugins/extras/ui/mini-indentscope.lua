bafca50bc81fe7647d4b7a7d232a25e681549785
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-16 18:53:21 +0200

    feat(ui)!: move `mini.indentscope` to an extra

---
+++ lua/lazyvim/plugins/extras/ui/mini-indentscope.lua
@@ -0,0 +1,42 @@
+return {
+  -- Active indent guide and indent text objects. When you're browsing
+  -- code, this highlights the current level of indentation, and animates
+  -- the highlighting.
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "LazyFile",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "LazyFile",
+    opts = {
+      scope = { enabled = false },
+    },
+  },
+}
