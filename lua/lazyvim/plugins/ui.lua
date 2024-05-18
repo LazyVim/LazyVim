72abb893acc866ce574c66af7d9de861440a3a1f
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-18 11:32:50 +0200

    fix(bufferline): force update bufferline after `BufDelete`. Closes #3197

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -95,7 +95,7 @@ return {
     config = function(_, opts)
       require("bufferline").setup(opts)
       -- Fix bufferline when restoring a session
-      vim.api.nvim_create_autocmd("BufAdd", {
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
         callback = function()
           vim.schedule(function()
             pcall(nvim_bufferline)
