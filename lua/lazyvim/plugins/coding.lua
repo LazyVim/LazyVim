4bc9389bb5347bb6af119daa6dd16ee646436453
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-18 21:32:53 +0200

    refactor(snippets): refactored snippet code

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -115,38 +115,16 @@ return {
           {
             "<Tab>",
             function()
-              if vim.snippet.active({ direction = 1 }) then
-                vim.schedule(function()
-                  vim.snippet.jump(1)
-                end)
-                return
-              end
-              return "<Tab>"
+              return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
             end,
             expr = true,
             silent = true,
-            mode = "i",
-          },
-          {
-            "<Tab>",
-            function()
-              vim.schedule(function()
-                vim.snippet.jump(1)
-              end)
-            end,
-            silent = true,
-            mode = "s",
+            mode = { "i", "s" },
           },
           {
             "<S-Tab>",
             function()
-              if vim.snippet.active({ direction = -1 }) then
-                vim.schedule(function()
-                  vim.snippet.jump(-1)
-                end)
-                return
-              end
-              return "<S-Tab>"
+              return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
             end,
             expr = true,
             silent = true,
