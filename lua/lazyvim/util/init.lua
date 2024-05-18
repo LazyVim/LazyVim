3a4672de3f8b6410de9fbd5b7134d5108d86f46c
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-18 23:54:15 +0200

    fix(util): fixup

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -193,7 +193,6 @@ end
 M.CREATE_UNDO = vim.api.nvim_replace_termcodes("<c-G>u", true, true, true)
 function M.create_undo()
   if vim.api.nvim_get_mode().mode == "i" then
-    dd("undo")
     vim.api.nvim_feedkeys(M.CREATE_UNDO, "n", false)
   end
 end
