08925421e840ac21f3feac28ee8b57319f0a4e59
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-18 11:27:41 +0200

    fix(trouble-v3): add neovim version check

--- lua/lazyvim/plugins/extras/editor/trouble-v3.lua
+++ lua/lazyvim/plugins/extras/editor/trouble-v3.lua
@@ -12,6 +12,13 @@ if vim.tbl_contains(Config.json.data.extras, "lazyvim.plugins.extras.editor.trou
       })
     end
   end
+  if vim.fn.has("nvim-0.9.2") == 0 then
+    LazyVim.error({
+      "Trouble v3 requires Neovim >= 0.9.2",
+      "Please update your Neovim version.",
+    })
+    return {}
+  end
 end
 
 return {
