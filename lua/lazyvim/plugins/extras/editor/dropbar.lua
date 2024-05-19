c6000303ea8b60aae354bd142102a9a7f34b0f55
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-19 15:29:16 +0545

    feat: add dropbar for vscode still breadcrumbs

---
+++ lua/lazyvim/plugins/extras/editor/dropbar.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "Bekaboo/dropbar.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    config = {},
+  },
+}
