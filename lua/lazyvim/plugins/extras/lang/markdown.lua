ae6d8f1a34fff49f9f1abf9fdd8a559c95b85cf3
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-21 23:05:29 +0200

    feat(markdown): add formatter for mdx

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -10,7 +10,8 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        markdown = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
+        ["markdown"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
+        ["markdown.mdx"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
       },
     },
   },
