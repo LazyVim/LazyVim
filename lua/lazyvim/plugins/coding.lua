2bae438570aa96904836e56d8dad7f33eb3204ee
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-21 19:51:13 +0200

    fix(cmp): fix cmp's broken snippet parsing functionality. Fixes #3266

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -71,6 +71,16 @@ return {
       for _, source in ipairs(opts.sources) do
         source.group_index = source.group_index or 1
       end
+
+      local parse = require("cmp.utils.snippet").parse
+      require("cmp.utils.snippet").parse = function(input)
+        local ok, ret = pcall(parse, input)
+        if ok then
+          return ret
+        end
+        return LazyVim.cmp.snippet_preview(input)
+      end
+
       local cmp = require("cmp")
       cmp.setup(opts)
       cmp.event:on("confirm_done", function(event)
