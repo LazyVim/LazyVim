e9b2dcfa1e531c8aeeaa257a5a2829c050f3941c
arthur <arthur.oceanus@gmail.com>
2024-05-23 22:14:01 +0800

    update nvim-dap opts
    using the current dap local var to maintain consistency.

--- lua/lazyvim/plugins/extras/lang/kotlin.lua
+++ lua/lazyvim/plugins/extras/lang/kotlin.lua
@@ -72,7 +72,7 @@ return {
     opts = function()
       local dap = require("dap")
       if not dap.adapters.kotlin then
-        require("dap").adapters.kotlin = {
+        dap.adapters.kotlin = {
           type = "executable",
           command = "kotlin-debug-adapter",
           options = { auto_continue_if_many_stopped = false },
