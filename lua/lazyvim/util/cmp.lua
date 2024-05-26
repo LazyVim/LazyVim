8cd2d64e6914d91fab81182e3540b7a87bb5281c
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-26 18:51:11 +0200

    fix(snippet): always use top-level snippet session. Fixes #3199

--- lua/lazyvim/util/cmp.lua
+++ lua/lazyvim/util/cmp.lua
@@ -86,6 +86,13 @@ function M.confirm(opts)
 end
 
 function M.expand(snippet)
+  -- Native sessions don't support nested snippet sessions.
+  -- Always use the top-level session.
+  -- Otherwise, when on the first placeholder and selecting a new completion,
+  -- the nested session will be used instead of the top-level session.
+  -- See: https://github.com/LazyVim/LazyVim/issues/3199
+  local session = vim.snippet.active() and vim.snippet._session or nil
+
   local ok = pcall(vim.snippet.expand, snippet)
   if not ok then
     local fixed = M.snippet_fix(snippet)
@@ -100,6 +107,11 @@ function M.expand(snippet)
 ```]]):format(msg, vim.bo.filetype, snippet),
       { title = "vim.snippet" }
     )
+  end
+
+  -- Restore top-level session when needed
+  if session then
+    vim.snippet._session = session
   end
 end
 
