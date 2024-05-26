6d00cb856a59f354a7dd92266241b88fd3ed198a
Oliver Papst <oliver.papst@mailbox.org>
2024-05-26 11:18:33 +0200

    fix(vimtex): suppress treesitter warning
    Currently, vimtex warns:
    
        syntax highlighting is controlled by Treesitter
    
    Explicitly disabling syntax higlighting disables the warning.

--- lua/lazyvim/plugins/extras/lang/tex.lua
+++ lua/lazyvim/plugins/extras/lang/tex.lua
@@ -36,6 +36,7 @@ return {
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      vim.g.vimtex_syntax_enabled = false -- suppress treesitter warning
     end,
   },
 
