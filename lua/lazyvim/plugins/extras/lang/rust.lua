d3500a4a33ab87c6225d991302a72012c54b26fe
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-27 08:09:24 +0200

    fix(rust): show error when rust-analyzer not found

--- lua/lazyvim/plugins/extras/lang/rust.lua
+++ lua/lazyvim/plugins/extras/lang/rust.lua
@@ -90,6 +90,12 @@ return {
     },
     config = function(_, opts)
       vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+      if vim.fn.executable("rust-analyzer") == 0 then
+        LazyVim.error(
+          "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
+          { title = "rustaceanvim" }
+        )
+      end
     end,
   },
 
