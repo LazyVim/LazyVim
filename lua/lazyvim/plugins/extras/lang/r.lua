2e7b80f289cedbc0e0af782558b99de82237d309
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-27 13:30:23 +0200

    fix: add cmp-r back

--- lua/lazyvim/plugins/extras/lang/r.lua
+++ lua/lazyvim/plugins/extras/lang/r.lua
@@ -48,6 +48,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     optional = true,
+    dependencies = { "R-nvim/cmp-r" },
     opts = function(_, opts)
       opts.sources = opts.sources or {}
       table.insert(opts.sources, { name = "cmp_r" })
