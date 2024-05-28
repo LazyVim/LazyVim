de5789415855eafa24f399ee11a5e96bee7c6f9e
Christian Kleinbölting <seakayone@users.noreply.github.com>
2024-05-28 14:55:11 +0200

    feat(scala-extra): Use noice for progress and add minimal keybinding (#3348)
    * Use fidget for progress and add minimal keybinding
    
    * remove figest, noice works already
    
    * make keybinding only available for scala filetypes

--- lua/lazyvim/plugins/extras/lang/scala.lua
+++ lua/lazyvim/plugins/extras/lang/scala.lua
@@ -26,7 +26,7 @@ return {
     ft = { "scala", "sbt", "java" },
     init = function()
       local metals_config = require("metals").bare_config()
-      metals_config.init_options.statusBarProvider = "on"
+      metals_config.init_options.statusBarProvider = "off"
       metals_config.settings = {
         showImplicitArguments = true,
         excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
@@ -70,6 +70,24 @@ return {
         },
       }
     end,
+    keys = {
+      {
+        "<leader>me",
+        function()
+          require("telescope").extensions.metals.commands()
+        end,
+        desc = "Metals commands",
+        ft = { "scala", "sbt", "java" },
+      },
+      {
+        "<leader>mc",
+        function()
+          require("metals").compile_cascade()
+        end,
+        desc = "Metals compile cascade",
+        ft = { "scala", "sbt", "java" },
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
