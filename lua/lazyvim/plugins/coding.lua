be1f39f66262c3eae05d5e1f543cd7b4de8cc371
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-28 17:50:13 +0200

    fix(nvim-snippets): no longer needed to set global_snippets

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -103,7 +103,6 @@ return {
             "garymjr/nvim-snippets",
             opts = {
               friendly_snippets = true,
-              global_snippets = { "all", "global" },
             },
             dependencies = { "rafamadriz/friendly-snippets" },
           },
