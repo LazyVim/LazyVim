25e198da17282b71b938366024cb3eb585639115
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-05-29 16:02:13 +0200

    fix(lsp): create `gr` with `nowait=true`

--- lua/lazyvim/plugins/lsp/keymaps.lua
+++ lua/lazyvim/plugins/lsp/keymaps.lua
@@ -15,7 +15,7 @@ function M.get()
     M._keys =  {
       { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
       { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition", has = "definition" },
-      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
       { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
       { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
       { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
