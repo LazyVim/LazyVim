ba0e1d916c8bf3d67ab74f30ce4dc6471a4ed273
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-31 22:06:52 +0545

    Update bigfile.lua

--- lua/lazyvim/plugins/extras/editor/bigfile.lua
+++ lua/lazyvim/plugins/extras/editor/bigfile.lua
@@ -1,19 +1,7 @@
 return {
-  "LunarVim/bigfile.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    pattern = function(bufnr, filesize_mib)
-      if filesize_mib >= 2 then
-        return true
-      end
-      -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
-      local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
-      local file_length = #file_contents
-      if file_length > 5000 then
-        return true
-      end
-
-      return false
-    end,
-  },
+	"LunarVim/bigfile.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+	},
 }
