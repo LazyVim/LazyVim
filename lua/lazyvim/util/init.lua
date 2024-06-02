596c439db572c8b37c442a1f425b3f452607fd36
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-02 09:33:19 +0200

    feat(util): utility methods for lazy plugins

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -57,9 +57,22 @@ function M.is_win()
   return vim.uv.os_uname().sysname:find("Windows") ~= nil
 end
 
+---@param name string
+function M.get_plugin(name)
+  return require("lazy.core.config").spec.plugins[name]
+end
+
+---@param name string
+---@param path string?
+function M.get_plugin_path(name, path)
+  local plugin = M.get_plugin(name)
+  path = path and "/" .. path or ""
+  return plugin and (plugin.dir .. path)
+end
+
 ---@param plugin string
 function M.has(plugin)
-  return require("lazy.core.config").spec.plugins[plugin] ~= nil
+  return M.get_plugin(plugin) ~= nil
 end
 
 ---@param fn fun()
