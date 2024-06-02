da7b7738eb84b00c949f48afabebd39266e61d75
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-02 18:43:44 +0200

    fix(luasnip): move mappings to cmp spec so that they dont conflict with native snippets

--- lua/lazyvim/plugins/extras/coding/luasnip.lua
+++ lua/lazyvim/plugins/extras/coding/luasnip.lua
@@ -1,34 +1,38 @@
 return {
-  "L3MON4D3/LuaSnip",
-  build = (not LazyVim.is_win())
-      and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-    or nil,
-  dependencies = {
-    {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
-    {
-      "nvim-cmp",
-      dependencies = {
-        "saadparwaiz1/cmp_luasnip",
+  {
+    "L3MON4D3/LuaSnip",
+    build = (not LazyVim.is_win())
+        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
       },
-      opts = function(_, opts)
-        opts.snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        }
-        table.insert(opts.sources, { name = "luasnip" })
-      end,
+      {
+        "nvim-cmp",
+        dependencies = {
+          "saadparwaiz1/cmp_luasnip",
+        },
+        opts = function(_, opts)
+          opts.snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          }
+          table.insert(opts.sources, { name = "luasnip" })
+        end,
+      },
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
     },
   },
-  opts = {
-    history = true,
-    delete_check_events = "TextChanged",
-  },
+  {
+    "nvim-cmp",
     -- stylua: ignore
     keys = {
       {
@@ -41,4 +45,5 @@ return {
       { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
       { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
+  },
 }
