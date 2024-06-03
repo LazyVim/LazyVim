d65e74cb58f679a1dbe03f6799e263e9a167f3a4
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2024-06-03 11:08:06 +0100

    feat(editor-extra): Add inc-rename

---
+++ lua/lazyvim/plugins/extras/editor/inc-rename.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      keys[#keys + 1] = {
+        "<leader>cr",
+        function()
+          local inc_rename = require("inc_rename")
+          return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+        end,
+        expr = true,
+        desc = "Rename",
+        has = "rename",
+      }
+    end,
+  },
+}
