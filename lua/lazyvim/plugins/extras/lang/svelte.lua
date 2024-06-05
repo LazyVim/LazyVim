5bc8b4ace0d41ecfe147b67b6fbf575f1bd7110c
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2024-06-05 12:52:02 -0500

    feat(svelte): add key binding for organize imports

--- lua/lazyvim/plugins/extras/lang/svelte.lua
+++ lua/lazyvim/plugins/extras/lang/svelte.lua
@@ -27,7 +27,23 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        svelte = {},
+        svelte = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
+        },
       },
     },
   },
