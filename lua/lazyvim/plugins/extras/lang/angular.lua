99d62aff1a8581b961c1346d0f96004f039b6a20
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-05 23:55:05 +0200

    fix(extra-angular): reguster angular treesitter without autocmd

--- lua/lazyvim/plugins/extras/lang/angular.lua
+++ lua/lazyvim/plugins/extras/lang/angular.lua
@@ -20,12 +20,7 @@ return {
           [".*%.container%.html"] = "angular.html",
         },
       })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "angular.html",
-        callback = function()
-          vim.treesitter.language.register("angular", "angular.html")
-        end,
-      })
+      vim.treesitter.language.register("angular", "angular.html")
     end,
   },
 
