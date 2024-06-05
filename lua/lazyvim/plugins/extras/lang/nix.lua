9eae3e015aa5a90daa6880c2a7701900d21032a7
Izel Nakri | izelnakri.eth <contact@izelnakri.com>
2024-06-06 00:44:43 +0300

    feat(lang): add nix language support (#2956)
    In the future we can add:
    - dap / debugger support, if this is ever possible or needed.
    - extend neotest, if this is ever needed, for most people I suspect not
    needed.
    - linting, if this is ever needed.
    - ~~maybe enhance nvim-cmp behavior with nixpkgs, rust lang extension
    crates.nvim does smt with nvim-cmp, other language extensions don't.~~
    
    I think its already good to merge this PR, we can add these enhancements
    later, if they are ever needed.
    
    ---------
    
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>

---
+++ lua/lazyvim/plugins/extras/lang/nix.lua
@@ -0,0 +1,30 @@
+return {
+  recommended = {
+    ft = "nix",
+    root = "flake.nix",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "nix" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nil_ls = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}
