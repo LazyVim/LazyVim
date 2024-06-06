9904bbb8872bd49470fbcab66680632dbd522f4a
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-06 22:17:11 +0200

    feat: added dbout to close-with-q

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -66,6 +66,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "checkhealth",
     "neotest-summary",
     "neotest-output-panel",
+    "dbout",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
