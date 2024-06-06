91a5fc10e69cd601a800c5cf0b07b5213952b8fc
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-06 12:24:00 +0200

    feat(extras): added new `git` extra with treesitter and issue/pr support

---
+++ lua/lazyvim/plugins/extras/lang/git.lua
@@ -0,0 +1,25 @@
+return {
+  -- Treesitter git support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" })
+    end,
+  },
+
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "petertriho/cmp-git",
+        opts = {},
+      },
+    },
+    ---@module 'cmp'
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "git" })
+    end,
+  },
+}
