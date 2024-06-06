ed134d999b7d231d871e93778053053b6ceb371f
Nybkox <nybkox@gmail.com>
2024-06-06 13:44:39 +0200

    feat(extras): add supermaven

---
+++ lua/lazyvim/plugins/extras/coding/supermaven.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<C-y>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "supermaven", priority = 100 })
+    end,
+  },
+}
