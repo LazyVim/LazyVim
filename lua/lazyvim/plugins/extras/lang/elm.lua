d4803aff928c9e93af943d034b64a3b74e31c21c
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-06 13:35:29 +0200

    fix: added recommended and fixed mason

--- lua/lazyvim/plugins/extras/lang/elm.lua
+++ lua/lazyvim/plugins/extras/lang/elm.lua
@@ -1,4 +1,9 @@
 return {
+  recommended = {
+    ft = { "elm" },
+    root = { "elm.json" },
+  },
+
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
@@ -7,41 +12,30 @@ return {
       end
     end,
   },
+
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "elm-language-server", "elm-format" })
+      vim.list_extend(opts.ensure_installed, { "elm-format" })
     end,
   },
+
   {
     "stevearc/conform.nvim",
+    optional = true,
     opts = {
       formatters_by_ft = {
         elm = { "elm_format" },
       },
     },
   },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         elmls = {},
-      },
-    },
-  },
-  { -- neotest config needs improvement from someone more expert than me
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/neotest-vim-test",
-      "vim-test/vim-test",
-    },
-    opts = {
-      adapters = {
-        ["neotest-vim-test"] = {
-          allow_file_types = { "elm" },
-          runner = "elmtest",
-        },
       },
     },
   },
