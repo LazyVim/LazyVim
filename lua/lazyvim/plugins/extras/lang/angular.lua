11ce94e1857c4fe1b02e199dcde29500334ea3aa
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-06 11:28:37 +0200

    fix(extra-lang-angular): implement @folke suggestion for fixing angular treesitter

--- lua/lazyvim/plugins/extras/lang/angular.lua
+++ lua/lazyvim/plugins/extras/lang/angular.lua
@@ -14,13 +14,12 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "angular", "scss" })
       end
-      vim.filetype.add({
-        pattern = {
-          [".*%.component%.html"] = "angular.html",
-          [".*%.container%.html"] = "angular.html",
-        },
+      vim.api.nvim_create_autocmd("BufRead", {
+        pattern = { "*.component.html", "*.container.html" },
+        callback = function()
+          vim.treesitter.start(nil, "angular")
+        end,
       })
-      vim.treesitter.language.register("angular", "angular.html")
     end,
   },
 
