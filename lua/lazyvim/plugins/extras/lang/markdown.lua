a9aea4b5a14523641aceb23ba6717c07043faca1
DrummyFloyd <jonathan.monnet28@gmail.com>
2024-06-06 16:22:40 +0200

    fix(markdown): missing markdown-toc in mason (#3499)
    missing `markdown-toc` in ensure installed for `extra/markdown`

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -19,7 +19,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "markdownlint" })
+      vim.list_extend(opts.ensure_installed, { "markdownlint", "markdown-toc" })
     end,
   },
   {
