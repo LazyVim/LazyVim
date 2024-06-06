2bbc7e179d4050f9564e9a7732804346bb22022e
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-06 10:54:43 +0200

    Revert "fix(angular): treesitter not enabled for angular templates (#3469)"
    This reverts commit 24af74eed60bf2f02d963fd730b07e537cdf34dc.

--- lua/lazyvim/plugins/extras/lang/angular.lua
+++ lua/lazyvim/plugins/extras/lang/angular.lua
@@ -14,13 +14,6 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "angular", "scss" })
       end
-      vim.filetype.add({
-        pattern = {
-          [".*%.component%.html"] = "angular.html",
-          [".*%.container%.html"] = "angular.html",
-        },
-      })
-      vim.treesitter.language.register("angular", "angular.html")
     end,
   },
 
