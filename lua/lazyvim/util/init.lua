5a11d740e119d392858b97b69470201e66e66212
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-07 22:49:06 +0200

    feat(util): added LazyVim.memoize

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -268,4 +268,17 @@ for _, level in ipairs({ "info", "warn", "error" }) do
   end
 end
 
+local cache = {} ---@type table<string, any>
+---@generic T: fun()
+---@param fn T
+---@return T
+function M.memoize(fn)
+  return function(...)
+    local key = vim.inspect({ ... })
+    if cache[key] == nil then
+      cache[key] = fn(...)
+    end
+    return cache[key]
+  end
+end
 return M
