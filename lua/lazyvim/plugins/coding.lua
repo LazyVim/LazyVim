f70d350085dc0972e35300c6116ea1f13f5dc8cc
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-07 06:16:32 +0200

    style: comment

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -19,7 +19,6 @@ return {
     --   auto_brackets = { "python" }
     -- }
     -- ```
-
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
