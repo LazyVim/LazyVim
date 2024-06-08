68af27a6703bb4ce9a5787a3e937405517ed8023
muneebusmani <muneebusmani8355@gmail.com>
2024-06-08 14:51:14 +0500

    fix(php): Added Formatting for php

--- lua/lazyvim/plugins/extras/lang/php.lua
+++ lua/lazyvim/plugins/extras/lang/php.lua
@@ -35,4 +35,26 @@ return {
       }
     end,
   },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.pretty_php)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    dependencies = {
+      "williamboman/mason.nvim",
+      opts = { ensure_installed = { "pretty-php" } },
+    },
+    opts = {
+      formatters_by_ft = {
+        php = { "pretty-php" },
+      },
+    },
+  },
 }
