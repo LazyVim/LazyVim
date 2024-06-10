b74db854470c5b7426b399b041e31ea8c6901364
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-10 18:10:13 +0200

    feat(fzf-lua): added support for todo-comments

--- lua/lazyvim/plugins/extras/editor/fzf.lua
+++ lua/lazyvim/plugins/extras/editor/fzf.lua
@@ -135,6 +135,16 @@ return {
     },
   },
 
+  {
+    "folke/todo-comments.nvim",
+    optional = true,
+    -- stylua: ignore
+    keys = {
+      { "<leader>st", function() require("todo-comments.fzf").todo() end, desc = "Todo" },
+      { "<leader>sT", function () require("todo-comments.fzf").todo({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    },
+  },
+
   -- remove when https://github.com/ibhagwan/fzf-lua/pull/1244 gets merged
   {
     "ibhagwan/fzf-lua",
