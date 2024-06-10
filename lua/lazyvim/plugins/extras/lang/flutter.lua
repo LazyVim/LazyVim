b4f6f32791b4eebf3917a1ad6c76beeb7e59b3ea
muneebusmani <muneebusmani8355@gmail.com>
2024-06-10 15:56:57 +0500

    added dart support

---
+++ lua/lazyvim/plugins/extras/lang/flutter.lua
@@ -0,0 +1,30 @@
+-- requires dart/flutter to be installed
+return {
+  recommended = {
+    ft = "dart",
+    root = { "pubspec.yaml" },
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = true,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "dart" } },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.dart_format)
+    end,
+  },
+}
