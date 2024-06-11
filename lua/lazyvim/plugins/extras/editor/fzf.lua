7e0ac6850b613ba8cbc179fa2f71199184504a54
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-11 16:11:45 +0200

    fix(fzf): symbols filter

--- lua/lazyvim/plugins/extras/editor/fzf.lua
+++ lua/lazyvim/plugins/extras/editor/fzf.lua
@@ -16,7 +16,12 @@ LazyVim.pick._open = function(command, opts)
 end
 
 local function symbols_filter(entry, ctx)
-  ctx.symbols_filter = ctx.symbols_filter or require("lazyvim.config").get_kind_filter(ctx.bufnr)
+  if ctx.symbols_filter == nil then
+    ctx.symbols_filter = require("lazyvim.config").get_kind_filter(ctx.bufnr) or false
+  end
+  if ctx.symbols_filter == false then
+    return true
+  end
   return vim.tbl_contains(ctx.symbols_filter, entry.kind)
 end
 
