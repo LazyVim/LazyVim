06a5265dc89bb204146d449477bfa630811a4d02
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-06-11 16:13:34 +0100

    fix typo

--- lua/lazyvim/plugins/extras/util/chezmoi.lua
+++ lua/lazyvim/plugins/extras/util/chezmoi.lua
@@ -3,10 +3,10 @@ return {
   {
     -- highlighting for chezmoi files template files
     "alker0/chezmoi.vim",
-	init = function()
-		vim.g.chezmoi#use_tmp_buffer = 1
-		vim.g.chezmoi#source_dir_path = os.getenv("HOME") .. "/.local/share/chezmoi",
-	end,
+    init = function()
+      vim.g["chezmoi#use_tmp_buffer"] = 1
+      vim.g["chezmoi#source_dir_path"] = os.getenv("HOME") .. "/.local/share/chezmoi"
+    end,
   },
   {
     "xvzc/chezmoi.nvim",
