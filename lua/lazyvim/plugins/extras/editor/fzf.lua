9f2a3699a8f47467359f2c3defb3a711b32e0efb
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-11 19:38:22 +0200

    feat(fzf): added ctrl-u and ctrl+d to scroll in the results list

--- lua/lazyvim/plugins/extras/editor/fzf.lua
+++ lua/lazyvim/plugins/extras/editor/fzf.lua
@@ -39,6 +39,8 @@ return {
 
       -- Quickfix
       config.defaults.keymap.fzf["ctrl-q"] = "select-all+accept"
+      config.defaults.keymap.fzf["ctrl-u"] = "half-page-up"
+      config.defaults.keymap.fzf["ctrl-d"] = "half-page-down"
       config.defaults.keymap.builtin["<c-f>"] = "preview-page-down"
       config.defaults.keymap.builtin["<c-b>"] = "preview-page-up"
 
