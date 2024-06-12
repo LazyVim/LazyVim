ad194c7b3a415df8b873d7129118e8a5b2e62593
Radvil <radvil.linux@gmail.com>
2024-06-12 22:32:11 +0800

    feat(angular): Add `filetypes` pattern to autocommand detection
    Angular also support svg file for it's view template

--- lua/lazyvim/plugins/extras/lang/angular.lua
+++ lua/lazyvim/plugins/extras/lang/angular.lua
@@ -17,7 +17,7 @@ return {
         vim.list_extend(opts.ensure_installed, { "angular", "scss" })
       end
       vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-        pattern = { "*.component.html", "*.container.html" },
+        pattern = { "*.component.html", "*.container.html", "*.component.svg", "*.container.svg" },
         callback = function()
           vim.treesitter.start(nil, "angular")
         end,
