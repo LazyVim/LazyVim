92ab058118be1072918f4bacc06b8616904e1bfb
zspher <66728045+zspher@users.noreply.github.com>
2024-06-12 12:29:10 +0800

    feat(omnisharp): add more handlers

--- lua/lazyvim/plugins/extras/lang/omnisharp.lua
+++ lua/lazyvim/plugins/extras/lang/omnisharp.lua
@@ -46,7 +46,16 @@ return {
         omnisharp = {
           handlers = {
             ["textDocument/definition"] = function(...)
-              return require("omnisharp_extended").handler(...)
+              return require("omnisharp_extended").definition_handler(...)
+            end,
+            ["textDocument/typeDefinition"] = function(...)
+              return require("omnisharp_extended").type_definition_handler(...)
+            end,
+            ["textDocument/references"] = function(...)
+              return require("omnisharp_extended").references_handler(...)
+            end,
+            ["textDocument/implementation"] = function(...)
+              return require("omnisharp_extended").implementation_handler(...)
             end,
           },
           keys = {
