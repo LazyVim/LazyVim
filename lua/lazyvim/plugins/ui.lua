1d50c75a4b149f1cf00f4d4163f1469bc2fa77fc
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-13 15:01:44 +0200

    fix(dashboard): proper fix for dashboard's weird action loading

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -325,11 +325,11 @@ return {
           header = vim.split(logo, "\n"),
           -- stylua: ignore
           center = {
-            { action = LazyVim.pick(),                                   desc = " Find File",       icon = " ", key = "f" },
+            { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
             { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-            { action = LazyVim.pick("oldfiles"),                         desc = " Recent Files",    icon = " ", key = "r" },
-            { action = LazyVim.pick("live_grep"),                        desc = " Find Text",       icon = " ", key = "g" },
-            { action = LazyVim.pick.config_files(),                      desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+            { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+            { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
             { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
             { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
             { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
