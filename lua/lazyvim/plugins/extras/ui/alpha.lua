339979d3143d46a37c304d45a00c7a4f448e707d
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-06-12 22:09:30 -0600

    fix(ui): use `LazyVim.pick` for alpha.nvim and mini.starter extras (#3611)
    ## What is this PR for?
    
    Make other nvim dashboards use LazyVim.pick so they can also make use of
    fzf-lua when the extra is enabled. In addition, aligned the sections to
    be the same as dashboard.nvim.
    
    **Note**: this is somewhat untested. I haven't tested each dashboard and
    their sections to make sure they all work.
    
    ## Does this PR fix an existing issue?
    
    <!--
      If this PR fixes any issues, please link to the issue here.
      Fixes #<issue_number>
    -->
    
    ## Checklist
    
    - [x] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.
    
    ---------
    
    Co-authored-by: Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>

--- lua/lazyvim/plugins/extras/ui/alpha.lua
+++ lua/lazyvim/plugins/extras/ui/alpha.lua
@@ -23,11 +23,11 @@ return {
       dashboard.section.header.val = vim.split(logo, "\n")
       -- stylua: ignore
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
-        dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
-        dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
-        dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
-        dashboard.button("c", " " .. " Config",          "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
+        dashboard.button("f", " " .. " Find file",       LazyVim.pick()),
+        dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+        dashboard.button("r", " " .. " Recent files",    LazyVim.pick("oldfiles")),
+        dashboard.button("g", " " .. " Find text",       LazyVim.pick("live_grep")),
+        dashboard.button("c", " " .. " Config",          LazyVim.pick.config_files()),
         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
         dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
         dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
