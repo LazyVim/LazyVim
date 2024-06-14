239fb90ce216d93706f2c27b4a9a54eef42ed437
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-14 16:46:35 +0200

    style: cleanup trouble

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -236,8 +236,8 @@ return {
   -- better diagnostics list and others
   {
     "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
+    cmd = { "Trouble" },
+    opts = {},
     keys = {
       { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
       { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
