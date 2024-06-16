f64e3ffca27f76e0f47e2d5e5d3548d0093f6baf
Ahmed Kamal <ahmed@moon-i.com.au>
2024-06-16 14:55:31 +1000

    feat(extra): Add prisma support and config

---
+++ lua/lazyvim/plugins/extras/lang/prisma.lua
@@ -0,0 +1,19 @@
+return {
+  recommended = {
+    ft = "prisma",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "prisma" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        prismals = {},
+      },
+    },
+  },
+}
