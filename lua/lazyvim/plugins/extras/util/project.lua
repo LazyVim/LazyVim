f24999fd3645756a338137278ccc4fa3e1240a90
xiaojianzheng <1272209235@qq.com>
2024-06-16 13:21:30 +0800

    feat(project): add some fzf-lua keymap (#3666)
    - `ctrl-s` search_in_project_files
    - `ctrl-w` change_working_directory
    - `ctrl-d` delete_project
    - `ctrl-r` recent_project_files
    
    Co-authored-by: 肖健正 <xiaojianzheng@qq.com>

--- lua/lazyvim/plugins/extras/util/project.lua
+++ lua/lazyvim/plugins/extras/util/project.lua
@@ -1,11 +1,15 @@
-local pick = function()
+local pick = nil
+
+pick = function()
   if LazyVim.pick.picker.name == "telescope" then
     return vim.cmd("Telescope projects")
   elseif LazyVim.pick.picker.name == "fzf" then
     local fzf_lua = require("fzf-lua")
+    local project = require("project_nvim.project")
     local history = require("project_nvim.utils.history")
     local results = history.get_recent_projects()
     local actions = require("fzf-lua.actions")
+
     fzf_lua.fzf_exec(results, {
       actions = {
         ["default"] = {
@@ -14,6 +18,34 @@ local pick = function()
           end,
         },
         ["ctrl-t"] = actions.file_tabedit,
+        ["ctrl-s"] = {
+          function(selected)
+            fzf_lua.live_grep({ cwd = selected[1] })
+          end,
+        },
+        ["ctrl-r"] = {
+          function(selected)
+            fzf_lua.oldfiles({ cwd = selected[1] })
+          end,
+        },
+        ["ctrl-w"] = {
+          function(selected)
+            local path = selected[1]
+            local ok = project.set_pwd(path)
+            if ok then
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+              LazyVim.info("Change project dir to " .. path)
+            end
+          end,
+        },
+        ["ctrl-d"] = function(selected)
+          local path = selected[1]
+          local choice = vim.fn.confirm("Delete '" .. path .. "' project? ", "&Yes\n&No")
+          if choice == 1 then
+            history.delete_project({ value = path })
+          end
+          pick()
+        end,
       },
     })
   end
