e2972f0ccaf87c7d75bcd498fa9b2a64eb3d39a5
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-16 15:17:53 +0200

    refactor: cmp

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -23,19 +23,19 @@ return {
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
       local defaults = require("cmp.config.default")()
+      local auto_select = true
       return {
         auto_brackets = {}, -- configure any filetype to auto add brackets
         completion = {
-          completeopt = "menu,menuone,noinsert",
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
         },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = LazyVim.cmp.confirm(),
+          ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+          ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
           ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ["<C-CR>"] = function(fallback)
             cmp.abort()
@@ -50,7 +50,7 @@ return {
         }),
         formatting = {
           format = function(_, item)
-            local icons = require("lazyvim.config").icons.kinds
+            local icons = LazyVim.config.icons.kinds
             if icons[item.kind] then
               item.kind = icons[item.kind] .. item.kind
             end
@@ -65,32 +65,7 @@ return {
         sorting = defaults.sorting,
       }
     end,
-    ---@param opts cmp.ConfigSchema | {auto_brackets?: string[]}
-    config = function(_, opts)
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-
-      local parse = require("cmp.utils.snippet").parse
-      require("cmp.utils.snippet").parse = function(input)
-        local ok, ret = pcall(parse, input)
-        if ok then
-          return ret
-        end
-        return LazyVim.cmp.snippet_preview(input)
-      end
-
-      local cmp = require("cmp")
-      cmp.setup(opts)
-      cmp.event:on("confirm_done", function(event)
-        if vim.tbl_contains(opts.auto_brackets or {}, vim.bo.filetype) then
-          LazyVim.cmp.auto_brackets(event.entry)
-        end
-      end)
-      cmp.event:on("menu_opened", function(event)
-        LazyVim.cmp.add_missing_snippet_docs(event.window)
-      end)
-    end,
+    main = "lazyvim.util.cmp",
   },
 
   -- snippets
--- lua/lazyvim/util/cmp.lua
+++ lua/lazyvim/util/cmp.lua
@@ -128,4 +128,31 @@ function M.expand(snippet)
   end
 end
 
+---@param opts cmp.ConfigSchema | {auto_brackets?: string[]}
+function M.setup(opts)
+  for _, source in ipairs(opts.sources) do
+    source.group_index = source.group_index or 1
+  end
+
+  local parse = require("cmp.utils.snippet").parse
+  require("cmp.utils.snippet").parse = function(input)
+    local ok, ret = pcall(parse, input)
+    if ok then
+      return ret
+    end
+    return LazyVim.cmp.snippet_preview(input)
+  end
+
+  local cmp = require("cmp")
+  cmp.setup(opts)
+  cmp.event:on("confirm_done", function(event)
+    if vim.tbl_contains(opts.auto_brackets or {}, vim.bo.filetype) then
+      LazyVim.cmp.auto_brackets(event.entry)
+    end
+  end)
+  cmp.event:on("menu_opened", function(event)
+    LazyVim.cmp.add_missing_snippet_docs(event.window)
+  end)
+end
+
 return M
