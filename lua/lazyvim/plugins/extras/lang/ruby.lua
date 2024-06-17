4cd5c4162ab398cfe5c7c8ef9a9480280aa0b0ce
Kevin Robayna <me@kevinrobayna.com>
2024-06-17 09:37:34 +0100

    feat(extras): improve ruby extra by using RubyLsp
    Shopify started working on its own LSP
    (https://github.com/Shopify/ruby-lsp) and it performs way better than
    Solargraph which has a lot of limitations. This paired with sorbet gives
    better IntelliSense when navigating the code.
    
    One caveat though is that RubyLsp does not work very well with NeoVim <
    0.10 https://github.com/Shopify/ruby-lsp/blob/main/EDITORS.md#neovim

--- lua/lazyvim/plugins/extras/lang/ruby.lua
+++ lua/lazyvim/plugins/extras/lang/ruby.lua
@@ -11,9 +11,16 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
+      ---@type lspconfig.options
       servers = {
-        solargraph = {},
+        -- disable solargraph from auto running when you open ruby files
+        solargraph = {
+          autostart = false,
+        },
+        -- ruby_lsp will be automatically installed with mason and loaded with lspconfig
+        ruby_lsp = {},
       },
     },
   },
