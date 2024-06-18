492fa86faa29f2987f662ad0cdb796b5c1c49ef3
Songli Yu <1097985743@qq.com>
2024-06-18 13:15:24 +0800

    fix(snippets): nvim-snippets `<S-Tab>` prev key (#3712)
    Fix nvim-snippets' <S-Tab> rollback keymap.

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -103,7 +103,7 @@ return {
       {
         "<S-Tab>",
         function()
-          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
+          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
         end,
         expr = true,
         silent = true,
