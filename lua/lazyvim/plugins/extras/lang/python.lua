06f13daf7261377ecfa6b4ac845d742f446df719
dbger <3982742+dbger@users.noreply.github.com>
2024-06-19 01:02:41 +0800

    fix(dap): get the correct debugpy pkg path on windows (#3716)
    ## What is this PR for?
    fix(dap): get the correct debugpy pkg path on windows.
    
    <!-- Describe the big picture of your changes to communicate to the
    maintainers
      why we should accept this pull request. -->
    
    ## Does this PR fix an existing issue?
    
    <!--
      If this PR fixes any issues, please link to the issue here.
      Fixes #<issue_number>
    -->
    
    ## Checklist
    
    - [ ] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -92,7 +92,11 @@ return {
         { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
       },
       config = function()
-        require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
+        if vim.fn.has("win32") == 1 then
+          require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/Scripts/python.exe"))
+        else
+          require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
+        end
       end,
     },
   },
