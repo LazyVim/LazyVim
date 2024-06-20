9c7a8f5b27324c974bda5e05062de7cdbd50dcc5
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-20 20:40:33 +0200

    feat(go): switch to neotest-golang

--- lua/lazyvim/plugins/extras/test/core.lua
+++ lua/lazyvim/plugins/extras/test/core.lua
@@ -10,10 +10,11 @@ return {
       -- or a table of adapter names, mapped to adapter configs.
       -- The adapter will then be automatically loaded with the config.
       adapters = {},
-      -- Example for loading neotest-go with a custom config
+      -- Example for loading neotest-golang with a custom config
       -- adapters = {
-      --   ["neotest-go"] = {
-      --     args = { "-tags=integration" },
+      --   ["neotest-golang"] = {
+      --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+      --     dap_go_enabled = true,
       --   },
       -- },
       status = { virtual_text = true },
