7c7ec7dd916ae01dab83879351ac58255e8d8f3d
arthur <arthur.oceanus@gmail.com>
2024-06-22 12:52:41 +0800

    update

--- lua/lazyvim/plugins/extras/lang/sql.lua
+++ lua/lazyvim/plugins/extras/lang/sql.lua
@@ -10,8 +10,10 @@ if lazyvim_docs then
     { name = "staging", url = "Replace with your database connection URL." },
   }
 
-  -- NOTICE: Replace `secrets/db-env` with your own module path.
-  -- You can set your secrets directly through a Lua table, for example:
+  -- You can set your secrets directly through a Lua table. For example:
+  -- Create a secrets/db-env.lua file and return the connection table.
+  -- Or
+  -- Replace `secrets/db-env` with your own module path.
   local ok, dbs = pcall(require, "secrests/db-env")
   if ok then
     vim.g.dbs = dbs
