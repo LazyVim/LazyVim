7eb7fe0b3b3a89d3cc5b51740f21b892bf4a8e65
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-06-23 15:25:07 +0200

    feat(c#): route references and implementations lsp calls through omnisharp-extended

--- lua/lazyvim/plugins/extras/lang/omnisharp.lua
+++ lua/lazyvim/plugins/extras/lang/omnisharp.lua
@@ -46,7 +46,13 @@ return {
         omnisharp = {
           handlers = {
             ["textDocument/definition"] = function(...)
-              return require("omnisharp_extended").handler(...)
+              return require("omnisharp_extended").definition_handler(...)
+            end,
+            ["textDocument/references"] = function(...)
+              return require("omnisharp_extended").references_handler(...)
+            end,
+            ["textDocument/implementation"] = function(...)
+              return require("omnisharp_extended").implementation_handler(...)
             end,
           },
           keys = {
@@ -56,6 +62,20 @@ return {
                 require("omnisharp_extended").telescope_lsp_definitions()
               end,
               desc = "Goto Definition",
+            },
+            {
+              "gr",
+              function()
+                require("omnisharp_extended").telescope_lsp_references()
+              end,
+              desc = "References",
+            },
+            {
+              "gI",
+              function()
+                require("omnisharp_extended").telescope_lsp_implementation()
+              end,
+              desc = "Goto Implementation",
             },
           },
           enable_roslyn_analyzers = true,
