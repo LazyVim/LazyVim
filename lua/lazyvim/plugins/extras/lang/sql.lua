da1ce6c243ab628dbf6904f90b8ac7421df06013
arthur <arthur.oceanus@gmail.com>
2024-06-25 21:32:59 +0800

    update message

--- lua/lazyvim/plugins/extras/lang/sql.lua
+++ lua/lazyvim/plugins/extras/lang/sql.lua
@@ -1,32 +1,29 @@
 if lazyvim_docs then
-  -- Automatically configure connections without needing to manually input them each time.
+  -- The setup below will automatically configure connections without the need for manual input each time.
+
+  -- Example configuration using dictionary with keys:
   --    vim.g.dbs = {
   --      dev = "Replace with your database connection URL.",
   --      staging = "Replace with your database connection URL.",
   --    }
   -- or
+  -- Example configuration using a list of dictionaries:
   --    vim.g.dbs = {
   --      { name = "dev", url = "Replace with your database connection URL." },
   --      { name = "staging", url = "Replace with your database connection URL." },
   --    }
 
-  -- You can set your those configuration through a Lua table. For example:
-  -- Create a `.lazy.lua` file in your project and set your connections in it like this:
+  -- or
+  -- Create a `.lazy.lua` file in your project and set your connections like this:
   -- ```lua
   --    vim.g.dbs = {...}
   --
   --    return {}
   -- ```
 
-  local ok = pcall(dofile, vim.fn.getcwd() .. "/.lazy.lua")
+  -- Alternatively, you can also use other methods to inject your environment variables.
 
-  if not ok then
-    vim.g.dbs = {}
-  end
-
-  -- Alternatively, you can also use other ways to inject your environment variables.
-
-  -- After all, please make sure to add `.lazy` to your global `.gitignore` file.
+  -- Finally, please make sure to add `.lazy.lua` to your `.gitignore` file to protect your secrets.
 end
 
 local sql_ft = { "sql", "mysql", "plsql" }
