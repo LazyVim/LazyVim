938a6718c6f0d5c6716a34bd3383758907820c52
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-25 20:30:57 +0200

    feat(autocmds): added proper bigfile support

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -40,6 +40,12 @@ vim.g.lazyvim_statuscolumn = {
 -- Hide deprecation warnings
 vim.g.deprecation_warnings = false
 
+-- Set filetype to `bigfile` for files larger than 1.5 MB
+-- Only vim syntax will be enabled (with the correct filetype)
+-- LSP, treesitter and other ft plugins will be disabled.
+-- mini.animate will also be disabled.
+vim.g.bigfile_size = 1024 * 1024 * 1.5 -- 1.5 MB
+
 -- Show the current document symbols location from Trouble in lualine
 vim.g.trouble_lualine = true
 
