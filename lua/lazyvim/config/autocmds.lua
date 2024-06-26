135ce0d7edb64958b2c8f636a9ba624ec0721eb3
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-26 17:09:37 +0200

    fix(autocmds): check that buf exists

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -119,7 +119,11 @@ vim.filetype.add({
   pattern = {
     [".*"] = {
       function(path, buf)
-        return vim.bo[buf].filetype ~= "bigfile" and path and vim.fn.getfsize(path) > vim.g.bigfile_size and "bigfile"
+        return vim.bo[buf]
+            and vim.bo[buf].filetype ~= "bigfile"
+            and path
+            and vim.fn.getfsize(path) > vim.g.bigfile_size
+            and "bigfile"
           or nil
       end,
     },
