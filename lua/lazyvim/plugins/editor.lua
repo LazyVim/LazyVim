36cfe5269eb2d208a05f5546b90a2fde80108e92
k14lb3 <k14lb3@gmail.com>
2024-06-28 01:20:16 +0800

    fix(gitsigns): fix inconsistent signs

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -207,6 +207,13 @@ return {
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
 
