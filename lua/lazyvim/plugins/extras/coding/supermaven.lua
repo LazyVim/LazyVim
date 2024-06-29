35f9343af5d43059bee2966b38eacbcc4973dd67
Nybkox <nybkox@gmail.com>
2024-06-29 15:54:29 +0200

    feat: update cmp and lualine spec to match codeium style

--- lua/lazyvim/plugins/extras/coding/supermaven.lua
+++ lua/lazyvim/plugins/extras/coding/supermaven.lua
@@ -13,7 +13,19 @@ return {
     "hrsh7th/nvim-cmp",
     optional = true,
     opts = function(_, opts)
-      table.insert(opts.sources, { name = "supermaven", priority = 100 })
+      table.insert(opts.sources, 1, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("supermaven"))
     end,
   },
   {
