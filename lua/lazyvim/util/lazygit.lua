b43ace1ecfbeb87626fbde8973a2717247471b2e
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-29 15:37:26 +0200

    fix(git): use current dir when git not found to get a meaningful error message

--- lua/lazyvim/util/lazygit.lua
+++ lua/lazyvim/util/lazygit.lua
@@ -154,7 +154,7 @@ function M.blame_line(opts)
   local cursor = vim.api.nvim_win_get_cursor(0)
   local line = cursor[1]
   local file = vim.api.nvim_buf_get_name(0)
-  local root = LazyVim.root.detectors.pattern(0, { ".git" })[1]
+  local root = LazyVim.root.detectors.pattern(0, { ".git" })[1] or "."
   local cmd = { "git", "-C", root, "log", "-n", opts.count, "-u", "-L", line .. ",+1:" .. file }
   return require("lazy.util").float_cmd(cmd, opts)
 end
