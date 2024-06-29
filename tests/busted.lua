35f89fb3a189dfaaec0dbdb1882028105c129fa2
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-29 07:27:41 +0200

    ci: use lazy minit for tests

--- tests/busted.lua
+++ tests/busted.lua
@@ -1,26 +1,11 @@
 #!/usr/bin/env -S nvim -l
 
--- set stdpaths to use .tests
-local root = vim.fn.fnamemodify("./.tests", ":p")
-for _, name in ipairs({ "config", "data", "state", "cache" }) do
-  vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
-end
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.o.loadplugins = true -- enable since nvim -l disables plugins
+vim.env.LAZY_STDPATH = ".tests"
+load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 
 -- Setup lazy.nvim
-require("lazy").setup({
+require("lazy.minit").busted({
   spec = {
-    { "LazyVim/LazyVim", dir = vim.fn.expand(".") },
-    "lunarmodules/busted", -- add busted
     "LazyVim/starter",
     { "nvim-lua/plenary.nvim" },
     { "folke/lazy.nvim" },
@@ -28,10 +13,4 @@ require("lazy").setup({
     "williamboman/mason.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  rocks = { hererocks = true },
 })
-
--- run busted
-return pcall(require("busted.runner"), {
-  standalone = false,
-}) or os.exit(1)
