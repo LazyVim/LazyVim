fe3d0da5087ecadfee8922fa363d3e84c0ba3461
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-02 19:02:13 +0200

    feat(options): jumpoptions=view

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -75,6 +75,7 @@ opt.grepformat = "%f:%l:%c:%m"
 opt.grepprg = "rg --vimgrep"
 opt.ignorecase = true -- Ignore case
 opt.inccommand = "nosplit" -- preview incremental substitute
+opt.jumpoptions = "view"
 opt.laststatus = 3 -- global statusline
 opt.linebreak = true -- Wrap lines at convenient points
 opt.list = true -- Show some invisible characters (tabs...
