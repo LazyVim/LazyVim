045faec0351ff9a38fda1af9b06bd7f948e84d28
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-04 18:18:10 +0200

    feat(ui): use `mini.icons` instead of `nvim-wev-devicons` (#3899)
    ## What is this PR for?
    
    Replace the icon support with the new mini library
    
    ## Blockers
    
    - [ ]
    https://github.com/echasnovski/mini.nvim/issues/1007#issuecomment-2206553024

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -303,7 +303,17 @@ return {
   },
 
   -- icons
-  { "nvim-tree/nvim-web-devicons", lazy = true },
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    opts = {},
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
 
   -- ui components
   { "MunifTanjim/nui.nvim", lazy = true },
