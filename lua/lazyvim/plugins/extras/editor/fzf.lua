7d961578171b8cbdc47c3f481acf4017ba67b289
Emmanuel Pilande <epilande@axon.com>
2024-07-05 15:50:12 -0700

    feat(fzf): Add ability to send additional flags to ripgrep

--- lua/lazyvim/plugins/extras/editor/fzf.lua
+++ lua/lazyvim/plugins/extras/editor/fzf.lua
@@ -167,6 +167,15 @@ return {
           },
         },
         grep = {
+          rg_glob = true,
+          -- first returned string is the new search query
+          -- second returned string are (optional) additional rg flags
+          -- @return string, string?
+          rg_glob_fn = function(query)
+            local regex, flags = query:match("^(.-)%s%-%-(.*)$")
+            -- If no separator is detected will return the original query
+            return (regex or query), flags
+          end,
           actions = {
             ["alt-i"] = { actions.toggle_ignore },
             ["alt-h"] = { actions.toggle_hidden },
