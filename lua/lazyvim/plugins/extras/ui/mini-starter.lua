763398f8e9d58be48270302e3017800cabaa212e
Dronakurl <76913264+Dronakurl@users.noreply.github.com>
2024-07-06 06:18:51 +0200

    Fix lazyvim startuptime in mini.starter
    The filetype name has changed
    
    https://github.com/echasnovski/mini.starter/commit/394994b2bec10a997c69575825d5c444957b9ff9

--- lua/lazyvim/plugins/extras/ui/mini-starter.lua
+++ lua/lazyvim/plugins/extras/ui/mini-starter.lua
@@ -69,7 +69,7 @@ return {
           local pad_footer = string.rep(" ", 8)
           starter.config.footer = pad_footer .. "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
           -- INFO: based on @echasnovski's recommendation (thanks a lot!!!)
-          if vim.bo[ev.buf].filetype == "starter" then
+          if vim.bo[ev.buf].filetype == "ministarter" then
             pcall(starter.refresh)
           end
         end,
