3b88644561fcff4ead4eb1eef87d59f94ef19ced
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-15 14:06:14 +0700

    feat(typst): add none-ls config

--- lua/lazyvim/plugins/extras/lang/typst.lua
+++ lua/lazyvim/plugins/extras/lang/typst.lua
@@ -40,6 +40,17 @@ return {
   },
 
   {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.formatting.typstyle,
+      })
+    end,
+  },
+
+  {
     "chomosuke/typst-preview.nvim",
     cmd = { "TypstPreview" },
     build = function()
