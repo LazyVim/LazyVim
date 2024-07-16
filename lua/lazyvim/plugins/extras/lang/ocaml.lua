d1fffee3e34ada0b2e8d3c748cfa85516e34f159
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-07-16 10:05:19 -0700

    added ocaml extra

---
+++ lua/lazyvim/plugins/extras/lang/ocaml.lua
@@ -0,0 +1,40 @@
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = { "ml", "mli", "cmi", "cmo", "cmx", "cma", "cmxa", "cmxs", "cmt", "cmti", "opam" },
+      root = { "merlin.opam", "dune-project" },
+    })
+  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ocaml" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ocamllsp = {
+          get_language_id = function(_, ftype)
+            return ftype
+          end,
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "*.opam",
+              "esy.json",
+              "package.json",
+              ".git",
+              "dune-project",
+              "dune-workspace",
+              "*.ml"
+            )(fname)
+          end,
+          on_attach = on_attach,
+        },
+      },
+    },
+  },
+}
