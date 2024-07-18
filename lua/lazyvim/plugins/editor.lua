5adec474310b75615b8c76591f4fc56ec43cc933
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-18 17:01:45 +0200

    feat: use selection when in visual mode

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -135,13 +135,16 @@ return {
       {
         "<leader>sr",
         function()
-          require("grug-far").grug_far({
-            prefills = {
-              search = vim.fn.expand("<cword>"),
-              filesFilter = "*." .. vim.fn.expand("%:e"),
-            },
+          local is_visual = vim.fn.mode():lower():find("v")
+          if is_visual then -- needed to make visual selection work
+            vim.cmd([[normal! v]])
+          end
+          local grug = require("grug-far");
+          (is_visual and grug.with_visual_selection or grug.grug_far)({
+            prefills = { filesFilter = "*." .. vim.fn.expand("%:e") },
           })
         end,
+        mode = { "n", "v" },
         desc = "Search and Replace",
       },
     },
