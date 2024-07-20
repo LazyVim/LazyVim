0d561a3226b46f6a44c4f2c9be2288f3b52cc351
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-20 21:39:39 +0200

    feat(editor): replace nvim-spectre with grug-far.nvim (#4099)
    ## Description
    
    I'm considering to replace
    [nvim-spectre](https://github.com/nvim-pack/nvim-spectre) with
    [grug-far.nvim](https://github.com/MagicDuck/grug-far.nvim).
    
    It has a better ui and I like the workflow better.
    
    I won't merge this right away. I'm mostly looking for feedback on
    whether this would be a good thing and whether the defaults option I've
    added seem ok.
    
    ## Related Issue(s)
    
    <!--
      If this PR fixes any issues, please link to the issue here.
      - Fixes #<issue_number>
    -->
    
    ## Screenshots
    
    
    ![image](https://github.com/user-attachments/assets/bc44e35b-e033-4769-ad40-cdfe9e3482f4)
    
    ## Checklist
    
    - [x] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -128,13 +128,25 @@ return {
 
   -- search/replace in multiple files
   {
-    "nvim-pack/nvim-spectre",
-    build = false,
-    cmd = "Spectre",
-    opts = { open_cmd = "noswapfile vnew" },
-    -- stylua: ignore
+    "MagicDuck/grug-far.nvim",
+    opts = { headerMaxWidth = 80 },
+    cmd = "GrugFar",
     keys = {
-      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in Files (Spectre)" },
+      {
+        "<leader>sr",
+        function()
+          local is_visual = vim.fn.mode():lower():find("v")
+          if is_visual then -- needed to make visual selection work
+            vim.cmd([[normal! v]])
+          end
+          local grug = require("grug-far");
+          (is_visual and grug.with_visual_selection or grug.grug_far)({
+            prefills = { filesFilter = "*." .. vim.fn.expand("%:e") },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
     },
   },
 
