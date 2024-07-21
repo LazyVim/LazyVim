6411ab0897f19dad9f902dfee29e101a9a767357
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-21 17:20:29 +0200

    fix(grug-far): only prefill files filter when file has an extension. Closes #4130

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -139,9 +139,11 @@ return {
           if is_visual then -- needed to make visual selection work
             vim.cmd([[normal! v]])
           end
-          local grug = require("grug-far");
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          local filesFilter = ext and ext ~= "" and "*." .. ext or nil;
           (is_visual and grug.with_visual_selection or grug.grug_far)({
-            prefills = { filesFilter = "*." .. vim.fn.expand("%:e") },
+            prefills = { filesFilter = filesFilter },
           })
         end,
         mode = { "n", "v" },
