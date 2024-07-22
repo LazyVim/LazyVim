72d0cad3530d877401ad9674f5266c79fbec396b
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-22 08:38:32 +0200

    feat(grug-far): no longer needed to call visual replace separately

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -135,15 +135,12 @@ return {
       {
         "<leader>sr",
         function()
-          local is_visual = vim.fn.mode():lower():find("v")
-          if is_visual then -- needed to make visual selection work
-            vim.cmd([[normal! v]])
-          end
           local grug = require("grug-far")
           local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-          local filesFilter = ext and ext ~= "" and "*." .. ext or nil;
-          (is_visual and grug.with_visual_selection or grug.grug_far)({
-            prefills = { filesFilter = filesFilter },
+          grug.grug_far({
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
           })
         end,
         mode = { "n", "v" },
