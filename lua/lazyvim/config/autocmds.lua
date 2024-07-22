1c2be200c185a4567c6a634da2b624d9a638fe73
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-22 23:26:03 +0200

    fix(grug-far): use new transient option

--- lua/lazyvim/config/autocmds.lua
+++ lua/lazyvim/config/autocmds.lua
@@ -55,7 +55,6 @@ vim.api.nvim_create_autocmd("FileType", {
   group = augroup("close_with_q"),
   pattern = {
     "PlenaryTestPopup",
-    "grug-far",
     "help",
     "lspinfo",
     "notify",
--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -138,6 +138,7 @@ return {
           local grug = require("grug-far")
           local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
           grug.grug_far({
+            transient = true,
             prefills = {
               filesFilter = ext and ext ~= "" and "*." .. ext or nil,
             },
