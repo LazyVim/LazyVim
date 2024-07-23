caf1aab03c0beb210d5043c4929827382dc72c29
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-23 08:27:29 +0200

    fix: new config

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -95,12 +95,16 @@ return {
     "MeanderingProgrammer/markdown.nvim",
     opts = {
       file_types = { "markdown", "norg", "rmd", "org" },
-      code = { sign = false },
-      heading = { sign = false, icons = {} },
-      inline_query = [[
-        (shortcut_link) @callout
-        [(inline_link) (image)] @link
-      ]],
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        -- FIXME: uncomment when PR is merged
+        -- icons = {},
+      },
     },
     ft = { "markdown", "norg", "rmd", "org" },
     enabled = true,
