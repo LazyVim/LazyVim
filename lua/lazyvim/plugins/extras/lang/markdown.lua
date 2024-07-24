c8d0faf9b152283b2ae74f9594287a834eae002a
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-07-24 08:21:36 +0200

    feat(markdown): markdown-render toggle

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -106,6 +106,22 @@ return {
       },
     },
     ft = { "markdown", "norg", "rmd", "org" },
-    enabled = true,
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      LazyVim.toggle.map("<leader>um", {
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      })
+    end,
   },
 }
