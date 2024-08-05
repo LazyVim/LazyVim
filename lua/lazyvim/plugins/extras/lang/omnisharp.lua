92a38ac069aaa39c4bd5a7185e9a7b743d73c197
Sheol27 <63117338+Sheol27@users.noreply.github.com>
2024-08-05 19:06:40 +0200

    fix(omnisharp): use Go To Definition with fzf.lua

--- lua/lazyvim/plugins/extras/lang/omnisharp.lua
+++ lua/lazyvim/plugins/extras/lang/omnisharp.lua
@@ -52,8 +52,10 @@ return {
           keys = {
             {
               "gd",
-              function()
+              LazyVim.has("telescope.nvim") and function()
                 require("omnisharp_extended").telescope_lsp_definitions()
+              end or function()
+                require("omnisharp_extended").lsp_definitions()
               end,
               desc = "Goto Definition",
             },
