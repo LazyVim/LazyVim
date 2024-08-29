afd17a1bd33f87bfaa6d12a521517b4992065721
cbosvik <132846580+cbosvik@users.noreply.github.com>
2024-08-29 12:18:31 +0200

    Update rest.lua

--- lua/lazyvim/plugins/extras/util/rest.lua
+++ lua/lazyvim/plugins/extras/util/rest.lua
@@ -9,16 +9,13 @@ return {
     ft = "http",
     keys = {
       { "<leader>R", "", desc = "+Rest" },
-       -- The scratchpad is a (throwaway) buffer where you can write your requests.
       { "<leader>Rb", "<cmd>lua require('kulala').scratchpad()<cr>", desc = "Open scratchpad" },
-       -- Copies the current request (as cURL command) to the clipboard
       { "<leader>Rc", "<cmd>lua require('kulala').copy()<cr>", desc = "Copy as cURL" },
       { "<leader>Rr", "<cmd>lua require('kulala').replay()<cr>", desc = "Replay the last request" },
       { "<leader>Rs", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request" },
       { "<leader>Rt", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle headers/body" },
       { "<leader>Rp", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Jump to previous request" },
       { "<leader>Rn", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Jump to next request" },
-      -- Closes the kulala window and also the current buffer if it is a .http or .rest file
       { "<leader>Rq", "<cmd>lua require('kulala').close()<cr>", desc = "Close window" },
     },
     opts = {},
