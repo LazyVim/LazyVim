1d2c0a5cf4eb00542deb7f770e0b9816e28a7746
George Guimarães <george.guimaraes@gmail.com>
2024-08-31 03:16:45 -0400

    chore(markdown): markdown.nvim was renamed to render-markdown.nvim (#4259)
    As described in
    https://github.com/MeanderingProgrammer/render-markdown.nvim/commit/aeb5cec617c3bd5738ab82ba2c3f9ccdc27656c2

--- lua/lazyvim/plugins/extras/lang/markdown.lua
+++ lua/lazyvim/plugins/extras/lang/markdown.lua
@@ -92,7 +92,7 @@ return {
   },
 
   {
-    "MeanderingProgrammer/markdown.nvim",
+    "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       file_types = { "markdown", "norg", "rmd", "org" },
       code = {
