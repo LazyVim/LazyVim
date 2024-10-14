482ce6ef1c9ef4900802a20f23c0418e3f85cce6
loqusion <38332081+loqusion@users.noreply.github.com>
2024-10-14 15:56:42 -0500

    feat(terraform): add formatter for packer files

--- lua/lazyvim/plugins/extras/lang/terraform.lua
+++ lua/lazyvim/plugins/extras/lang/terraform.lua
@@ -29,6 +29,7 @@ return {
     opts = function(_, opts)
       local null_ls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources or {}, {
+        null_ls.builtins.formatting.packer,
         null_ls.builtins.formatting.terraform_fmt,
         null_ls.builtins.diagnostics.terraform_validate,
       })
@@ -49,6 +50,7 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
+        hcl = { "packer_fmt" },
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
         ["terraform-vars"] = { "terraform_fmt" },
