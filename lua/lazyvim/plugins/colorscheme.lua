6570a141c0de30fccee38e2b10dcd14830624e16
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-10-24 07:58:12 +0200

    fix(catppuccin): fix bufferline integration when no colorscheme is set. Closes #4641

--- lua/lazyvim/plugins/colorscheme.lua
+++ lua/lazyvim/plugins/colorscheme.lua
@@ -55,7 +55,7 @@ return {
         "akinsho/bufferline.nvim",
         optional = true,
         opts = function(_, opts)
-          if vim.g.colors_name:find("catppuccin") then
+          if (vim.g.colors_name or ""):find("catppuccin") then
             opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
           end
         end,
