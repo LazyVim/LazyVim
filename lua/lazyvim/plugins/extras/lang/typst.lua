837ed5774554aa95cdae97fb4c72aaa2e61d32c2
Vo Quang Chien <voquangchien.dev@proton.me>
2024-10-29 08:31:02 +0700

    feat(typst): use builtin lsp formatter

--- lua/lazyvim/plugins/extras/lang/typst.lua
+++ lua/lazyvim/plugins/extras/lang/typst.lua
@@ -13,45 +13,21 @@ return {
   },
 
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = { "typstyle" },
-    },
-  },
-
-  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        tinymist = {},
+        tinymist = {
+          settings = {
+            formatterMode = "typstyle",
+          },
+        },
       },
     },
-  },
-
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        typst = { "typstyle" },
-      },
-    },
-  },
-
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.formatting.typstyle,
-      })
-    end,
   },
 
   {
     "chomosuke/typst-preview.nvim",
-    cmd = { "TypstPreview" },
+    cmd = { "TypstPreview", "TypstPreviewToggle", "TypstPreviewUpdate" },
     build = function()
       require("typst-preview").update()
     end,
@@ -65,7 +41,7 @@ return {
     },
     opts = {
       dependencies_bin = {
-        ["typst-preview"] = "tinymist",
+        tinymist = "tinymist",
       },
     },
   },
