76eeeb27f7962bdf6798dfff4120b85b4b8e8e9e
maddawik <edwardreillycomputers@gmail.com>
2024-10-30 01:08:08 -0400

    fix(extras): lazy-load telescope-terraform plugins

--- lua/lazyvim/plugins/extras/lang/terraform.lua
+++ lua/lazyvim/plugins/extras/lang/terraform.lua
@@ -58,9 +58,10 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     optional = true,
-    dependencies = {
+    specs = {
       {
         "ANGkeith/telescope-terraform-doc.nvim",
+        ft = { "terraform", "hcl" },
         config = function()
           LazyVim.on_load("telescope.nvim", function()
             require("telescope").load_extension("terraform_doc")
@@ -69,6 +70,7 @@ return {
       },
       {
         "cappyzawa/telescope-terraform.nvim",
+        ft = { "terraform", "hcl" },
         config = function()
           LazyVim.on_load("telescope.nvim", function()
             require("telescope").load_extension("terraform")
