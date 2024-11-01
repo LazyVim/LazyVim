41d87764745255f619c86b6d9bd7f7e32d2408e2
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-01 22:49:37 +0100

    feat: use latest blink for now

--- lua/lazyvim/plugins/extras/coding/blink.lua
+++ lua/lazyvim/plugins/extras/coding/blink.lua
@@ -5,6 +5,8 @@ return {
   },
   {
     "saghen/blink.cmp",
+    -- TODO: use release version
+    -- version = "*",
     opts_extend = { "sources.completion.enabled_providers" },
     dependencies = {
       "rafamadriz/friendly-snippets",
@@ -13,16 +15,15 @@ return {
     },
     event = "InsertEnter",
 
-    -- use a release tag to download pre-built binaries
-    version = "*",
-    -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      fuzzy = {
+        prebuilt_binaries = {
+          download = true,
+          force_version = "v0.5.0",
+        },
+      },
       highlight = {
         -- sets the fallback highlight groups to nvim-cmp's highlight groups
         -- useful for when your theme doesn't support blink.cmp
@@ -57,21 +58,7 @@ return {
       },
 
       keymap = {
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide" },
-        ["<C-y>"] = { "select_and_accept" },
-        ["<CR>"] = { "select_and_accept", "fallback" },
-
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback" },
-        ["<C-n>"] = { "select_next", "fallback" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        preset = "enter",
       },
     },
   },
