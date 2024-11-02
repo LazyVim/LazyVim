6e1d0994d99e63a46c3eff737c44ca47e3ecbe28
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-02 17:41:38 +0100

    fix(copilot): create undo point before accepting copilot suggestion when using blink

--- lua/lazyvim/plugins/extras/coding/copilot.lua
+++ lua/lazyvim/plugins/extras/coding/copilot.lua
@@ -109,6 +109,7 @@ return {
             if cmp.is_in_snippet() then
               return cmp.accept()
             elseif require("copilot.suggestion").is_visible() then
+              LazyVim.create_undo()
               require("copilot.suggestion").accept()
             else
               return cmp.select_and_accept()
