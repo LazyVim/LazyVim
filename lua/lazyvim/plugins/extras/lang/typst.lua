4a7e80c4aa3a978712e994c4bcb44160090d8945
Vo Quang Chien <voquangchien.dev@proton.me>
2024-11-02 16:50:31 +0700

    fix(typst): prevent redundant download of tinymist binary in typst-preview

--- lua/lazyvim/plugins/extras/lang/typst.lua
+++ lua/lazyvim/plugins/extras/lang/typst.lua
@@ -38,8 +38,10 @@ return {
   {
     "chomosuke/typst-preview.nvim",
     cmd = { "TypstPreview", "TypstPreviewToggle", "TypstPreviewUpdate" },
-    build = function()
-      require("typst-preview").update()
+    build = function(plugin)
+      local typst_preview = require("typst-preview")
+      typst_preview.setup(plugin.opts)
+      typst_preview.update()
     end,
     keys = {
       {
