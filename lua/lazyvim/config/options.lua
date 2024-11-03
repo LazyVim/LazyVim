60192b3548309a6b4e9766d1ef0d0e85670d8845
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-03 12:45:45 +0100

    feat(snacks): use statuscolumn

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -107,7 +107,6 @@ opt.spelloptions:append("noplainbuffer")
 opt.splitbelow = true -- Put new windows below current
 opt.splitkeep = "screen"
 opt.splitright = true -- Put new windows right of current
-opt.statuscolumn = [[%!v:lua.require'lazyvim.util'.ui.statuscolumn()]]
 opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
 opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
