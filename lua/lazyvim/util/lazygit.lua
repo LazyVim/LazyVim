ad698dd0aa6e02a7518aa755df3d5a1532e41841
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-04 11:51:23 +0100

    feat(snacks): use git

--- lua/lazyvim/util/lazygit.lua
+++ lua/lazyvim/util/lazygit.lua
@@ -1,25 +1,5 @@
 ---@class lazyvim.util.lazygit
----@param opts? {count?: number}|LazyCmdOptions
-function M.blame_line(opts)
-  opts = vim.tbl_deep_extend("force", {
-    count = 3,
-    interactive = false,
-    float = {
-      win = {
-        width = 0.6,
-        height = 0.6,
-        border = "rounded",
-      },
-      bo = { filetype = "git" },
-    },
-  }, opts or {})
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = cursor[1]
-  local file = vim.api.nvim_buf_get_name(0)
-  local root = LazyVim.root.detectors.pattern(0, { ".git" })[1] or "."
-  local cmd = { "git", "-C", root, "log", "-n", opts.count, "-u", "-L", line .. ",+1:" .. file }
-  return Snacks.terminal(cmd, opts)
-end
+local M = {}
 
 -- stylua: ignore
 M.remote_patterns = {
