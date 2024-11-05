c6d87f8092d44b86ecee6fcb74d4d91bafd0375b
Conarius <Conarius+34580743@users.noreply.github.com>
2024-11-05 17:54:56 +0100

    feat(extras.nushell): update to upstream treesitter

--- lua/lazyvim/plugins/extras/lang/nushell.lua
+++ lua/lazyvim/plugins/extras/lang/nushell.lua
@@ -12,23 +12,6 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "nushell/tree-sitter-nu" },
-    },
-    opts = function(_, opts)
-      ---@diagnostic disable-next-line: inject-field
-      require("nvim-treesitter.parsers").get_parser_configs().nu = {
-        install_info = {
-          url = "https://github.com/nushell/tree-sitter-nu",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "nu",
-      }
-
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "nu" })
-      end
-    end,
+    opts = { ensure_installed = { "nu" } },
   },
 }
