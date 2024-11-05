b6ddeb23418e4f65dc6fefdeb653162991ca27f2
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-05 16:55:30 +0100

    feat(snacks): use snacks notifier instead of nvim-notify

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -27,18 +27,30 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
-    ---@type snacks.Config
-    opts = {
-      toggle = { map = LazyVim.safe_keymap_set },
-      terminal = {
-        win = {
-          keys = {
-            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+    opts = function()
+      ---@type snacks.Config
+      return {
+        toggle = { map = LazyVim.safe_keymap_set },
+        notifier = { enabled = not LazyVim.has("noice.nvim") },
+        terminal = {
+          win = {
+            keys = {
+              nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+              nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+              nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+              nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+            },
           },
         },
+      }
+    end,
+    keys = {
+      {
+        "<leader>un",
+        function()
+          Snacks.notifier:hide()
+        end,
+        desc = "Dismiss All Notifications",
       },
     },
   },
