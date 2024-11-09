e613ab7140b66f817ff0609a783b3ad971d1faa3
Stanislav Zeman <zeman@standa.dev>
2024-11-10 00:01:40 +0100

    feat(extras): add Dart language

---
+++ lua/lazyvim/plugins/extras/lang/dart.lua
@@ -0,0 +1,45 @@
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = "dart",
+      root = { "pubspec.yaml" },
+    })
+  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dartls = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "dart" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        dart_format = {
+          args = { "format", "--line-length", "120" },
+        },
+      },
+      formatters_by_ft = {
+        dart = { "dart_format" },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "sidlatau/neotest-dart",
+    },
+    opts = {
+      adapters = {
+        ["neotest-dart"] = {},
+      },
+    },
+  },
+}
