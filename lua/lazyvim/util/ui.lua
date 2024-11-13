3ac62a4364780412fb791d459f908ca7a9232919
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-13 21:14:29 +0100

    refactor(ui): cleanup fg/color

--- lua/lazyvim/util/ui.lua
+++ lua/lazyvim/util/ui.lua
@@ -22,29 +22,9 @@ end
 
 ---@return {fg?:string}?
 function M.fg(name)
-  local color = M.color(name)
-  return color and { fg = color } or nil
-end
-
----@param name string
----@param bg? boolean
----@return string?
-function M.color(name, bg)
-  ---@type {foreground?:number}?
-  ---@diagnostic disable-next-line: deprecated
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name, link = false })
-    or vim.api.nvim_get_hl_by_name(name, true)
-  ---@diagnostic disable-next-line: undefined-field
-  ---@type string?
-  local color = nil
-  if hl then
-    if bg then
-      color = hl.bg or hl.background
-    else
-      color = hl.fg or hl.foreground
-    end
-  end
-  return color and string.format("#%06x", color) or nil
+  local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+  local fg = hl and hl.fg or hl.foreground
+  return fg and { fg = string.format("#%06x", fg) } or nil
 end
 
 function M.maximize()
