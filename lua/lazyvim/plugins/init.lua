b873f9a7e7ca77f329b40b17741ee244520a1306
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-19 09:03:00 +0100

    fix(snacks): make sure early notifications show up in noice

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -57,5 +57,14 @@ return {
         desc = "Dismiss All Notifications",
       },
     },
+    config = function(_, opts)
+      local notify = vim.notify
+      require("snacks").setup(opts)
+      -- HACK: restore vim.notify after snacks setup and let noice.nvim take over
+      -- this is needed to have early notifications show up in noice history
+      if LazyVim.has("noice.nvim") then
+        vim.notify = notify
+      end
+    end,
   },
 }
