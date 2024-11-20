e6f6db26675a7d26ea00d17a709c1b8b961addd3
Vo Quang chien <voquangchien.dev@proton.me>
2024-11-20 09:25:53 +0700

    fix(typst): remove `build` for `typst-preview`
    plugin now downloads binaries during setup

--- lua/lazyvim/plugins/extras/lang/typst.lua
+++ lua/lazyvim/plugins/extras/lang/typst.lua
@@ -39,11 +39,6 @@ return {
   {
     "chomosuke/typst-preview.nvim",
     cmd = { "TypstPreview", "TypstPreviewToggle", "TypstPreviewUpdate" },
-    build = function(plugin)
-      local typst_preview = require("typst-preview")
-      typst_preview.setup(plugin.opts) -- Setup preview during build to avoid redundant downloads
-      typst_preview.update()
-    end,
     keys = {
       {
         "<leader>cp",
