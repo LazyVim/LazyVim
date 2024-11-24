eb525c680d0423f5addb12e10f87ce5b81fc0d9e
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-24 23:03:03 +0100

    fix(lazydev): use luals' bundled luv library

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -183,15 +183,13 @@ return {
     cmd = "LazyDev",
     opts = {
       library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         { path = "LazyVim", words = { "LazyVim" } },
         { path = "snacks.nvim", words = { "Snacks" } },
         { path = "lazy.nvim", words = { "LazyVim" } },
       },
     },
   },
-  -- Manage libuv types with lazy. Plugin will never be loaded
-  { "Bilal2453/luvit-meta", lazy = true },
   -- Add lazydev source to cmp
   {
     "hrsh7th/nvim-cmp",
