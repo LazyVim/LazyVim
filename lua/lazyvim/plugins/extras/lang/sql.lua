64c2731046c6bfe58a68fb7a5f233ac6907c3297
Jakob Pfender <jakob.pfender@safetyio.com>
2024-11-26 14:08:02 +0100

    fix(sql): only use cmp if it is available

--- lua/lazyvim/plugins/extras/lang/sql.lua
+++ lua/lazyvim/plugins/extras/lang/sql.lua
@@ -48,19 +48,21 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = sql_ft,
         callback = function()
-          local cmp = require("cmp")
+          if LazyVim.has("cmp") then
+            local cmp = require("cmp")
 
-          -- global sources
-          ---@param source cmp.SourceConfig
-          local sources = vim.tbl_map(function(source)
-            return { name = source.name }
-          end, cmp.get_config().sources)
+            -- global sources
+            ---@param source cmp.SourceConfig
+            local sources = vim.tbl_map(function(source)
+              return { name = source.name }
+            end, cmp.get_config().sources)
 
-          -- add vim-dadbod-completion source
-          table.insert(sources, { name = "vim-dadbod-completion" })
+            -- add vim-dadbod-completion source
+            table.insert(sources, { name = "vim-dadbod-completion" })
 
-          -- update sources for the current buffer
-          cmp.setup.buffer({ sources = sources })
+            -- update sources for the current buffer
+            cmp.setup.buffer({ sources = sources })
+          end
         end,
       })
     end,
