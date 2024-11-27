40dba869254309865fcdabbc6d8c6b8e187c2444
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-27 17:10:22 +0100

    feat(extras): added extra for `smear-cursor.nvim`

---
+++ lua/lazyvim/plugins/extras/ui/smear-cursor.lua
@@ -0,0 +1,18 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  cond = vim.g.neovide == nil,
+  opts = {
+    hide_target_hack = true,
+  },
+  specs = {
+    -- disable mini.animate cursor
+    {
+      "echasnovski/mini.animate",
+      optional = true,
+      opts = {
+        cursor = { enable = false },
+      },
+    },
+  },
+}
