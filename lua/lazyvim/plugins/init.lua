be0cb9622dd47a5f62fbb32ac6f485a33a253119
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-01 09:29:18 +0100

    feat(snacks): added leader-n to show notification history

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -48,14 +48,10 @@ return {
         words = { enabled = true },
       }
     end,
+    -- stylua: ignore
     keys = {
-      {
-        "<leader>un",
-        function()
-          Snacks.notifier.hide()
-        end,
-        desc = "Dismiss All Notifications",
-      },
+      { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
     },
     config = function(_, opts)
       local notify = vim.notify
