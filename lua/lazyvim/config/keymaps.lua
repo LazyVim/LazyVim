8232651c4a4a80da34f8b6c1742b04144ae4b959
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-05 09:18:16 +0100

    feat(toggle): added leader-A to toggle tabline. Closes #4951

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -128,6 +128,7 @@ Snacks.toggle.option("relativenumber", { name = "Relative Number"}):map("<leader
 Snacks.toggle.diagnostics():map("<leader>ud")
 Snacks.toggle.line_number():map("<leader>ul")
 Snacks.toggle.option("conceallevel", {off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2}):map("<leader>uc")
+Snacks.toggle.option("showtabline", {off = 0, on = vim.o.showtabline > 0 and vim.o.showtabline or 2, desc = "Tabline"}):map("<leader>uA")
 Snacks.toggle.treesitter():map("<leader>uT")
 Snacks.toggle.option("background", { off = "light", on = "dark" , name = "Dark Background"}):map("<leader>ub")
 if vim.lsp.inlay_hint then
