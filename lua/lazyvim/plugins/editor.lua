2ebba15e1274f5437a418ddacfe9dbf342fd8a18
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-07 11:57:21 +0100

    feat(git): added toggles for gitsigns.nvim and mini.diff

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -300,6 +300,20 @@ return {
       end,
     },
   },
+  {
+    "gitsigns.nvim",
+    opts = function()
+      Snacks.toggle({
+        name = "Git Signs",
+        get = function()
+          return require("gitsigns.config").config.signcolumn
+        end,
+        set = function(state)
+          require("gitsigns").toggle_signs(state)
+        end,
+      }):map("<leader>uG")
+    end,
+  },
 
   -- better diagnostics list and others
   {
