8c79ab601af19b5370d560aa619567371ac70d86
Valentin Degenne <vdegenne@gmail.com>
2024-12-11 11:13:57 +0100

    feat(luasnip): add default user snippet location (#4987)
    ## Description
    
    Loads snippets in user directories if it exists.
    
    ## Checklist
    
    - [X] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.

--- lua/lazyvim/plugins/extras/coding/luasnip.lua
+++ lua/lazyvim/plugins/extras/coding/luasnip.lua
@@ -14,6 +14,7 @@ return {
         "rafamadriz/friendly-snippets",
         config = function()
           require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
         end,
       },
     },
