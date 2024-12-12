f3543d8d2969fa07ffa7f76a5e0ee7d1cac341a2
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-12 09:37:47 +0100

    refactor(snacks): move snacks specs to ui/util so that they show up in the generated docs

--- lua/lazyvim/plugins/util.lua
+++ lua/lazyvim/plugins/util.lua
@@ -1,4 +1,33 @@
+-- Terminal Mappings
+local function term_nav(dir)
+  ---@param self snacks.terminal
+  return function(self)
+    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
+      vim.cmd.wincmd(dir)
+    end)
+  end
+end
+
 return {
+
+  -- Snacks utils
+  {
+    "snacks.nvim",
+    opts = {
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+      terminal = {
+        win = {
+          keys = {
+            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+          },
+        },
+      },
+    },
+  },
 
   -- Session management. This saves your session in the background,
   -- keeping track of open buffers, window arrangement, and more.
