54bece17b390453ea0beac41e419394d5e0d69ff
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-12 12:03:17 +0100

    feat(snacks): added `snacks.input`

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -268,6 +268,7 @@ return {
     "snacks.nvim",
     opts = {
       indent = { enabled = true },
+      input = { enabled = true },
       notifier = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
