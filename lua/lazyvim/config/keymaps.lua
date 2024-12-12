a35b3289782768a8eb7da11ce6a044a1670499f4
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-12 08:08:01 +0100

    feat(keymaps): added leader-Z to toggle zoom

--- lua/lazyvim/config/keymaps.lua
+++ lua/lazyvim/config/keymaps.lua
@@ -176,7 +176,7 @@ map("n", "<leader>w", "<c-w>", { desc = "Windows", remap = true })
 map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-Snacks.toggle.zoom():map("<leader>wm")
+Snacks.toggle.zoom():map("<leader>wm"):map("<leader>Z")
 
 -- tabs
 map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
