808ba0dfdea100af7198b416c30974f3aa698497
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2024-12-12 12:46:05 -0300

    fix(clangd): avoid nix-indexing the completion sorting comparators

--- lua/lazyvim/plugins/extras/lang/clangd.lua
+++ lua/lazyvim/plugins/extras/lang/clangd.lua
@@ -105,6 +105,8 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
+      opts.sorting = opts.sorting or {}
+      opts.sorting.comparators = opts.sorting.comparators or {}
       table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
     end,
   },
