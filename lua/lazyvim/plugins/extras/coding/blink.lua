b07378dde40f1fa98b099cbd496129f5ca376e2c
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-13 22:54:38 +0100

    fix(blink): only override color symbol kind for blink

--- lua/lazyvim/plugins/extras/coding/blink.lua
+++ lua/lazyvim/plugins/extras/coding/blink.lua
@@ -147,10 +147,9 @@ return {
     "saghen/blink.cmp",
     opts = function(_, opts)
       opts.appearance = opts.appearance or {}
-      opts.appearance.kind_icons = LazyVim.config.icons.kinds
-
-      -- Use block instead of icon for color items to make swatches more usable
-      opts.appearance.kind_icons.Color = "██"
+      opts.appearance.kind_icons = vim.tbl_extend("keep", {
+        Color = "██", -- Use block instead of icon for color items to make swatches more usable
+      }, LazyVim.config.icons.kinds)
     end,
   },
 
