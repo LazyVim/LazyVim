22015e0b4835ae148c19b459bf494fe5f72b415b
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-16 08:17:09 +0100

    feat(blink): increase lazydev scores

--- lua/lazyvim/plugins/extras/coding/blink.lua
+++ lua/lazyvim/plugins/extras/coding/blink.lua
@@ -187,6 +187,7 @@ return {
           lazydev = {
             name = "LazyDev",
             module = "lazydev.integrations.blink",
+            score_offset = 100, -- show at a higher priority than lsp
           },
         },
       },
