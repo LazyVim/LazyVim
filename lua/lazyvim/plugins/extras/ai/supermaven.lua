25ee13ce29ad8c4e10d677e641955cfd3c810fb5
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-12-17 07:08:34 +0100

    fix(supermaven): don't attach to bigfile and some snacks ft. Fixes #5139

--- lua/lazyvim/plugins/extras/ai/supermaven.lua
+++ lua/lazyvim/plugins/extras/ai/supermaven.lua
@@ -6,6 +6,7 @@ return {
         accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
       },
       disable_inline_completion = vim.g.ai_cmp,
+      ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
     },
   },
 
