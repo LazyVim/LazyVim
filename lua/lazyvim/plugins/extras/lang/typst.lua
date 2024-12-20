3a59edf56c45fcee230e5bdf16a505b704bcf282
Vo Quang Chien <voquangchien.dev@proton.me>
2024-12-20 09:34:10 +0700

    feat(typst): add keymap to define entry point of the project

--- lua/lazyvim/plugins/extras/lang/typst.lua
+++ lua/lazyvim/plugins/extras/lang/typst.lua
@@ -17,6 +17,19 @@ return {
     opts = {
       servers = {
         tinymist = {
+          keys = {
+            {
+              "<leader>cP",
+              function()
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                LazyVim.lsp.execute({
+                  command = "tinymist.pinMain",
+                  arguments = { buf_name },
+                })
+              end,
+              desc = "Pin main file",
+            },
+          },
           single_file_support = true, -- Fixes LSP attachment in non-Git directories
           settings = {
             formatterMode = "typstyle",
