a4775abcfa9100ce9e34d53f74169cae5009f1c4
Benjamin <benjaminf.bauer@gmail.com>
2024-12-24 17:25:22 +0100

    Update lua/lazyvim/plugins/extras/lang/python.lua
    Co-authored-by: Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -116,7 +116,6 @@ return {
     "linux-cultist/venv-selector.nvim",
     branch = "regexp", -- Use this branch for the new version
     cmd = "VenvSelect",
-    enabled = true,
     opts = {
       settings = {
         options = {
