7ef68dff21697182a28ad6aa6a633db320bab99e
abeldekat <abel@nomail.com>
2025-01-03 09:54:57 +0100

    feat: an extra for mini.snippets. Review dpetka. Back to one extra. Final step, blink integration(standalone)

--- lua/lazyvim/plugins/extras/coding/mini-snippets.lua
+++ lua/lazyvim/plugins/extras/coding/mini-snippets.lua
@@ -4,8 +4,12 @@ if lazyvim_docs then
   -- set to `false` to prevent "non-lsp snippets"" from appearing inside completion popups
   -- motivation: less clutter in completion windows and a more direct usage of snippits
   vim.g.lazyvim_mini_snippets_in_cmp = true
+
+  -- NOTE: Blink has an open issue to address mini.snippets integration: #741
+  -- For now, blink ignores vim.g.lazyvim_mini_snippets_in_cmp, operating as if it where false.
 end
 
+-- Blink: hardcoded to false, see #741:
 local snippets_in_cmp = vim.g.lazyvim_mini_snippets_in_cmp == nil or vim.g.lazyvim_mini_snippets_in_cmp
 
 --[[
@@ -34,6 +38,25 @@ return {
   },
 }
 --]]
+
+local snippet_select_for_cmp = function(snippets, insert)
+  local cmp = require("cmp")
+  if cmp.visible() then
+    cmp.close()
+  end
+  MiniSnippets.default_select(snippets, insert)
+end
+local snippet_select_for_blink = function(snippets, insert)
+  -- Blink's cancel uses vim.schedule!
+  require("blink.cmp").cancel()
+  -- Schedule, otherwise blink's virtual text is not removed on vim.ui.select
+  vim.schedule(function()
+    MiniSnippets.default_select(snippets, insert)
+  end)
+end
+local snippet_select = function(snippets, insert)
+  MiniSnippets.default_select(snippets, insert)
+end
 
 local function expand(args)
   ---@diagnostic disable-next-line: undefined-global
@@ -65,18 +88,21 @@ return {
     opts = function()
       local snippets = require("mini.snippets")
 
-      LazyVim.cmp.actions.snippet_stop = function() end
-      LazyVim.cmp.actions.snippet_forward = snippets_in_cmp and function()
-        return jump("next")
-      end or nil
+      -- Load snippets based on current language by reading files from
+      -- "snippets/" subdirectories from 'runtimepath' directories.
+      local ret = { snippets = { snippets.gen_loader.from_lang() } }
 
-      return {
-        snippets = {
-          -- Load snippets based on current language by reading files from
-          -- "snippets/" subdirectories from 'runtimepath' directories.
-          snippets.gen_loader.from_lang(),
-        },
-      }
+      LazyVim.cmp.actions.snippet_stop = function() end -- by design, <esc> should not stop the session!
+      if not snippets_in_cmp then
+        LazyVim.cmp.actions.snippet_forward = nil
+        -- stylua: ignore
+        -- Close completion windows on snippet select to handle virtual text:
+        ret.expand = { select = function(...) snippet_select(...) end, }
+      else
+        -- stylua: ignore
+        LazyVim.cmp.actions.snippet_forward = function() return jump("next") end
+      end
+      return ret
     end,
   },
 
@@ -86,8 +112,7 @@ return {
     optional = true,
     dependencies = snippets_in_cmp and { "abeldekat/cmp-mini-snippets" } or nil,
     opts = function(_, opts)
-      -- snippet_select = snippet_select_for_cmp
-
+      snippet_select = snippet_select_for_cmp
       -- stylua: ignore
       -- Use mini.snippets to expand snippets from lsp:
       opts.snippet = { expand = function(args) expand(args) end }
@@ -99,5 +124,36 @@ return {
     -- stylua: ignore
     -- counterpart to <tab> defined in cmp.mappings
     keys = snippets_in_cmp and { { "<s-tab>", function() jump("prev") end, mode = "i" } } or nil,
+  },
+
+  -- blink.cmp integration
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = function(_, opts)
+      snippets_in_cmp = false
+      snippet_select = snippet_select_for_blink
+
+      opts.sources.default = vim.tbl_filter(function(source) -- rm builtin snippets
+        if source == "snippets" then
+          return false
+        end
+        return true
+      end, opts.sources.default)
+
+      -- Blink defines the <s-tab> key.
+      opts.snippets = {
+        -- Use mini.snippets to expand snippets from lsp:
+        expand = function(snippet)
+          expand({ body = snippet })
+        end,
+        active = function()
+          return require("mini.snippets").session.get(false) ~= nil -- lazy loading...
+        end,
+        jump = function(direction)
+          jump(direction < 0 and "prev" or "next")
+        end,
+      }
+    end,
   },
 }
