78576c1fa1010ee6b1f770bf84dd2282294e1f96
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-12 07:19:58 +0100

    style(snacks): descriptions for LSP pickers

--- lua/lazyvim/plugins/extras/editor/snacks_picker.lua
+++ lua/lazyvim/plugins/extras/editor/snacks_picker.lua
@@ -80,10 +80,10 @@ return {
       local Keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- stylua: ignore
       vim.list_extend(Keys, {
-        { "gd", function() Snacks.picker.lsp_definitions() end },
-        { "gr", function() Snacks.picker.lsp_references() end, nowait = true },
-        { "gI", function() Snacks.picker.lsp_implementations() end },
-        { "gy", function() Snacks.picker.lsp_type_definitions() end },
+        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition", has = "definition" },
+        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+        { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+        { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
       })
     end,
   },
