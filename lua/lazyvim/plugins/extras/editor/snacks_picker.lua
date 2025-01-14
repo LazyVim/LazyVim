cd1d1e35286091f749026b0921f2e419320d614d
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-14 16:20:11 +0100

    feat(snacks.picker): added keymaps for snacks picker with todo comments

--- lua/lazyvim/plugins/extras/editor/snacks_picker.lua
+++ lua/lazyvim/plugins/extras/editor/snacks_picker.lua
@@ -115,4 +115,13 @@ return {
       })
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    optional = true,
+    -- stylua: ignore
+    keys = {
+      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    },
+  },
 }
