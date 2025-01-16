df7426eefa79d5dfa2fcbe2f381abfb2cca70bad
Riri <Daydreamerriri@outlook.com>
2025-01-16 18:37:59 +0800

    fix(snacks.picker): respect lazyvim.config.kind_filter (#5415)
    ## Description
    
    Currently, snacks.picker does not filter symbols based on the
    kind_filter configured in lazyvim.
    
    This PR addresses that issue.
    
    ## Screenshots
    
    nil
    
    ## Checklist
    
    - [x] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.
    
    Co-authored-by: ding.zhao <ding.zhao@trip.com>

--- lua/lazyvim/plugins/extras/editor/snacks_picker.lua
+++ lua/lazyvim/plugins/extras/editor/snacks_picker.lua
@@ -112,7 +112,7 @@ return {
         { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
         { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
         { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-        { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols", has = "documentSymbol" },
+        { "<leader>ss", function() Snacks.picker.lsp_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Symbols", has = "documentSymbol" },
       })
     end,
   },
