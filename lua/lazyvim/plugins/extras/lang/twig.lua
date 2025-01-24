f7905adec675ebd7da89a3ff6838600a2bafcb16
tw <tw@pretexx.fr>
2025-01-24 10:43:02 +0100

    feat(extras): added twig language

---
+++ lua/lazyvim/plugins/extras/lang/twig.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = { ensure_installed = { 'twig' } },
+  },
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'twiggy-language-server',
+        'twigcs',
+        'twig-cs-fixer',
+      },
+    },
+  },
+  {
+    'nvimtools/none-ls.nvim',
+    optional = true,
+    opts = function(_, opts)
+      local nls = require('null-ls')
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.diagnostics.twigcs)
+      -- Twig-CS-Fixer builtin not available in none-ls
+    end,
+  },
+  {
+    'mfussenegger/nvim-lint',
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        twig = { 'twigcs' },
+      },
+    },
+  },
+  {
+    'stevearc/conform.nvim',
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        twig = { 'twig-cs-fixer' },
+      },
+    },
+  },
+}
