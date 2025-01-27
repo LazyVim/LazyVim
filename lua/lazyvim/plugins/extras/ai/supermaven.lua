17ee28dc4a42fc3a5231c01b069c466a6bac07b3
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2025-01-27 18:31:23 +0100

    feat(supermaven): support word-completion
    The supermaven-plugin supports partial completion, which is essentially
    expanding the next word. Configuration is extended to allow
    word-expansion on S-Tab with blink.

--- lua/lazyvim/plugins/extras/ai/supermaven.lua
+++ lua/lazyvim/plugins/extras/ai/supermaven.lua
@@ -20,16 +20,22 @@ return {
     "supermaven-inc/supermaven-nvim",
     opts = function()
       require("supermaven-nvim.completion_preview").suggestion_group = "SupermavenSuggestion"
-      LazyVim.cmp.actions.ai_accept = function()
-        local suggestion = require("supermaven-nvim.completion_preview")
-        if suggestion.has_suggestion() then
-          LazyVim.create_undo()
-          vim.schedule(function()
-            suggestion.on_accept_suggestion()
-          end)
-          return true
+
+      local ai_accept = function(partial)
+        return function()
+          local suggestion = require("supermaven-nvim.completion_preview")
+          if suggestion.has_suggestion() then
+            LazyVim.create_undo()
+            vim.schedule(function()
+              suggestion.on_accept_suggestion(partial)
+            end)
+            return true
+          end
         end
       end
+
+      LazyVim.cmp.actions.ai_accept = ai_accept(false)
+      LazyVim.cmp.actions.ai_accept_word = ai_accept(true)
     end,
   },
 
