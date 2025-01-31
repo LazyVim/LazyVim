4f006f1fba5fdaa0150c544ad7966b96ec9cb04a
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-31 08:32:46 +0100

    feat(snacks.explorer): enabled netrw integration

--- lua/lazyvim/plugins/extras/editor/snacks_explorer.lua
+++ lua/lazyvim/plugins/extras/editor/snacks_explorer.lua
@@ -2,6 +2,7 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   {
     "folke/snacks.nvim",
+    opts = { explorer = {} },
     keys = {
       {
         "<leader>fe",
@@ -20,18 +21,5 @@ return {
       { "<leader>e", "<leader>fe", desc = "Explorer Snacks (root dir)", remap = true },
       { "<leader>E", "<leader>fE", desc = "Explorer Snacks (cwd)", remap = true },
     },
-    init = function()
-      vim.api.nvim_create_autocmd("BufEnter", {
-        group = vim.api.nvim_create_augroup("snacks_explorer_start_directory", { clear = true }),
-        desc = "Start Snacks Explorer with directory",
-        once = true,
-        callback = function()
-          local dir = vim.fn.argv(0) --[[@as string]]
-          if dir ~= "" and vim.fn.isdirectory(dir) == 1 then
-            Snacks.picker.explorer({ cwd = dir })
-          end
-        end,
-      })
-    end,
   },
 }
