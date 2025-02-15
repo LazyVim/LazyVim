4aff0063a42bbc499cfa03feb6e58d4339c0950d
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-02-15 15:46:51 +0100

    feat(snacks.picker): move trouble keymap from `ctrl+t` -> `alt+t`

--- lua/lazyvim/plugins/extras/editor/snacks_picker.lua
+++ lua/lazyvim/plugins/extras/editor/snacks_picker.lua
@@ -121,7 +121,7 @@ return {
             win = {
               input = {
                 keys = {
-                  ["<c-t>"] = {
+                  ["<a-t>"] = {
                     "trouble_open",
                     mode = { "n", "i" },
                   },
