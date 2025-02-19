2709de649969837949851816355ef5bfd9a3e10f
Igor Guerrero <igorgue@protonmail.com>
2025-02-19 17:11:51 -0600

    fix(sql): fix autocomplete when `omni` is used as a `blink.cmp` source

--- lua/lazyvim/plugins/extras/lang/sql.lua
+++ lua/lazyvim/plugins/extras/lang/sql.lua
@@ -28,6 +28,11 @@ end
 
 local sql_ft = { "sql", "mysql", "plsql" }
 
+-- disable nvim default `sql_completion` plugin to be compatible with blink.cmp's omni
+-- while still showing some keywords from the syntax autocomplete sources
+vim.g.omni_sql_default_compl_type = "syntax"
+vim.g.loaded_sql_completion = true
+
 return {
   recommended = function()
     return LazyVim.extras.wants({
