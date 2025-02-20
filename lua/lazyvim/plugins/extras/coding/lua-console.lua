d9ed2dcaca4dc92895aa7ab231d5cdcbcb6077ec
Yaro <yaro@mail.ru>
2025-02-20 09:38:53 +0000

    feat(extras): add lua-console.nvim

---
+++ lua/lazyvim/plugins/extras/coding/lua-console.lua
@@ -0,0 +1,9 @@
+return {
+  "yarospace/lua-console.nvim",
+  lazy = true,
+  keys = {
+    { "`", desc = "Lua-console - toggle" },
+    { "<Leader>`", desc = "Lua-console - attach to buffer" },
+  },
+  opts = {},
+}
