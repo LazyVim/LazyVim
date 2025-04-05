e748a559a7648de4a65935309eb410c3f11392ed
Baptiste GAZE <bpt.gaze@gmail.com>
2025-04-05 16:15:02 +0200

    fix(ai.extras.copilot): update status retrieval for lualine

--- lua/lazyvim/plugins/extras/ai/copilot.lua
+++ lua/lazyvim/plugins/extras/ai/copilot.lua
@@ -51,7 +51,7 @@ return {
         LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
           local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
           if #clients > 0 then
-            local status = require("copilot.api").status.data.status
+            local status = require("copilot.status").data.status
             return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
           end
         end)
