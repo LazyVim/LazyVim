c3afeebb711126782e4ce92fa6641a341a2fc8bc
jradtilbrook <jarryd@tilbrooktech.com>
2025-04-16 08:53:51 +0800

    Add neotest config for PHP test runners

--- lua/lazyvim/plugins/extras/lang/php.lua
+++ lua/lazyvim/plugins/extras/lang/php.lua
@@ -84,4 +84,21 @@ return {
       },
     },
   },
+
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "V13Axel/neotest-pest",
+      "olimorris/neotest-phpunit",
+    },
+    opts = {
+      adapters = {
+        "neotest-pest",
+        ["neotest-phpunit"] = {
+          root_ignore_files = { "tests/Pest.php" },
+        },
+      },
+    },
+  },
 }
