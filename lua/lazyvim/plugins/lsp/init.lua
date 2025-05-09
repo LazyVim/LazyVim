4ffeb74fe9a6596cc4efdce06a026c6458ed9679
mmj <121494430+mmj2023@users.noreply.github.com>
2025-05-09 02:01:44 -0400

    fixed and properly formatted

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -212,7 +212,7 @@ return {
       local have_mason, mlsp = pcall(require, "mason-lspconfig")
       local all_mslp_servers = {}
       if have_mason then
-        all_mslp_servers =require("mason-lspconfig.mappings").get_mason_map().lspconfig_to_package
+        all_mslp_servers = require("mason-lspconfig").get_mappings().lspconfig_to_package
       end
 
       local ensure_installed = {} ---@type string[]
