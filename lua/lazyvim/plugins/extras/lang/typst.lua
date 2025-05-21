e432206114bb91b475f395a7a103c27906fcabdc
Vo Quang Chien <voquangchien.dev@proton.me>
2025-05-21 11:36:05 +0700

    feat(typst): notify when a file is pinned

--- lua/lazyvim/plugins/extras/lang/typst.lua
+++ lua/lazyvim/plugins/extras/lang/typst.lua
@@ -22,10 +22,12 @@ return {
               "<leader>cP",
               function()
                 local buf_name = vim.api.nvim_buf_get_name(0)
+                local file_name = vim.fn.fnamemodify(buf_name, ":t")
                 LazyVim.lsp.execute({
                   command = "tinymist.pinMain",
                   arguments = { buf_name },
                 })
+                LazyVim.info("Tinymist: Pinned " .. file_name)
               end,
               desc = "Pin main file",
             },
