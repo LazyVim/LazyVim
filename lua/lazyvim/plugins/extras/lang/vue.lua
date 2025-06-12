0f5fd4a0f5f8164bc7b306a8345855e2a5d3c722
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2025-06-12 12:59:59 +0100

    maintain hybridMode

--- lua/lazyvim/plugins/extras/lang/vue.lua
+++ lua/lazyvim/plugins/extras/lang/vue.lua
@@ -22,7 +22,7 @@ return {
         vue_ls = {
           init_options = {
             vue = {
-              hybridMode = false,
+              hybridMode = true,
             },
           },
         },
