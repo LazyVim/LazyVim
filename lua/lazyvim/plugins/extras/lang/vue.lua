a8befbc82e97d5c772ff5b034bccf8709a425f7a
Kemboi Elvis <kemboielvis22@gmail.com>
2025-06-13 10:37:33 +0300

    update

--- lua/lazyvim/plugins/extras/lang/vue.lua
+++ lua/lazyvim/plugins/extras/lang/vue.lua
@@ -19,7 +19,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        vue_ls = {
+        volar = {
           filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
           init_options = {
             vue = {
