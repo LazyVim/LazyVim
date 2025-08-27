080b439ce10124db07ad425098e66a2e879f1031
akioweh <0@akioweh.com>
2025-08-27 16:02:13 +0400

    fix(python): go back to tracking main

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -114,7 +114,6 @@ return {
 
   {
     "linux-cultist/venv-selector.nvim",
-    branch = "regexp", -- Use this branch for the new version
     cmd = "VenvSelect",
     enabled = function()
       return LazyVim.has("telescope.nvim")
