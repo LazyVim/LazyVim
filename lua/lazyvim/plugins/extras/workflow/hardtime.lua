77477a380728cbf050fdc3ae0d62f114cff9aee5
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-15 20:40:30 +0700

    feat(extras): add workflow.hardtime and workflow.precognition

---
+++ lua/lazyvim/plugins/extras/workflow/hardtime.lua
@@ -0,0 +1,14 @@
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    disabled_keys = {
+      ["<Insert>"] = { "", "i" },
+      ["<Home>"] = { "", "i" },
+      ["<End>"] = { "", "i" },
+      ["<PageUp>"] = { "", "i" },
+      ["<PageDown>"] = { "", "i" },
+    },
+  },
+}
---
+++ lua/lazyvim/plugins/extras/workflow/precognition.lua
@@ -0,0 +1,12 @@
+return {
+  "tris203/precognition.nvim",
+  --event = "VeryLazy",
+  opts = {
+    startVisible = true,
+    showBlankVirtLine = true,
+    highlightColor = { link = "Comment" },
+    disabled_fts = {
+      "startify",
+    },
+  },
+}
