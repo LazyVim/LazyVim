02d0c9aebddb902d9c974a38bb44db9bae4eac6d
flippette <me@flippo.cc>
2025-09-15 12:21:32 +0300

    fix(catppuccin): follow renamed integration (#6354)
    ## Description
    
    [This
    commit](https://github.com/catppuccin/nvim/commit/30fa4d122d9b22ad8b2e0ab1b533c8c26c4dde86)
    renamed a Catppuccin integration; this PR follows the rename.
    
    ## Checklist
    
    - [x] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.

--- lua/lazyvim/plugins/colorscheme.lua
+++ lua/lazyvim/plugins/colorscheme.lua
@@ -58,7 +58,7 @@ return {
         optional = true,
         opts = function(_, opts)
           if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
           end
         end,
       },
