ae3aaf2dd3a04eb5dbc5b1e34843558c85e45ac2
Sergey Kochetkov <skoch13@pm.me>
2025-09-15 11:45:00 +0200

    fix(mini): update mini plugin references from echasnovski to nvim-mini (#6374)
    Updates all `mini.nvim` plugin references to use the new `nvim-mini`
    organization namespace instead of the legacy `echasnovski` namespace
    across all LazyVim configuration files.
    
    ## Description
    
    https://github.com/nvim-mini/mini.nvim/discussions/1970
    
    ## Checklist
    
    - [x] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.
    
    ---------
    
    Co-authored-by: Folke Lemaitre <folke.lemaitre@gmail.com>

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -263,7 +263,7 @@ return {
 
   -- Filetype icons
   {
-    "echasnovski/mini.icons",
+    "nvim-mini/mini.icons",
     opts = {
       file = {
         [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
