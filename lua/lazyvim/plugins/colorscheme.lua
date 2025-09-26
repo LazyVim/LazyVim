af6e2505b54270c30145e8f4191b865870537287
robin <comfysage@isabelroses.com>
2025-09-26 06:36:14 +0200

    fix(catppuccin): follow-up on api change (#6505)
    ## Description
    
    https://github.com/catppuccin/nvim/pull/931 will change the api for
    accessing the bufferline integration. im making this follow up pr so it
    wont have to be done through a user :)
    (sorry about https://github.com/LazyVim/LazyVim/pull/6354)
    
    ## Checklist
    
    - [x] I've read the
    [CONTRIBUTING](https://github.com/LazyVim/LazyVim/blob/main/CONTRIBUTING.md)
    guidelines.

--- lua/lazyvim/plugins/colorscheme.lua
+++ lua/lazyvim/plugins/colorscheme.lua
@@ -13,6 +13,14 @@ return {
     lazy = true,
     name = "catppuccin",
     opts = {
+      lsp_styles = {
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
       integrations = {
         aerial = true,
         alpha = true,
@@ -28,26 +36,14 @@ return {
         leap = true,
         lsp_trouble = true,
         mason = true,
-        markdown = true,
         mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
         navic = { enabled = true, custom_bg = "lualine" },
         neotest = true,
         neotree = true,
         noice = true,
         notify = true,
-        semantic_tokens = true,
         snacks = true,
         telescope = true,
-        treesitter = true,
         treesitter_context = true,
         which_key = true,
       },
@@ -58,7 +54,7 @@ return {
         optional = true,
         opts = function(_, opts)
           if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
           end
         end,
       },
