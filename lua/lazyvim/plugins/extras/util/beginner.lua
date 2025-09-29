c6908ff668e37c60f6a4f047a58ee48f7e5b6a3d
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-29 17:11:18 +0700

    cleanup

--- lua/lazyvim/plugins/extras/util/beginner.lua
+++ lua/lazyvim/plugins/extras/util/beginner.lua
@@ -1,17 +1,13 @@
 return {
   {
     "tris203/precognition.nvim",
-    event = "VeryLazy",
     opts = {
-      startVisible = true,
-      showBlankVirtLine = true,
-      highlightColor = { link = "Comment" },
+      showBlankVirtLine = false,
     },
   },
 
   {
     "m4xshen/hardtime.nvim",
-    lazy = false,
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {},
   },
