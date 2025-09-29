5d18a46b6a7bb5b946a6c87b9a45a2738c9ec9c3
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-09-29 23:12:20 +0200

    feat(sidekick): added keymaps to work with AI cli tools

--- lua/lazyvim/plugins/extras/ai/sidekick.lua
+++ lua/lazyvim/plugins/extras/ai/sidekick.lua
@@ -54,6 +54,39 @@ return {
     keys = {
       -- nes is also useful in normal mode
       { "<tab>", LazyVim.cmp.map({ "ai_nes" }, "<tab>"), mode = { "n" }, expr = true },
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+      {
+        "<leader>aa",
+        function()
+          require("sidekick.cli").toggle()
+        end,
+        mode = { "n" },
+        desc = "Sidekick Toggle",
+      },
+      {
+        "<leader>as",
+        function()
+          require("sidekick.cli").select_tool()
+        end,
+        mode = { "n" },
+        desc = "Sidekick Select Tool",
+      },
+      {
+        "<c-.>",
+        function()
+          require("sidekick.cli").focus()
+        end,
+        mode = { "n", "x", "i", "t" },
+        desc = "Sidekick Switch Focus",
+      },
+      {
+        "<leader>ap",
+        function()
+          require("sidekick.cli").select_prompt()
+        end,
+        desc = "Sidekick Ask Prompt",
+        mode = { "n", "v" },
+      },
     },
   },
 }
