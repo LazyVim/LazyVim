23b1da170f6367fafe0be47b9c141770ed75a78e
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-01 19:04:14 +0200

    fix(copilot-native): schedule inline_completion.enable

--- lua/lazyvim/plugins/extras/ai/copilot-native.lua
+++ lua/lazyvim/plugins/extras/ai/copilot-native.lua
@@ -45,7 +45,9 @@ return {
       },
       setup = {
         copilot = function()
-          vim.lsp.inline_completion.enable()
+          vim.schedule(function()
+            vim.lsp.inline_completion.enable()
+          end)
           -- Accept inline suggestions or next edits
           LazyVim.cmp.actions.ai_accept = function()
             return vim.lsp.inline_completion.get()
