188b2886147acb4d9e2a5c8ae7b73770fd66a92c
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-02 19:14:51 +0200

    fix(sidekick): better keymaps

--- lua/lazyvim/plugins/extras/ai/sidekick.lua
+++ lua/lazyvim/plugins/extras/ai/sidekick.lua
@@ -64,26 +64,39 @@ return {
       {
         "<leader>as",
         function() require("sidekick.cli").select() end,
-        mode = { "n" },
-        desc = "Sidekick Select CLI",
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = "Select CLI",
       },
       {
-        "<leader>as",
-        function() require("sidekick.cli").send() end,
-        mode = { "v" },
-        desc = "Sidekick Send Visual Selection",
+        "<leader>at",
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>av",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
       },
       {
         "<leader>ap",
         function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
         desc = "Sidekick Select Prompt",
-        mode = { "n", "v" },
       },
       {
         "<c-.>",
         function() require("sidekick.cli").focus() end,
         mode = { "n", "x", "i", "t" },
         desc = "Sidekick Switch Focus",
+      },
+      -- Example of a keybinding to open Claude directly
+      {
+        "<leader>ac",
+        function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
+        desc = "Sidekick Claude Toggle",
       },
     },
   },
