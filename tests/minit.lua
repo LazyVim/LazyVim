8546f6232ecb1305a4ef9df281bad3512ba09da5
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-08 21:32:23 +0200

    ci: update minit.lua

--- tests/minit.lua
+++ tests/minit.lua
@@ -3,7 +3,7 @@
 vim.env.LAZY_STDPATH = ".tests"
 vim.env.LAZY_PATH = vim.fs.normalize("~/projects/lazy.nvim")
 
-if vim.fn.isdirectory(vim.env.LAZY_PATH) then
+if vim.fn.isdirectory(vim.env.LAZY_PATH) == 1 then
   loadfile(vim.env.LAZY_PATH .. "/bootstrap.lua")()
 else
   load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"), "bootstrap.lua")()
