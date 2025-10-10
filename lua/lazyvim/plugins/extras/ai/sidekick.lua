f8b062b130177f07d3c1c13b642402d42e980df5
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-11 00:05:16 +0200

    feat(sidekick): added `<leader>ad` to close/detach a terminal/session

--- lua/lazyvim/plugins/extras/ai/sidekick.lua
+++ lua/lazyvim/plugins/extras/ai/sidekick.lua
@@ -75,6 +75,11 @@ return {
         desc = "Select CLI",
       },
       {
+        "<leader>ad",
+        function() require("sidekick.cli").close() end,
+        desc = "Detach a CLI Session",
+      },
+      {
         "<leader>at",
         function() require("sidekick.cli").send({ msg = "{this}" }) end,
         mode = { "x", "n" },
