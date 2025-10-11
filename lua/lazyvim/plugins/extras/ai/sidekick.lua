049db39de0f272e8368a4b3881ac5a13f0d04b09
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-11 08:16:17 +0200

    fix(sidekick): only add copilot to lspconfig when `opts.nes.enabled ~= false`

--- lua/lazyvim/plugins/extras/ai/sidekick.lua
+++ lua/lazyvim/plugins/extras/ai/sidekick.lua
@@ -4,11 +4,13 @@ return {
   -- copilot-language-server
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        copilot = {},
-      },
-    },
+    opts = function(_, opts)
+      local sk = LazyVim.opts("sidekick.nvim") ---@type sidekick.Config|{}
+      if vim.tbl_get(sk, "nes", "enabled") ~= false then
+        opts.servers = opts.servers or {}
+        opts.servers.copilot = opts.servers.copilot or {}
+      end
+    end,
   },
 
   -- lualine
