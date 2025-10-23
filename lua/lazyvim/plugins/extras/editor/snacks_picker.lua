f0a91d9fa5f0cf27d6ba5fac759fa1d4e0df97a3
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-23 09:26:22 +0200

    feat(snacks.picker): added `<leader>gD` to diff against merge base of branch/PR

--- lua/lazyvim/plugins/extras/editor/snacks_picker.lua
+++ lua/lazyvim/plugins/extras/editor/snacks_picker.lua
@@ -73,6 +73,7 @@ return {
       { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
       -- git
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (hunks)" },
+      { "<leader>gD", function() Snacks.picker.git_diff({ base = "origin" }) end, desc = "Git Diff (origin)" },
       { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
       { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
       -- Grep
