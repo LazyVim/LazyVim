1a401627146c66beaeb213f66dd02662f1006e82
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-10-26 12:21:22 +0100

    fix(lsp.keymaps): make cond -> enabled work again. Closes #6697

--- lua/lazyvim/plugins/lsp/keymaps.lua
+++ lua/lazyvim/plugins/lsp/keymaps.lua
@@ -3,8 +3,8 @@ local M = {}
 ---@type LazyKeysLspSpec[]|nil
 M._keys = {}
 
----@alias LazyKeysLspSpec LazyKeysSpec|{has?:string|string[], cond?:fun():boolean}
----@alias LazyKeysLsp LazyKeys|{has?:string|string[], cond?:fun():boolean}
+---@alias LazyKeysLspSpec LazyKeysSpec|{has?:string|string[], enabled?:fun():boolean}
+---@alias LazyKeysLsp LazyKeys|{has?:string|string[], enabled?:fun():boolean}
 
 ---@deprecated
 ---@return LazyKeysLspSpec[]
@@ -43,24 +43,23 @@ function M.set(filter, spec)
   local Keys = require("lazy.core.handler.keys")
   for _, keys in pairs(Keys.resolve(spec)) do
     ---@cast keys LazyKeysLsp
-    if keys.cond == nil or keys.cond() then
-      local filters = {} ---@type vim.lsp.get_clients.Filter[]
-      if keys.has then
-        local methods = type(keys.has) == "string" and { keys.has } or keys.has --[[@as string[] ]]
-        for _, method in ipairs(methods) do
-          method = method:find("/") and method or ("textDocument/" .. method)
-          filters[#filters + 1] = vim.tbl_extend("force", vim.deepcopy(filter), { method = method })
-        end
-      else
-        filters[#filters + 1] = filter
+    local filters = {} ---@type vim.lsp.get_clients.Filter[]
+    if keys.has then
+      local methods = type(keys.has) == "string" and { keys.has } or keys.has --[[@as string[] ]]
+      for _, method in ipairs(methods) do
+        method = method:find("/") and method or ("textDocument/" .. method)
+        filters[#filters + 1] = vim.tbl_extend("force", vim.deepcopy(filter), { method = method })
       end
+    else
+      filters[#filters + 1] = filter
+    end
 
-      for _, f in ipairs(filters) do
-        local opts = Keys.opts(keys)
-        ---@cast opts snacks.keymap.set.Opts
-        opts.lsp = f
-        Snacks.keymap.set(keys.mode or "n", keys.lhs, keys.rhs, opts)
-      end
+    for _, f in ipairs(filters) do
+      local opts = Keys.opts(keys)
+      ---@cast opts snacks.keymap.set.Opts
+      opts.lsp = f
+      opts.enabled = keys.enabled
+      Snacks.keymap.set(keys.mode or "n", keys.lhs, keys.rhs, opts)
     end
   end
 end
