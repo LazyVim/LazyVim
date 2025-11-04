c777a93240418684eb6201f6ca7104f8d3918317
Lexin Gong <gonglexin@gmail.com>
2025-11-04 21:32:46 +0800

    feat(elixir): Switch elixir lsp to expert
    According to https://github.com/elixir-lang/expert, Expert is the `Official Elixir Language Server Protocol implementation`

--- lua/lazyvim/plugins/extras/lang/elixir.lua
+++ lua/lazyvim/plugins/extras/lang/elixir.lua
@@ -6,37 +6,8 @@ return {
     })
   end,
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        elixirls = {
-          keys = {
-            {
-              "<leader>cp",
-              function()
-                local params = vim.lsp.util.make_position_params()
-                LazyVim.lsp.execute({
-                  command = "manipulatePipes:serverid",
-                  arguments = { "toPipe", params.textDocument.uri, params.position.line, params.position.character },
-                })
-              end,
-              desc = "To Pipe",
-            },
-            {
-              "<leader>cP",
-              function()
-                local params = vim.lsp.util.make_position_params()
-                LazyVim.lsp.execute({
-                  command = "manipulatePipes:serverid",
-                  arguments = { "fromPipe", params.textDocument.uri, params.position.line, params.position.character },
-                })
-              end,
-              desc = "From Pipe",
-            },
-          },
-        },
-      },
-    },
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "expert" } },
   },
   {
     "nvim-treesitter/nvim-treesitter",
