8aee4d3fe61842677697ea9a9a16b39418353050
mint <116579758+Mintass@users.noreply.github.com>
2025-12-22 17:38:55 +0800

    fix(extras): remove matlab_ls installation via mason
    Co-authored-by: Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>

--- lua/lazyvim/plugins/extras/lang/matlab.lua
+++ lua/lazyvim/plugins/extras/lang/matlab.lua
@@ -21,7 +21,6 @@ return {
     "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "matlab_language_server" })
       if formatter == "miss_hit" then
         vim.list_extend(opts.ensure_installed, { "miss_hit" })
       end
