0f5c9ea2349d2c846770a6cefe1c359e5e81033d
Mika Andrianarijaona <mikaoelitiana@users.noreply.github.com>
2026-02-19 11:36:53 +0100

    Refactor Overseer commands and key mappings
    Overseer v2 https://github.com/stevearc/overseer.nvim/pull/448 introduced some breaking changes so this removes several Overseer commands and key mappings and adds `OverseerShell`.

--- lua/lazyvim/plugins/extras/editor/overseer.lua
+++ lua/lazyvim/plugins/extras/editor/overseer.lua
@@ -12,16 +12,8 @@ return {
       "OverseerOpen",
       "OverseerClose",
       "OverseerToggle",
-      "OverseerSaveBundle",
-      "OverseerLoadBundle",
-      "OverseerDeleteBundle",
-      "OverseerRunCmd",
-      "OverseerRun",
-      "OverseerInfo",
-      "OverseerBuild",
-      "OverseerQuickAction",
-      "OverseerTaskAction",
-      "OverseerClearCache",
+      "OverseerShell",
+      "OverseerRun"
     },
     opts = {
       dap = false,
@@ -53,11 +45,8 @@ return {
     keys = {
       { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
       { "<leader>oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
-      { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
-      { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
-      { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
-      { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
-      { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
+      { "<leader>os", "<cmd>OverseerShell<cr>",       desc = "Run Shell" },
+      { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" }
     },
   },
   {
