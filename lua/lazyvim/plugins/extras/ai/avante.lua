4bc3ec33a0513d72e6e62f4f63c578640d9bc036
Ibán Domínguez <domingueznoda@gmail.com>
2026-04-25 20:44:12 +0100

    feat(avante): add visual mode support for edit command

--- lua/lazyvim/plugins/extras/ai/avante.lua
+++ lua/lazyvim/plugins/extras/ai/avante.lua
@@ -31,9 +31,10 @@ return {
       "AvanteToggle",
     },
     keys = {
+      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
       { "<leader>aa", "<cmd>AvanteAsk<CR>", desc = "Ask Avante" },
       { "<leader>ac", "<cmd>AvanteChat<CR>", desc = "Chat with Avante" },
-      { "<leader>ae", "<cmd>AvanteEdit<CR>", desc = "Edit Avante" },
+      { "<leader>ae", "<cmd>AvanteEdit<CR>", mode = { "n", "v" }, desc = "Edit Avante" },
       { "<leader>af", "<cmd>AvanteFocus<CR>", desc = "Focus Avante" },
       { "<leader>ah", "<cmd>AvanteHistory<CR>", desc = "Avante History" },
       { "<leader>am", "<cmd>AvanteModels<CR>", desc = "Select Avante Model" },
