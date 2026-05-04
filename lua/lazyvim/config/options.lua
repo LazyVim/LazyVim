55934524a4ba64eaa1ee212711a33e72e94e622c
Felipe Matarazzo <felipemps@protonmail.com>
2026-05-04 10:39:05 -0300

    fix: add missing apostrophe in comment

--- lua/lazyvim/config/options.lua
+++ lua/lazyvim/config/options.lua
@@ -92,7 +92,7 @@ opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "glob
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false -- Dont show mode since we have a statusline
+opt.showmode = false -- Don't show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
