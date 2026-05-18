dc6240f4d56ae0d40cd0e673ca740dda50c4bf3b
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-05-18 12:32:49 +0200

    fix(hipatterns): don't color #add (use it too often as a private function in TS)

--- lua/lazyvim/plugins/extras/util/mini-hipatterns.lua
+++ lua/lazyvim/plugins/extras/util/mini-hipatterns.lua
@@ -39,6 +39,9 @@ M.plugin = {
           group = function(_, _, data)
             ---@type string
             local match = data.full_match
+            if match == "#add" then
+              return
+            end
             local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
             local hex_color = "#" .. r .. r .. g .. g .. b .. b
 
